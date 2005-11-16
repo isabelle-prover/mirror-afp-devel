beda22567cb4d57d5986490eac6cb6c68648dca1
lsf37 <unknown>
2005-11-16 15:19:47 +0000

    auto sync with Isabelle2005

---
+++ thys/Ordinal/Ordinal.thy
@@ -0,0 +1,11 @@
+(*  Title:       Countable Ordinals
+    ID:          $Id: Ordinal.thy,v 1.2 2005-11-16 15:19:47 lsf37 Exp $
+    Author:      Brian Huffman, 2005
+    Maintainer:  Brian Huffman <brianh@cse.ogi.edu>
+*)
+(*<*)
+theory Ordinal
+imports OrdinalVeblen
+begin
+end
+(*>*)
