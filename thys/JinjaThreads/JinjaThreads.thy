2868c2e8fb7b718348f95b052640fb0e6ac986af
fhaftmann <unknown>
2008-07-24 12:28:48 +0000

    added explicit root theory

---
+++ thys/JinjaThreads/JinjaThreads.thy
@@ -0,0 +1,13 @@
+theory JinjaThreads
+imports
+  "Common/WellForm"
+  "Common/Conform"
+  "Framework/FWProgress"
+  "Framework/FWLiftingSem"
+  "J/Deadlocked"
+  "BV/JVMDeadlocked"
+  "BV/TF_JVM"
+  "BV/Abstract_BV"
+begin
+
+end
