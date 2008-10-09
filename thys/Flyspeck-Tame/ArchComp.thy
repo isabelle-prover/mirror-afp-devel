40f8c811b0989ce7c4247ddf3499ed39bdac9ca5
fhaftmann <unknown>
2008-10-09 13:28:03 +0000

    split theory ArchComp in ArchCompAux and ArchComp

--- thys/Flyspeck-Tame/ArchComp.thy
+++ thys/Flyspeck-Tame/ArchComp.thy
@@ -1,108 +1,50 @@
-(*  ID:         $Id: ArchComp.thy,v 1.6 2008-06-12 06:57:17 lsf37 Exp $
+(*  ID:         $Id: ArchComp.thy,v 1.7 2008-10-09 13:27:29 fhaftmann Exp $
     Author:     Tobias Nipkow
 *)
 
-header "Comparing Enumeration and Achive"
+header {* Comparing Enumeration and Archive" *}
 
 theory ArchComp
-imports TameEnum TrieList Arch Efficient_Nat
+imports ArchCompAux Efficient_Nat 
 begin
 
-function qsort :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a list \<Rightarrow> 'a list" where
-  "qsort le []       = []"
-  | "qsort le (x#xs) = qsort le [y\<leftarrow>xs . ~ le x y] @ [x] @
-		       qsort le [y\<leftarrow>xs . le x y]"
-by pat_completeness auto
-termination by (relation "measure (size o snd)")
-  (auto simp add: length_filter_le [THEN le_less_trans])
-
-constdefs
- nof_vertices :: "'a fgraph \<Rightarrow> nat"
-"nof_vertices == length o remdups o concat"
-
- fgraph :: "graph \<Rightarrow> nat fgraph"
-"fgraph g == map vertices (faces g)"
-
- hash :: "nat fgraph \<Rightarrow> nat list"
-"hash fs == let n = nof_vertices fs in
-   [n, size fs] @
-   qsort (%x y. y < x) (map (%i. foldl (op +) 0 (map size [f\<leftarrow>fs. i mem f]))
-                           [0..<n])"
-
-consts
- enum_finals :: "(graph \<Rightarrow> graph list) \<Rightarrow> nat \<Rightarrow> graph list \<Rightarrow> nat fgraph list
-                 \<Rightarrow> nat fgraph list option"
-primrec
-"enum_finals succs 0 todo fins = None"
-"enum_finals succs (Suc n) todo fins =
- (if todo = [] then Some fins
-  else let g = hd todo; todo' = tl todo in
-    if final g then enum_finals succs n todo' (fgraph g # fins)
-    else enum_finals succs n (succs g @ todo') fins)"
-
-constdefs
- tameEnum :: "nat \<Rightarrow> nat \<Rightarrow> nat fgraph list option"
-"tameEnum p n == enum_finals (next_tame p) n [Seed p] []"
-
- diff2 :: "nat fgraph list \<Rightarrow> nat fgraph list \<Rightarrow>
-           (nat * nat fgraph) list * (nat * nat fgraph)list"
-"diff2 fgs ags ==
- let xfgs = map (%fs. (nof_vertices fs, fs)) fgs;
-     xags = map (%fs. (nof_vertices fs, fs)) ags in
- (filter (%xfg. ~list_ex (iso_test xfg) xags) xfgs,
-  filter (%xag. ~list_ex (iso_test xag) xfgs) xags)"
-
- same :: "nat fgraph list option \<Rightarrow> nat fgraph list \<Rightarrow> bool"
-"same fgso ags == case fgso of None \<Rightarrow> False | Some fgs \<Rightarrow>
- let niso_test = (%fs1 fs2. iso_test (nof_vertices fs1,fs1)
-                                     (nof_vertices fs2,fs2));
-     tfgs = trie_of_list hash fgs;
-     tags = trie_of_list hash ags in
- (list_all (%fg. list_ex (niso_test fg) (lookup_trie tags (hash fg))) fgs &
-  list_all (%ag. list_ex (niso_test ag) (lookup_trie tfgs (hash ag))) ags)"
-
-constdefs
- pre_iso_test :: "vertex fgraph \<Rightarrow> bool"
-"pre_iso_test Fs \<equiv>
- [] \<notin> set Fs \<and> (\<forall>F\<in>set Fs. distinct F) \<and> distinct (map rotate_min Fs)"
-
 lemma pre_iso_test3: "\<forall>g \<in> set Tri. pre_iso_test g"
-by evaluation
+by eval
 
 lemma pre_iso_test4: "\<forall>g \<in> set Quad. pre_iso_test g"
-by evaluation
+by eval
 
 lemma pre_iso_test5: "\<forall>g \<in> set Pent. pre_iso_test g"
-by evaluation
+by eval
 
 lemma pre_iso_test6: "\<forall>g \<in> set Hex. pre_iso_test g"
-by evaluation
+by eval
 
 lemma pre_iso_test7: "\<forall>g \<in> set Hept. pre_iso_test g"
-by evaluation
+by eval
 
 lemma pre_iso_test8: "\<forall>g \<in> set Oct. pre_iso_test g"
-by evaluation
+by eval
 
 ML "set Toplevel.timing"
 
 lemma same3: "same (tameEnum 0 800000) Tri"
-by evaluation
+by eval
 
 lemma same4: "same (tameEnum 1 8000000) Quad"
-by evaluation
+by eval
 
 lemma same5: "same (tameEnum 2 20000000) Pent"
-by evaluation
+by eval
 
 lemma same6: "same (tameEnum 3 4000000) Hex"
-by evaluation
+by eval
 
 lemma same7: "same (tameEnum 4 1000000) Hept"
-by evaluation
+by eval
 
 lemma same8: "same (tameEnum 5 2000000) Oct"
-by evaluation
+by eval
 
 ML "reset Toplevel.timing"
 
---
+++ thys/Flyspeck-Tame/ArchCompAux.thy
@@ -0,0 +1,64 @@
+(*  ID:         $Id: ArchCompAux.thy,v 1.1 2008-10-09 13:28:03 fhaftmann Exp $
+    Author:     Tobias Nipkow
+*)
+
+header {* Comparing Enumeration and Archive *}
+
+theory ArchCompAux
+imports TameEnum TrieList Arch
+begin
+
+function qsort :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a list \<Rightarrow> 'a list" where
+  "qsort le []       = []"
+  | "qsort le (x#xs) = qsort le [y\<leftarrow>xs . ~ le x y] @ [x] @
+		       qsort le [y\<leftarrow>xs . le x y]"
+by pat_completeness auto
+termination by (relation "measure (size o snd)")
+  (auto simp add: length_filter_le [THEN le_less_trans])
+
+definition nof_vertices :: "'a fgraph \<Rightarrow> nat" where
+  "nof_vertices = length o remdups o concat"
+
+definition fgraph :: "graph \<Rightarrow> nat fgraph" where
+  "fgraph g = map vertices (faces g)"
+
+definition hash :: "nat fgraph \<Rightarrow> nat list" where
+  "hash fs = (let n = nof_vertices fs in
+     [n, size fs] @
+     qsort (%x y. y < x) (map (%i. foldl (op +) 0 (map size [f\<leftarrow>fs. i mem f]))
+                             [0..<n]))"
+
+primrec enum_finals :: "(graph \<Rightarrow> graph list) \<Rightarrow> nat \<Rightarrow> graph list \<Rightarrow> nat fgraph list
+                 \<Rightarrow> nat fgraph list option" where
+  "enum_finals succs 0 todo fins = None"
+  | "enum_finals succs (Suc n) todo fins =
+     (if todo = [] then Some fins
+      else let g = hd todo; todo' = tl todo in
+        if final g then enum_finals succs n todo' (fgraph g # fins)
+        else enum_finals succs n (succs g @ todo') fins)"
+
+definition tameEnum :: "nat \<Rightarrow> nat \<Rightarrow> nat fgraph list option" where
+  "tameEnum p n = enum_finals (next_tame p) n [Seed p] []"
+
+definition diff2 :: "nat fgraph list \<Rightarrow> nat fgraph list \<Rightarrow>
+           (nat * nat fgraph) list * (nat * nat fgraph)list" where
+  "diff2 fgs ags =
+   (let xfgs = map (%fs. (nof_vertices fs, fs)) fgs;
+       xags = map (%fs. (nof_vertices fs, fs)) ags in
+   (filter (%xfg. ~list_ex (iso_test xfg) xags) xfgs,
+    filter (%xag. ~list_ex (iso_test xag) xfgs) xags))"
+
+definition same :: "nat fgraph list option \<Rightarrow> nat fgraph list \<Rightarrow> bool" where
+  "same fgso ags = (case fgso of None \<Rightarrow> False | Some fgs \<Rightarrow>
+   let niso_test = (%fs1 fs2. iso_test (nof_vertices fs1,fs1)
+                                       (nof_vertices fs2,fs2));
+       tfgs = trie_of_list hash fgs;
+       tags = trie_of_list hash ags in
+   (list_all (%fg. list_ex (niso_test fg) (lookup_trie tags (hash fg))) fgs &
+    list_all (%ag. list_ex (niso_test ag) (lookup_trie tfgs (hash ag))) ags))"
+
+definition pre_iso_test :: "vertex fgraph \<Rightarrow> bool" where
+  "pre_iso_test Fs \<longleftrightarrow>
+   [] \<notin> set Fs \<and> (\<forall>F\<in>set Fs. distinct F) \<and> distinct (map rotate_min Fs)"
+
+end
