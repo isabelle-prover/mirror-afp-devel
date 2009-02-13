8965b26a7c8de5aaeed800749639277065f35047
nipkow <unknown>
2009-02-13 08:57:28 +0000

    fix

--- thys/Integration/RealRandVar.thy
+++ thys/Integration/RealRandVar.thy
@@ -1,7 +1,7 @@
 header {*Real-Valued Random Variables*}
 
 theory RealRandVar
-imports Measure
+imports Measure Countable
 begin
 
 text {*While most of the above material was modeled after Hurd's work
