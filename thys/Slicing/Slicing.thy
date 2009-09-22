c7f06a6d2392f01416b56117bb21a8a0300cfe89
haftmann <unknown>
2009-09-22 15:37:01 +0200

    added session entrance theory

---
+++ thys/Slicing/Slicing.thy
@@ -0,0 +1,17 @@
+theory Slicing
+imports
+  "Basic/StandardControlDependence"
+  "Basic/WeakControlDependence"
+  "Basic/ControlDependenceRelations"
+  "Basic/SemanticsCFG"
+  "Dynamic/DynSlice"
+  "StaticIntra/CDepInstantiations"
+  "While/SemanticsWellFormed"
+  "While/DynamicControlDependences"
+  "While/StaticControlDependences"
+  "JinjaVM/JVMCFG_wf" 
+  "JinjaVM/JVMPostdomination" 
+  "JinjaVM/SemanticsWF"
+begin
+
+end
