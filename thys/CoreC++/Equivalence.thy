e152a469fe983f9e756ae1c3d3f2d8e9443501f6
haftmann <unknown>
2009-09-23 08:37:24 +0200

    tuned proofs

--- thys/CoreC++/Equivalence.thy
+++ thys/CoreC++/Equivalence.thy
@@ -1851,7 +1851,7 @@ proof(induct rule:eval_evals_inducts)
                  P,E(V \<mapsto> T) \<turnstile> \<langle>e\<^isub>0,(h\<^isub>0,l\<^isub>0(V:=None)|`W)\<rangle> \<Rightarrow> \<langle>e\<^isub>1,(h\<^isub>1,l\<^isub>1|`W)\<rangle>" by fact
   (*have type:"is_type P T" .*)
   have "fv({V:T; e\<^isub>0}) \<subseteq> W" by fact
-  hence "fv e\<^isub>0 - {V} \<subseteq> W" by (simp_all add:Un_subset_iff)
+  hence "fv e\<^isub>0 - {V} \<subseteq> W" by simp_all
   hence "fv e\<^isub>0 \<subseteq> insert V W" by fast
   with IH[OF this]
   have "P,E(V \<mapsto> T) \<turnstile> \<langle>e\<^isub>0,(h\<^isub>0, (l\<^isub>0|`W)(V := None))\<rangle> \<Rightarrow> \<langle>e\<^isub>1,(h\<^isub>1, l\<^isub>1|`insert V W)\<rangle>"
