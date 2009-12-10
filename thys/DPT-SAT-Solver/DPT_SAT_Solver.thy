2ccc97e031f516fe5cafa50770c51efc560d999a
nipkow <unknown>
2009-12-10 11:02:34 +0100

    New

---
+++ thys/DPT-SAT-Solver/DPT_SAT_Solver.thy
@@ -0,0 +1,10 @@
+(*  Title:       DPT (Decision Procedure Toolkit) SAT solver ported to SML
+    Author:      Armin Heller, 2008
+    Maintainer:  Jasmin Blanchette <blanchette at in.tum.de>
+*)
+
+theory DPT_SAT_Solver
+imports SAT
+uses "dpt_sat_solver.ML"
+begin
+end
