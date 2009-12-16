ba24dc309c7609cf7b1c3d80a98ae08440d938e6
haftmann <unknown>
2009-12-16 14:05:05 +0100

    explicit dest: predicate1D

--- thys/Ordinals_and_Cardinals/Cardinal_Order_Relation.thy
+++ thys/Ordinals_and_Cardinals/Cardinal_Order_Relation.thy
@@ -229,7 +229,7 @@ proof-
     moreover have "(?f y2,y2) : R" using * someI[of "% x. (x,y2) : R"] by auto
     ultimately show "y1 = y2" using ** INJ by auto
   qed
-  ultimately show "|?Y| <=o |?X|" using card_of_ordLeq by auto
+  ultimately show "|?Y| <=o |?X|" using card_of_ordLeq by (auto dest: predicate1D)
 qed
 
 
