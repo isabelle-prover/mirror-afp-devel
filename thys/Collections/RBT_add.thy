54741ff5ffbd26aced61bea6b6ded258b7c3b90c
haftmann <unknown>
2010-03-03 08:44:38 +0100

    adjusted to changes in RBT.thy

--- thys/Collections/RBT_add.thy
+++ thys/Collections/RBT_add.thy
@@ -23,59 +23,59 @@ lemma map_add_upd2: "m\<notin>dom e2 \<Longrightarrow> e1(m \<mapsto> u1) ++ e2 
   done
 
 lemma map_of_alist_of_aux: 
-  "st (Tr c t1 k v t2) \<Longrightarrow> RBT.map_of (Tr c t1 k v t2) 
-   = RBT.map_of t2 ++ [k\<mapsto>v] ++ RBT.map_of t1"
+  "RBT.sorted (RBT.Branch c t1 k v t2) \<Longrightarrow> RBT.lookup (RBT.Branch c t1 k v t2) 
+   = RBT.lookup t2 ++ [k\<mapsto>v] ++ RBT.lookup t1"
 proof (rule ext)
   fix x
-  assume ST: "st (Tr c t1 k v t2)"
+  assume ST: "RBT.sorted (RBT.Branch c t1 k v t2)"
   let 
-    ?thesis = "RBT.map_of (Tr c t1 k v t2) x 
-               = (RBT.map_of t2 ++ [k \<mapsto> v] ++ RBT.map_of t1) x"
+    ?thesis = "RBT.lookup (RBT.Branch c t1 k v t2) x 
+               = (RBT.lookup t2 ++ [k \<mapsto> v] ++ RBT.lookup t1) x"
 
-  have DOM_T1: "!!k'. k'\<in>dom (RBT.map_of t1) \<Longrightarrow> k>k'" 
+  have DOM_T1: "!!k'. k'\<in>dom (RBT.lookup t1) \<Longrightarrow> k>k'" 
   proof -
     fix k'
     from ST have "t1 |\<guillemotleft> k" by simp
-    with tlt_prop have "\<forall>k'\<in>keys t1. k>k'" by auto
-    moreover assume "k'\<in>dom (RBT.map_of t1)"
-    ultimately show "k>k'" using RBT.mapof_keys ST by auto
+    with tree_less_prop have "\<forall>k'\<in>keys t1. k>k'" by auto
+    moreover assume "k'\<in>dom (RBT.lookup t1)"
+    ultimately show "k>k'" using RBT.lookup_keys ST by auto
   qed
 
-  have DOM_T2: "!!k'. k'\<in>dom (RBT.map_of t2) \<Longrightarrow> k<k'" 
+  have DOM_T2: "!!k'. k'\<in>dom (RBT.lookup t2) \<Longrightarrow> k<k'" 
   proof -
     fix k'
     from ST have "k \<guillemotleft>| t2" by simp
-    with tgt_prop have "\<forall>k'\<in>keys t2. k<k'" by auto
-    moreover assume "k'\<in>dom (RBT.map_of t2)"
-    ultimately show "k<k'" using RBT.mapof_keys ST by auto
+    with tree_greater_prop have "\<forall>k'\<in>keys t2. k<k'" by auto
+    moreover assume "k'\<in>dom (RBT.lookup t2)"
+    ultimately show "k<k'" using RBT.lookup_keys ST by auto
   qed
 
   {
     assume C: "x<k"
-    hence "RBT.map_of (Tr c t1 k v t2) x = RBT.map_of t1 x" by simp
+    hence "RBT.lookup (RBT.Branch c t1 k v t2) x = RBT.lookup t1 x" by simp
     moreover from C have "x\<notin>dom [k\<mapsto>v]" by simp
-    moreover have "x\<notin>dom (RBT.map_of t2)" proof
-      assume "x\<in>dom (RBT.map_of t2)"
+    moreover have "x\<notin>dom (RBT.lookup t2)" proof
+      assume "x\<in>dom (RBT.lookup t2)"
       with DOM_T2 have "k<x" by blast
       with C show False by simp
     qed
     ultimately have ?thesis by (simp add: map_add_upd2 map_add_dom_app_simps)
   } moreover {
     assume [simp]: "x=k"
-    hence "RBT.map_of (Tr c t1 k v t2) x = [k \<mapsto> v] x" by simp
-    moreover have "x\<notin>dom (RBT.map_of t1)" proof
-      assume "x\<in>dom (RBT.map_of t1)"
+    hence "RBT.lookup (RBT.Branch c t1 k v t2) x = [k \<mapsto> v] x" by simp
+    moreover have "x\<notin>dom (RBT.lookup t1)" proof
+      assume "x\<in>dom (RBT.lookup t1)"
       with DOM_T1 have "k>x" by blast
       thus False by simp
     qed
     ultimately have ?thesis by (simp add: map_add_upd2 map_add_dom_app_simps)
   } moreover {
     assume C: "x>k"
-    hence "RBT.map_of (Tr c t1 k v t2) x = RBT.map_of t2 x" 
+    hence "RBT.lookup (RBT.Branch c t1 k v t2) x = RBT.lookup t2 x" 
       by (simp add: less_not_sym[of k x])
     moreover from C have "x\<notin>dom [k\<mapsto>v]" by simp
-    moreover have "x\<notin>dom (RBT.map_of t1)" proof
-      assume "x\<in>dom (RBT.map_of t1)"
+    moreover have "x\<notin>dom (RBT.lookup t1)" proof
+      assume "x\<in>dom (RBT.lookup t1)"
       with DOM_T1 have "k>x" by simp
       with C show False by simp
     qed
@@ -85,50 +85,48 @@ qed
 
 (* This one is marked with an oops in RBT.thy *)
 lemma map_of_alist_of:
-  shows "st t \<Longrightarrow> Map.map_of (alist_of t) = map_of t"
+  shows "RBT.sorted t \<Longrightarrow> map_of (RBT.entries t) = RBT.lookup t"
 proof (induct t)
-  case Empty thus ?case by (simp add: RBT.map_of_Empty)
+  case Empty thus ?case by (simp add: RBT.lookup_Empty)
 next
-  case (Tr c t1 k v t2)
-  hence "Map.map_of (alist_of (Tr c t1 k v t2)) 
-         = RBT.map_of t2 ++ [k \<mapsto> v] ++ RBT.map_of t1" by simp
-  also note map_of_alist_of_aux[OF Tr.prems,symmetric]
+  case (Branch c t1 k v t2)
+  hence "map_of (RBT.entries (RBT.Branch c t1 k v t2)) 
+         = RBT.lookup t2 ++ [k \<mapsto> v] ++ RBT.lookup t1" by simp
+  also note map_of_alist_of_aux [OF Branch.prems,symmetric]
   finally show ?case .
 qed
 
-lemma tlt_trans: "\<lbrakk>l |\<guillemotleft> u; u\<le>v\<rbrakk> \<Longrightarrow> l |\<guillemotleft> v"
+lemma tree_less_eq_trans: "l |\<guillemotleft> u \<Longrightarrow> u \<le> v \<Longrightarrow> l |\<guillemotleft> v"
   by (induct l) auto
 
-lemma trt_trans: "\<lbrakk> u\<le>v; v\<guillemotleft>|r \<rbrakk> \<Longrightarrow> u\<guillemotleft>|r"
+lemma tree_greater_eq_trans: "u \<le> v \<Longrightarrow> v \<guillemotleft>| r \<Longrightarrow> u \<guillemotleft>| r"
   by (induct r) auto
 
-lemmas tlt_trans' = tlt_trans[OF _ less_imp_le]
-lemmas trt_trans' = trt_trans[OF less_imp_le]
+lemmas tree_less_eq_trans' = tree_less_eq_trans [OF _ less_imp_le]
+lemmas tree_greater_eq_trans' = tree_greater_eq_trans [OF less_imp_le]
 
-
-lemma map_of_rbt_finite[simp, intro!]: "finite (dom (RBT.map_of t))"
+lemma map_of_rbt_finite[simp, intro!]: "finite (dom (RBT.lookup t))"
 proof (induct t)
   case Empty thus ?case by simp
 next
-  case (Tr color t1 a b t2)
+  case (Branch color t1 a b t2)
   thus ?case
     apply (rule_tac 
-      B="insert a (dom (RBT.map_of t1) \<union> dom (RBT.map_of t2))" 
+      B="insert a (dom (RBT.lookup t1) \<union> dom (RBT.lookup t2))" 
       in finite_subset)
     apply (auto split: split_if_asm)
     done
-
 qed
 
 lemma rbt_dom_node: 
-  "st (Tr c t1 k v t2) \<Longrightarrow> 
-    dom (RBT.map_of (Tr c t1 k v t2)) 
-    = insert k (dom (RBT.map_of t1) \<union> dom (RBT.map_of t2))"
+  "RBT.sorted (RBT.Branch c t1 k v t2) \<Longrightarrow> 
+    dom (RBT.lookup (RBT.Branch c t1 k v t2)) 
+    = insert k (dom (RBT.lookup t1) \<union> dom (RBT.lookup t2))"
 proof -
-  assume ST: "st (Tr c t1 k v t2)"
-  hence STT[simp]: "st t1" "st t2" by simp_all
-  note [simp] = RBT.mapof_keys[OF ST] RBT.mapof_keys[OF STT(1)] 
-    RBT.mapof_keys[OF STT(2)]
+  assume ST: "RBT.sorted (RBT.Branch c t1 k v t2)"
+  hence STT[simp]: "RBT.sorted t1" "RBT.sorted t2" by simp_all
+  note [simp] = RBT.lookup_keys[OF ST] RBT.lookup_keys[OF STT(1)] 
+    RBT.lookup_keys[OF STT(2)]
   thus ?thesis by simp
 qed
 
