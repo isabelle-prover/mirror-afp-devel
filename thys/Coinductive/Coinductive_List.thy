ea6f2fb1b2a570642508673e60970fb2f8d82170
wenzelm <unknown>
2010-03-13 16:50:07 +0100

    modernized primrec;

--- thys/Coinductive/Coinductive_List.thy
+++ thys/Coinductive/Coinductive_List.thy
@@ -53,12 +53,12 @@ proof
   qed
 qed
 
-consts
+primrec
   LList_corec_aux :: "nat \<Rightarrow> ('a \<Rightarrow> ('b Datatype.item \<times> 'a) option) \<Rightarrow>
     'a \<Rightarrow> 'b Datatype.item"
-primrec
+where
   "LList_corec_aux 0 f x = {}"
-  "LList_corec_aux (Suc k) f x =
+| "LList_corec_aux (Suc k) f x =
     (case f x of
       None \<Rightarrow> NIL
     | Some (z, w) \<Rightarrow> CONS z (LList_corec_aux k f w))"
