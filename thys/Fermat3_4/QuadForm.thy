790dabfc8d0e1d725900fe40087e1d44ad834811
Brian Huffman <brianh@cs.pdx.edu>
2010-05-18 06:21:05 -0700

    use correct paths for Old_Number_Theory imports

--- thys/Fermat3_4/QuadForm.thy
+++ thys/Fermat3_4/QuadForm.thy
@@ -7,7 +7,7 @@ header {* The quadratic form $x^2 + Ny^2$ *}
 
 theory QuadForm
 imports 
-  "~~/src/HOL/Number_Theory/Quadratic_Reciprocity" 
+  "~~/src/HOL/Old_Number_Theory/Quadratic_Reciprocity" 
   IntNatAux
 begin
 
