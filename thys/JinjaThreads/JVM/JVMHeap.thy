4dd551cba4616b81987a03b530265555c0d4fc83
Unknown <lochbihl@i44pc65.info.uni-karlsruhe.de>
2010-06-09 16:26:52 +0200

    Added missing theory JVMHeap to JinjaThreads

---
+++ thys/JinjaThreads/JVM/JVMHeap.thy
@@ -0,0 +1,115 @@
+(*  Title:      JinjaThreads/JVM/JVMHeap.thy
+    Author:     Andreas Lochbihler
+*)
+
+header {* \isaheader{Abstract heap locales for byte code programs} *}
+
+theory JVMHeap imports
+  "../Common/Conform"
+  "JVMInstructions"
+begin
+
+locale JVM_heap_base =
+  heap_base +
+  constrains empty_heap :: "'heap"
+  and new_obj :: "'heap \<Rightarrow> cname \<Rightarrow> ('heap \<times> addr option)"
+  and new_arr :: "'heap \<Rightarrow> ty \<Rightarrow> nat \<Rightarrow> ('heap \<times> addr option)"
+  and typeof_addr :: "'heap \<Rightarrow> addr \<rightharpoonup> ty"
+  and array_length :: "'heap \<Rightarrow> addr \<Rightarrow> nat"
+  and heap_read :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> bool"
+  and heap_write :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> 'heap \<Rightarrow> bool"
+
+locale JVM_heap =
+  JVM_heap_base +
+  heap +
+  constrains empty_heap :: "'heap"
+  and new_obj :: "'heap \<Rightarrow> cname \<Rightarrow> ('heap \<times> addr option)"
+  and new_arr :: "'heap \<Rightarrow> ty \<Rightarrow> nat \<Rightarrow> ('heap \<times> addr option)"
+  and typeof_addr :: "'heap \<Rightarrow> addr \<rightharpoonup> ty"
+  and array_length :: "'heap \<Rightarrow> addr \<Rightarrow> nat"
+  and heap_read :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> bool"
+  and heap_write :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> 'heap \<Rightarrow> bool"
+
+locale JVM_heap_conf_base =
+  heap_conf_base +
+  constrains empty_heap :: "'heap"
+  and new_obj :: "'heap \<Rightarrow> cname \<Rightarrow> ('heap \<times> addr option)"
+  and new_arr :: "'heap \<Rightarrow> ty \<Rightarrow> nat \<Rightarrow> ('heap \<times> addr option)"
+  and typeof_addr :: "'heap \<Rightarrow> addr \<rightharpoonup> ty"
+  and array_length :: "'heap \<Rightarrow> addr \<Rightarrow> nat"
+  and heap_read :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> bool"
+  and heap_write :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> 'heap \<Rightarrow> bool"
+  and hconf :: "'heap \<Rightarrow> bool"
+  and P :: "jvm_prog"
+
+sublocale JVM_heap_conf_base < JVM_heap_base .
+
+locale JVM_heap_conf_base' =
+  JVM_heap_conf_base +
+  heap +
+  constrains empty_heap :: "'heap"
+  and new_obj :: "'heap \<Rightarrow> cname \<Rightarrow> ('heap \<times> addr option)"
+  and new_arr :: "'heap \<Rightarrow> ty \<Rightarrow> nat \<Rightarrow> ('heap \<times> addr option)"
+  and typeof_addr :: "'heap \<Rightarrow> addr \<rightharpoonup> ty"
+  and array_length :: "'heap \<Rightarrow> addr \<Rightarrow> nat"
+  and heap_read :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> bool"
+  and heap_write :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> 'heap \<Rightarrow> bool"
+  and hconf :: "'heap \<Rightarrow> bool"
+  and P :: "jvm_prog"
+
+sublocale JVM_heap_conf_base' < JVM_heap by(unfold_locales)
+
+locale JVM_heap_conf = 
+  JVM_heap_conf_base' +
+  heap_conf +
+  constrains empty_heap :: "'heap"
+  and new_obj :: "'heap \<Rightarrow> cname \<Rightarrow> ('heap \<times> addr option)"
+  and new_arr :: "'heap \<Rightarrow> ty \<Rightarrow> nat \<Rightarrow> ('heap \<times> addr option)"
+  and typeof_addr :: "'heap \<Rightarrow> addr \<rightharpoonup> ty"
+  and array_length :: "'heap \<Rightarrow> addr \<Rightarrow> nat"
+  and heap_read :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> bool"
+  and heap_write :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> 'heap \<Rightarrow> bool"
+  and hconf :: "'heap \<Rightarrow> bool"
+  and P :: "jvm_prog"
+
+locale JVM_progress =
+  heap_progress +
+  JVM_heap_conf_base' +
+  constrains empty_heap :: "'heap"
+  and new_obj :: "'heap \<Rightarrow> cname \<Rightarrow> ('heap \<times> addr option)"
+  and new_arr :: "'heap \<Rightarrow> ty \<Rightarrow> nat \<Rightarrow> ('heap \<times> addr option)"
+  and typeof_addr :: "'heap \<Rightarrow> addr \<rightharpoonup> ty"
+  and array_length :: "'heap \<Rightarrow> addr \<Rightarrow> nat"
+  and heap_read :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> bool"
+  and heap_write :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> 'heap \<Rightarrow> bool"
+  and hconf :: "'heap \<Rightarrow> bool"
+  and P :: "jvm_prog"
+
+locale JVM_conf_read =
+  heap_conf_read +
+  JVM_heap_conf +
+  constrains empty_heap :: "'heap"
+  and new_obj :: "'heap \<Rightarrow> cname \<Rightarrow> ('heap \<times> addr option)"
+  and new_arr :: "'heap \<Rightarrow> ty \<Rightarrow> nat \<Rightarrow> ('heap \<times> addr option)"
+  and typeof_addr :: "'heap \<Rightarrow> addr \<rightharpoonup> ty"
+  and array_length :: "'heap \<Rightarrow> addr \<Rightarrow> nat"
+  and heap_read :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> bool"
+  and heap_write :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> 'heap \<Rightarrow> bool"
+  and hconf :: "'heap \<Rightarrow> bool"
+  and P :: "jvm_prog"
+
+locale JVM_typesafe =
+  heap_typesafe +
+  JVM_conf_read +
+  JVM_progress +
+  constrains empty_heap :: "'heap"
+  and new_obj :: "'heap \<Rightarrow> cname \<Rightarrow> ('heap \<times> addr option)"
+  and new_arr :: "'heap \<Rightarrow> ty \<Rightarrow> nat \<Rightarrow> ('heap \<times> addr option)"
+  and typeof_addr :: "'heap \<Rightarrow> addr \<rightharpoonup> ty"
+  and array_length :: "'heap \<Rightarrow> addr \<Rightarrow> nat"
+  and heap_read :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> bool"
+  and heap_write :: "'heap \<Rightarrow> addr \<Rightarrow> addr_loc \<Rightarrow> val \<Rightarrow> 'heap \<Rightarrow> bool"
+  and hconf :: "'heap \<Rightarrow> bool"
+  and P :: "jvm_prog"
+
+end
