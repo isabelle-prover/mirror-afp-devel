ed2163fec0e61371a2ecc06e549e62c896e481ff
Unknown <lochbihl@i44pc65.info.uni-karlsruhe.de>
2010-06-10 14:31:20 +0200

    New entry theory Coinductive

---
+++ thys/Coinductive/Coinductive.thy
@@ -0,0 +1,10 @@
+(*  Title:       Coinductive developments in Isabelle/HOL
+    Author:      Andreas Lochbihler
+    Maintainer:  Andreas Lochbihler
+*)
+theory Coinductive imports
+  Coinductive_List_Prefix
+  Quotient_Coinductive_List
+begin
+
+end
