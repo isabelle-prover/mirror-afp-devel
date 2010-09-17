9fce23c5b21df18d58bee3ec380aba185ab8b6ad
griff <unknown>
2010-09-17 14:04:55 +0200

    adapted to actual dependencies

--- thys/Matrix/Matrix.thy
+++ thys/Matrix/Matrix.thy
@@ -26,7 +26,7 @@ with IsaFoR/CeTA. If not, see <http://www.gnu.org/licenses/>.
 header {* Matrices *}
 
 theory Matrix
-imports "../Abstract-Rewriting/Abstract_Rewriting" "../Abstract-Rewriting/SN_Orders"
+imports "../Abstract-Rewriting/SN_Orders"
 begin
 
 text {*
