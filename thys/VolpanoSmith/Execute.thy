6df6cccdfc3b8cb132382fb213e76594b65f8461
bulwahn <unknown>
2010-09-27 15:28:52 +0200

    adapting code_pred invocations to recent changes in Isabelle rev. b1febbbda458; fixed ROOT.ML after modernization (afp 9f72e0b26a37)

--- thys/VolpanoSmith/Execute.thy
+++ thys/VolpanoSmith/Execute.thy
@@ -15,36 +15,36 @@ thm red.equation
 definition [code]: "one_step x = Predicate.the (exec_red x)"
 
 lemmas [code_pred_intro] = typeVal[where lev = Low] typeVal[where lev = High]
-  typeVar[unfolded Predicate.eq_is_eq[symmetric]]
+  typeVar
   typeBinOp1 typeBinOp2[where lev = Low] typeBinOp2[where lev = High] typeBinOp3[where lev = Low]
 
 code_pred (modes: i => i => o => bool as compute_secExprTyping,
   i => i => i => bool as check_secExprTyping) secExprTyping
 proof -
   case secExprTyping
-  from this(1) show thesis
+  from secExprTyping.prems show thesis
   proof
     fix \<Gamma> V lev assume "x = \<Gamma>" "xa = Val V" "xb = lev"
-    from secExprTyping(2-3) this show thesis by (cases lev) auto
+    from secExprTyping(1-2) this show thesis by (cases lev) auto
   next
     fix \<Gamma> Vn lev
     assume "x = \<Gamma>" "xa = Var Vn" "xb = lev" "\<Gamma> Vn = Some lev"
-    from secExprTyping(4) this show thesis by (auto simp add: Predicate.eq_is_eq)
+    from secExprTyping(3) this show thesis by (auto simp add: Predicate.eq_is_eq)
   next
     fix \<Gamma> e1 e2 bop
     assume "x = \<Gamma>" "xa = e1\<guillemotleft>bop\<guillemotright> e2" "xb = Low"
       "\<Gamma> \<turnstile> e1 : Low" "\<Gamma> \<turnstile> e2 : Low"
-    from secExprTyping(5) this show thesis by auto
+    from secExprTyping(4) this show thesis by auto
   next
     fix \<Gamma> e1 e2 lev bop
     assume "x = \<Gamma>" "xa = e1\<guillemotleft>bop\<guillemotright> e2" "xb = High"
     "\<Gamma> \<turnstile> e1 : High" "\<Gamma> \<turnstile> e2 : lev"
-    from secExprTyping(6-7) this show thesis by (cases lev) (auto, blast)
+    from secExprTyping(5-6) this show thesis by (cases lev) (auto, blast)
   next
     fix \<Gamma> e1 e2 lev bop
     assume "x = \<Gamma>" "xa = e1\<guillemotleft>bop\<guillemotright> e2" "xb = High"
     "\<Gamma> \<turnstile> e1 : lev" "\<Gamma> \<turnstile> e2 : High"
-    from secExprTyping(7-8) this show thesis by (cases lev) (auto, blast)
+    from secExprTyping(6-7) this show thesis by (cases lev) (auto, blast)
   qed
 qed
 
@@ -56,33 +56,33 @@ code_pred (modes: i => o => i => bool as compute_secComTyping,
   i => i => i => bool as check_secComTyping) secComTyping
 proof -
   case secComTyping
-  from this(1) show thesis
+  from secComTyping.prems show thesis
   proof
     fix \<Gamma> T assume "x = \<Gamma>" "xa = T" "xb = Skip"
-    from secComTyping(2-3) this show thesis by (cases T) auto
+    from secComTyping(1-2) this show thesis by (cases T) auto
   next
     fix \<Gamma> V T e assume "x = \<Gamma>" "xa = T" "xb = V:=e" "\<Gamma> V = Some High"
-    from secComTyping(4-5) this show thesis by (cases T) (auto, blast)
+    from secComTyping(3-4) this show thesis by (cases T) (auto, blast)
   next
     fix \<Gamma> e V
     assume "x = \<Gamma>" "xa = Low" "xb = V:=e" "\<Gamma> \<turnstile> e : Low" "\<Gamma> V = Some Low"
-    from secComTyping(6) this show thesis by auto
+    from secComTyping(5) this show thesis by auto
   next
     fix \<Gamma> T c1 c2
     assume "x = \<Gamma>" "xa = T" "xb = Seq c1 c2" "\<Gamma>,T \<turnstile> c1" "\<Gamma>,T \<turnstile> c2"
-    from secComTyping(7) this show thesis by auto
+    from secComTyping(6) this show thesis by auto
   next
     fix \<Gamma> b T c
     assume "x = \<Gamma>" "xa = T" "xb = while (b) c" "\<Gamma> \<turnstile> b : T" "\<Gamma>,T \<turnstile> c"
-    from secComTyping(8) this show thesis by auto
+    from secComTyping(7) this show thesis by auto
   next
     fix \<Gamma> b T c1 c2
     assume "x = \<Gamma>" "xa = T" "xb = if (b) c1 else c2" "\<Gamma> \<turnstile> b : T" "\<Gamma>,T \<turnstile> c1" "\<Gamma>,T \<turnstile> c2"
-    from secComTyping(9) this show thesis by blast
+    from secComTyping(8) this show thesis by blast
   next
     fix \<Gamma> c
     assume "x = \<Gamma>" "xa = Low" "xb = c" "\<Gamma>,High \<turnstile> c"
-    from secComTyping(10) this show thesis by blast
+    from secComTyping(9) this show thesis by blast
   qed
 qed
 
