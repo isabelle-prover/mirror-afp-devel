b2bb02be7deb598c3b3cc81a386b86ca5cf51f88
haftmann <unknown>
2010-11-03 10:45:13 +0100

    polyml_as_definition does not require explicit dependencies on external ML files

--- thys/Flyspeck-Tame/Arch.thy
+++ thys/Flyspeck-Tame/Arch.thy
@@ -6,7 +6,6 @@ theory Arch
 imports Main Efficient_Nat
 begin
 
-(*
 setup {*
 let val T = @{typ "nat list list list"}
 in
@@ -17,21 +16,6 @@ in
                        "Archives/Pent.ML", "Archives/Hex.ML"])
 end
 *}
-*)
-
-ML {* Secure.use_file ML_Env.local_context false "Archives/Tri.ML" *}
-ML {* Secure.use_file ML_Env.local_context false "Archives/Quad.ML" *}
-ML {* Secure.use_file ML_Env.local_context false "Archives/Pent.ML" *}
-ML {* Secure.use_file ML_Env.local_context false "Archives/Hex.ML" *}
-
-consts
-Tri :: "nat list list list"
-Quad :: "nat list list list"
-Pent :: "nat list list list"
-Hex :: "nat list list list"
-
-code_const Tri and Quad and Pent and Hex
-  (SML "Tri" and "Quad" and "Pent" and "Hex")
 
 text {* The definition of these constants is only ever needed at the ML level
 when running the eval proof method. *}
