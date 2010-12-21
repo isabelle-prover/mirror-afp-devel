14ecdee362a81e75035b90e029b531db3ff0ac19
Brian Huffman <brianh@cs.pdx.edu>
2010-12-21 08:08:16 -0800

    Stream-Fusion: use new Int_Discrete theory from HOLCF/Library for instance int :: predomain

--- thys/Stream-Fusion/LazyList.thy
+++ thys/Stream-Fusion/LazyList.thy
@@ -1,55 +1,8 @@
 header {* Lazy Lists *}
 
 theory LazyList
-imports HOLCF
+imports HOLCF Int_Discrete
 begin
-
-text {* Discrete cpo instance for @{typ int}. *}
-
-instantiation int :: discrete_cpo
-begin
-
-definition below_int_def:
-  "(x::int) \<sqsubseteq> y \<longleftrightarrow> x = y"
-
-instance proof
-qed (rule below_int_def)
-
-end
-
-text {*
-  TODO: implement a command to automate discrete predomain instances.
-*}
-
-instantiation int :: predomain
-begin
-
-definition
-  "(liftemb :: int u \<rightarrow> udom) \<equiv> liftemb oo u_map\<cdot>(\<Lambda> x. Discr x)"
-
-definition
-  "(liftprj :: udom \<rightarrow> int u) \<equiv> u_map\<cdot>(\<Lambda> y. undiscr y) oo liftprj"
-
-definition
-  "liftdefl \<equiv> (\<lambda>(t::int itself). LIFTDEFL(int discr))"
-
-instance proof
-  show "ep_pair liftemb (liftprj :: udom \<rightarrow> int u)"
-    unfolding liftemb_int_def liftprj_int_def
-    apply (rule ep_pair.intro)
-    apply (simp add: cfcomp1 u_map_map predomain_ep [THEN ep_pair.e_inverse])
-    apply (simp add: ID_def [symmetric] u_map_ID)
-    apply (simp add: cfcomp1 u_map_map ID_def [symmetric] u_map_ID)
-    apply (rule predomain_ep [THEN ep_pair.e_p_below])
-    done
-  show "cast\<cdot>LIFTDEFL(int) = liftemb oo (liftprj :: udom \<rightarrow> int u)"
-    unfolding liftemb_int_def liftprj_int_def liftdefl_int_def
-    apply (simp add: cast_liftdefl cfcomp1 u_map_map)
-    apply (simp add: ID_def [symmetric] u_map_ID)
-    done
-qed
-
-end
 
 domain 'a LList = LNil | LCons (lazy 'a) (lazy "'a LList")
  
