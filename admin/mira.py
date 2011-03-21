b342de087444bb3fb4cd8ba39c5e0c9b56af98e3
krauss <unknown>
2011-03-21 21:08:24 +0100

    added judgement day configurations

--- admin/mira.py
+++ admin/mira.py
@@ -56,3 +56,31 @@ def AFP_Verified_Prover(env, case, paths, dep_paths, playground):
 def AFP_almost_all(*args):
     """All AFP sessions except Flyspeck-Tame"""
     return (True, 'ok', {}, {}, None)
+
+
+# AFP-based Judgement Day configurations
+
+@configuration(repos = [Isabelle, AFP], deps = [(HOL, [0])])
+def JD_Arrow(*args):
+    """Judgement Day regression suite Arrow"""
+    return isabelle.judgement_day('AFP/thys/ArrowImpossibilityGS/Thys', 'Arrow_Order', 'prover_timeout=10', *args)
+
+@configuration(repos = [Isabelle, AFP], deps = [(HOL, [0])])
+def JD_FFT(*args):
+    """Judgement Day regression suite FFT"""
+    return isabelle.judgement_day('AFP/thys/FFT', 'FFT', 'prover_timeout=10', *args)
+
+@configuration(repos = [Isabelle, AFP], deps = [(HOL, [0])])
+def JD_Jinja(*args):
+    """Judgement Day regression suite Jinja"""
+    return isabelle.judgement_day('AFP/thys/Jinja/J', 'TypeSafe', 'prover_timeout=10', *args)
+
+@configuration(repos = [Isabelle, AFP], deps = [(HOL, [0])])
+def JD_QE(*args):
+    """Judgement Day regression suite QE"""
+    return isabelle.judgement_day('AFP/thys/LinearQuantifierElim/Thys', 'QEpres', 'prover_timeout=10', *args)
+
+@configuration(repos = [Isabelle, AFP], deps = [(HOL, [0])])
+def JD_S2S(*args):
+    """Judgement Day regression suite S2S"""
+    return isabelle.judgement_day('AFP/thys/SumSquares', 'TwoSquares', 'prover_timeout=10', *args)
