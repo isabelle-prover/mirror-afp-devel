9611c9fc480b6609888d8c07d1dd8104ca085689
Christian Urban <urbanc@in.tum.de>
2011-08-27 23:16:37 +0100

    added author and removed dead code

--- thys/Regular-Sets/Derivatives.thy
+++ thys/Regular-Sets/Derivatives.thy
@@ -1,3 +1,7 @@
+(* Author: Christian Urban *)
+
+header "Derivatives of regular expressions"
+
 theory Derivatives
 imports Regular_Exp
 begin
@@ -68,14 +72,6 @@ lemma Derivs_simps [simp]:
   and   "Derivs (c # s) A = Derivs s (Deriv c A)"
   and   "Derivs (s1 @ s2) A = Derivs s2 (Derivs s1 A)"
 unfolding Derivs_def Deriv_def by auto
-
-(*
-lemma Deriv_insert_eps[simp]: 
-"Deriv a (insert [] A) = Deriv a A"
-by (auto simp: Deriv_def)
-*)
-
-
 
 subsection {* Brozowsky's derivatives of regular expressions *}
 
@@ -381,49 +377,5 @@ by (simp add: finite_pderivs_lang_UNIV1)
 lemma finite_pderivs_lang:
   shows "finite (pderivs_lang A r)"
 by (metis finite_pderivs_lang_UNIV pderivs_lang_subset rev_finite_subset subset_UNIV)
-
-(*
-text {* Relating the Myhill-Nerode relation with left-quotients. *}
-
-lemma MN_Rel_Derivs:
-  shows "x \<approx>A y \<longleftrightarrow> Derivs x A = Derivs y A"
-unfolding Derivs_def str_eq_def
-by auto
-
-subsection {*
-  The second direction of the Myhill-Nerode theorem using
-  partial derivivatives.
-*}
-
-lemma Myhill_Nerode3:
-  fixes r::"'a rexp"
-  shows "finite (UNIV // \<approx>(lang r))"
-proof -
-  have "finite (UNIV // =(\<lambda>x. pderivs x r)=)"
-  proof - 
-    have "range (\<lambda>x. pderivs x r) \<subseteq> Pow (pderivs_lang UNIV r)"
-      unfolding pderivs_lang_def by auto
-    moreover 
-    have "finite (Pow (pderivs_lang UNIV r))" by (simp add: finite_pderivs_lang)
-    ultimately
-    have "finite (range (\<lambda>x. pderivs x r))"
-      by (simp add: finite_subset)
-    then show "finite (UNIV // =(\<lambda>x. pderivs x r)=)" 
-      by (rule finite_eq_tag_rel)
-  qed
-  moreover 
-  have "=(\<lambda>x. pderivs x r)= \<subseteq> \<approx>(lang r)"
-    unfolding tag_eq_def
-    by (auto simp add: MN_Rel_Derivs Derivs_pderivs)
-  moreover 
-  have "equiv UNIV =(\<lambda>x. pderivs x r)="
-  and  "equiv UNIV (\<approx>(lang r))"
-    unfolding equiv_def refl_on_def sym_def trans_def
-    unfolding tag_eq_def str_eq_def
-    by auto
-  ultimately show "finite (UNIV // \<approx>(lang r))" 
-    by (rule refined_partition_finite)
-qed
-*)
 
 end
