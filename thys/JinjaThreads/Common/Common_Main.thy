73409788214cb75f979ccc6a0b1c932458b2b606
haftmann <unknown>
2011-11-21 22:48:56 +0100

    main theories for each area

---
+++ thys/JinjaThreads/Common/Common_Main.thy
@@ -0,0 +1,12 @@
+theory Common_Main
+imports
+  Aux
+  "../Framework/FWProgress"
+  "../Framework/FWBisimDeadlock"
+  "../Framework/FWInitFinLift"
+  ExternalCallWF
+  ConformThreaded
+  BinOp
+begin
+
+end
---
+++ thys/JinjaThreads/Execute/Execute_Main.thy
@@ -0,0 +1,9 @@
+theory Execute_Main
+imports
+  J_Execute
+  JVM_Execute
+  JVM_Execute2
+  PCompilerRefine
+begin
+
+end
