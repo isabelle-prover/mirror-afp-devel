60f1725266a807f7e4b1a7ed886e7866bfd106d9
haftmann <unknown>
2011-12-29 15:15:03 +0100

    moved pred/set conversions to distribution

--- thys/JinjaThreads/Basic/Set_Monad.thy
+++ thys/JinjaThreads/Basic/Set_Monad.thy
@@ -4,26 +4,19 @@ imports
   "~~/src/HOL/Library/Monad_Syntax"
 begin
 
-definition pred_of_set :: "'a set \<Rightarrow> 'a Predicate.pred"
-  where [code del]: "pred_of_set = Predicate.Pred \<circ> (\<lambda>A x. Set.member x A)"
-
-lemma eval_pred_of_set [simp]: "Predicate.eval (pred_of_set A) = (\<lambda>x. Set.member x A)"
-  by(simp add: pred_of_set_def)
-
-definition of_pred :: "'a Predicate.pred \<Rightarrow> 'a set"
-  where "of_pred = Collect \<circ> Predicate.eval"
-
-definition of_seq :: "'a Predicate.seq \<Rightarrow> 'a set"
-  where "of_seq = of_pred \<circ> Predicate.pred_of_seq"
-
-lemmas bind_def = Set.bind_def
-lemmas bind_bind = Set.bind_bind
-lemmas empty_bind = Set.empty_bind
-lemmas bind_const = Set.bind_const
-
 lemma member_SUPR: (* FIXME delete candidate: should be subsumed by default simpset as soon as SUP_apply is included *)
   "x \<in> UNION A f = (SUP B:A. (\<lambda>x. x \<in> f B)) x"
-  by auto -- {* dangerous as simp rule: @{const UNION} is standard operation *}
+  by auto
+
+abbreviation (input) "of_pred == Set.set_of_pred" (* FIXME delte alias *)
+abbreviation (input) "of_seq == Set.set_of_seq" (* FIXME delte alias *)
+
+lemmas bind_def = Set.bind_def (* FIXME delte alias *)
+lemmas bind_bind = Set.bind_bind (* FIXME delte alias *)
+lemmas empty_bind = Set.empty_bind (* FIXME delte alias *)
+lemmas bind_const = Set.bind_const (* FIXME delte alias *)
+lemmas member_set_of_pred = Set.member_of_pred (* FIXME delte alias *)
+lemmas member_set_of_seq = Set.member_of_seq (* FIXME delte alias *)
 
 definition single :: "'a \<Rightarrow> 'a set"
   where "single a = {a}"
@@ -61,39 +54,8 @@ lemma single_sup_simps [simp, code_unfold]:
   and sup_single: "sup A (single a) = insert a A"
   by (unfold set_eq_iff) auto
 
-lemma member_of_pred [simp]:
-  "x \<in> of_pred P = Predicate.eval P x"
-  by (simp add: of_pred_def)
-
-lemma member_of_seq [simp]:
-  "x \<in> of_seq xq = Predicate.member xq x"
-  by (simp add: of_seq_def eval_member)
-
-lemma of_pred_code [code]:
-  "of_pred (Predicate.Seq f) = (case f () of
-     Predicate.Empty \<Rightarrow> {}
-   | Predicate.Insert x P \<Rightarrow> insert x (of_pred P)
-   | Predicate.Join P xq \<Rightarrow> sup (of_pred P) (of_seq xq))"
-   by (auto split: seq.split simp add: Predicate.Seq_def of_pred_def eval_member set_eq_iff)
-
-lemma of_seq_code [code]:
-  "of_seq Predicate.Empty = {}"
-  "of_seq (Predicate.Insert x P) = insert x (of_pred P)"
-  "of_seq (Predicate.Join P xq) = sup (of_pred P) (of_seq xq)"
-  by (auto simp add: of_seq_def of_pred_def set_eq_iff)
-
 lemma single_code [code]:
   "single a = set [a]"
   by (simp add: set_eq_iff)
-
-lemma pred_of_cset_code [code]:
-  "pred_of_set (set xs) = foldr sup (map Predicate.single xs) bot"
-proof -
-  have "pred_of_set (set xs) = Predicate.Pred (\<lambda>x. x \<in> set xs)"
-    by (auto simp add: pred_of_set_def)
-  moreover have "foldr sup (map Predicate.single xs) bot = \<dots>"
-    by (induct xs) (auto simp add: bot_pred_def intro: pred_eqI)
-  ultimately show ?thesis by simp
-qed
 
 end
