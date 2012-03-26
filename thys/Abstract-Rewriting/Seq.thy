d9207bd8aa4b3f9aff8e963567acdbd4173fe379
griff <unknown>
2012-03-26 15:19:04 +0900

    added license + corrected typo

--- thys/Abstract-Rewriting/Seq.thy
+++ thys/Abstract-Rewriting/Seq.thy
@@ -1,3 +1,27 @@
+(*  Title:       Infinite Sequences
+    Author:      Christian Sternagel <c-sterna@jaist.ac.jp>
+                 René Thiemann       <rene.thiemann@uibk.ac.at>
+    Maintainer:  Christian Sternagel and René Thiemann
+    License:     LGPL
+*)
+
+(*
+Copyright 2012 Christian Sternagel, René Thiemann
+
+This file is part of IsaFoR/CeTA.
+
+IsaFoR/CeTA is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+IsaFoR/CeTA is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with IsaFoR/CeTA. If not, see <http://www.gnu.org/licenses/>.
+*)
 header {* Infinite Sequences *}
 theory Seq
 imports
@@ -162,7 +186,7 @@ qed
 subsection {* Predicates on Natural Numbers *}
 
 text {*If some property holds for infinitely many natural numbers, obtain
-a function that points to these numbres.*}
+an index function that points to these numbers in increasing order.*}
 
 locale infinitely_many =
   fixes p :: "nat \<Rightarrow> bool"
