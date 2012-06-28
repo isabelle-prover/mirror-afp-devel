a0a215f713fed9d7bbe722c7103fc2d0be8ee40b
nipkow <unknown>
2012-06-28 12:57:56 +0200

    new entry Tycon

---
+++ thys/Tycon/Lazy_List_Monad.thy
@@ -0,0 +1,200 @@
+header {* Lazy list monad *}
+
+theory Lazy_List_Monad
+imports Monad_Zero_Plus
+begin
+
+text {* To illustrate the general process of defining a new type
+constructor, we formalize the datatype of lazy lists. Below are the
+Haskell datatype definition and class instances. *}
+
+text_raw {*
+\begin{verbatim}
+data List a = Nil | Cons a (List a)
+
+instance Functor List where
+  fmap f Nil = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+instance Monad List where
+  return x        = Cons x Nil
+  Nil       >>= k = Nil
+  Cons x xs >>= k = mplus (k x) (xs >>= k)
+
+instance MonadZero List where
+  mzero = Nil
+
+instance MonadPlus List where
+  mplus Nil         ys = ys
+  mplus (Cons x xs) ys = Cons x (mplus xs ys)
+\end{verbatim}
+*}
+
+subsection {* Type definition *}
+
+text {* The first step is to register the datatype definition with
+@{text tycondef}. *}
+
+tycondef 'a\<cdot>llist = LNil | LCons (lazy "'a") (lazy "'a\<cdot>llist")
+
+text {* The @{text tycondef} command generates lots of theorems
+automatically, but there are a few more involving @{text coerce} and
+@{text fmapU} that we still need to prove manually. These proofs could
+be automated in a later version of @{text tycondef}. *}
+
+lemma coerce_llist_abs [simp]: "coerce\<cdot>(llist_abs\<cdot>x) = llist_abs\<cdot>(coerce\<cdot>x)"
+apply (simp add: llist_abs_def coerce_def)
+apply (simp add: emb_prj_emb prj_emb_prj DEFL_eq_llist)
+done
+
+lemma coerce_LNil [simp]: "coerce\<cdot>LNil = LNil"
+unfolding LNil_def by simp
+
+lemma coerce_LCons [simp]: "coerce\<cdot>(LCons\<cdot>x\<cdot>xs) = LCons\<cdot>(coerce\<cdot>x)\<cdot>(coerce\<cdot>xs)"
+unfolding LCons_def by simp
+
+lemma fmapU_llist_simps [simp]:
+  "fmapU\<cdot>f\<cdot>(\<bottom>::udom\<cdot>llist) = \<bottom>"
+  "fmapU\<cdot>f\<cdot>LNil = LNil"
+  "fmapU\<cdot>f\<cdot>(LCons\<cdot>x\<cdot>xs) = LCons\<cdot>(f\<cdot>x)\<cdot>(fmapU\<cdot>f\<cdot>xs)"
+unfolding fmapU_llist_def llist_map_def
+apply (subst fix_eq, simp)
+apply (subst fix_eq, simp add: LNil_def)
+apply (subst fix_eq, simp add: LCons_def)
+done
+
+subsection {* Class instances *}
+
+text {* The @{text tycondef} command defines @{text fmapU} for us and
+proves a @{text prefunctor} class instance automatically. For the
+@{text functor} instance we only need to prove the composition law,
+which we can do by induction. *}
+
+instance llist :: functor
+proof
+  fix f g and xs :: "udom\<cdot>llist"
+  show "fmapU\<cdot>f\<cdot>(fmapU\<cdot>g\<cdot>xs) = fmapU\<cdot>(\<Lambda> x. f\<cdot>(g\<cdot>x))\<cdot>xs"
+    by (induct xs rule: llist.induct) simp_all
+qed
+
+text {* For the other class instances, we need to provide definitions
+for a few constants: @{text returnU}, @{text bindU} @{text zeroU}, and
+@{text plusU}. We can use ordinary commands like @{text definition}
+and @{text fixrec} for this purpose. Finally we prove the class
+axioms, along with a few helper lemmas, using ordinary proof
+procedures like induction. *}
+
+instantiation llist :: monad_zero_plus
+begin
+
+fixrec plusU_llist :: "udom\<cdot>llist \<rightarrow> udom\<cdot>llist \<rightarrow> udom\<cdot>llist"
+  where "plusU_llist\<cdot>LNil\<cdot>ys = ys"
+  | "plusU_llist\<cdot>(LCons\<cdot>x\<cdot>xs)\<cdot>ys = LCons\<cdot>x\<cdot>(plusU_llist\<cdot>xs\<cdot>ys)"
+
+lemma plusU_llist_strict [simp]: "plusU\<cdot>\<bottom>\<cdot>ys = (\<bottom>::udom\<cdot>llist)"
+by fixrec_simp
+
+fixrec bindU_llist :: "udom\<cdot>llist \<rightarrow> (udom \<rightarrow> udom\<cdot>llist) \<rightarrow> udom\<cdot>llist"
+  where "bindU_llist\<cdot>LNil\<cdot>k = LNil"
+  | "bindU_llist\<cdot>(LCons\<cdot>x\<cdot>xs)\<cdot>k = plusU\<cdot>(k\<cdot>x)\<cdot>(bindU_llist\<cdot>xs\<cdot>k)"
+
+lemma bindU_llist_strict [simp]: "bindU\<cdot>\<bottom>\<cdot>k = (\<bottom>::udom\<cdot>llist)"
+by fixrec_simp
+
+definition zeroU_llist_def:
+  "zeroU = LNil"
+
+definition returnU_llist_def:
+  "returnU = (\<Lambda> x. LCons\<cdot>x\<cdot>LNil)"
+
+lemma plusU_LNil_right: "plusU\<cdot>xs\<cdot>LNil = xs"
+by (induct xs rule: llist.induct) simp_all
+
+lemma plusU_llist_assoc:
+  fixes xs ys zs :: "udom\<cdot>llist"
+  shows "plusU\<cdot>(plusU\<cdot>xs\<cdot>ys)\<cdot>zs = plusU\<cdot>xs\<cdot>(plusU\<cdot>ys\<cdot>zs)"
+by (induct xs rule: llist.induct) simp_all
+
+lemma bindU_plusU_llist:
+  fixes xs ys :: "udom\<cdot>llist" shows
+  "bindU\<cdot>(plusU\<cdot>xs\<cdot>ys)\<cdot>f = plusU\<cdot>(bindU\<cdot>xs\<cdot>f)\<cdot>(bindU\<cdot>ys\<cdot>f)"
+by (induct xs rule: llist.induct) (simp_all add: plusU_llist_assoc)
+
+instance proof
+  fix x :: "udom"
+  fix f :: "udom \<rightarrow> udom"
+  fix h k :: "udom \<rightarrow> udom\<cdot>llist"
+  fix xs ys zs :: "udom\<cdot>llist"
+  show "fmapU\<cdot>f\<cdot>xs = bindU\<cdot>xs\<cdot>(\<Lambda> x. returnU\<cdot>(f\<cdot>x))"
+    by (induct xs rule: llist.induct, simp_all add: returnU_llist_def)
+  show "bindU\<cdot>(returnU\<cdot>x)\<cdot>k = k\<cdot>x"
+    by (simp add: returnU_llist_def plusU_LNil_right)
+  show "bindU\<cdot>(bindU\<cdot>xs\<cdot>h)\<cdot>k = bindU\<cdot>xs\<cdot>(\<Lambda> x. bindU\<cdot>(h\<cdot>x)\<cdot>k)"
+    by (induct xs rule: llist.induct)
+       (simp_all add: bindU_plusU_llist)
+  show "bindU\<cdot>(plusU\<cdot>xs\<cdot>ys)\<cdot>k = plusU\<cdot>(bindU\<cdot>xs\<cdot>k)\<cdot>(bindU\<cdot>ys\<cdot>k)"
+    by (induct xs rule: llist.induct)
+       (simp_all add: plusU_llist_assoc)
+  show "plusU\<cdot>(plusU\<cdot>xs\<cdot>ys)\<cdot>zs = plusU\<cdot>xs\<cdot>(plusU\<cdot>ys\<cdot>zs)"
+    by (rule plusU_llist_assoc)
+  show "bindU\<cdot>zeroU\<cdot>k = zeroU"
+    by (simp add: zeroU_llist_def)
+  show "fmapU\<cdot>f\<cdot>(plusU\<cdot>xs\<cdot>ys) = plusU\<cdot>(fmapU\<cdot>f\<cdot>xs)\<cdot>(fmapU\<cdot>f\<cdot>ys)"
+    by (induct xs rule: llist.induct) simp_all
+  show "fmapU\<cdot>f\<cdot>zeroU = (zeroU :: udom\<cdot>llist)"
+    by (simp add: zeroU_llist_def)
+  show "plusU\<cdot>zeroU\<cdot>xs = xs"
+    by (simp add: zeroU_llist_def)
+  show "plusU\<cdot>xs\<cdot>zeroU = xs"
+    by (simp add: zeroU_llist_def plusU_LNil_right)
+qed
+
+end
+
+subsection {* Transfer properties to polymorphic versions *}
+
+text {* After proving the class instances, there is still one more
+step: We must transfer all the list-specific lemmas about the
+monomorphic constants (e.g., @{text fmapU} and @{text bindU}) to the
+corresponding polymorphic constants (@{text fmap} and @{text bind}).
+These lemmas primarily consist of the defining equations for each
+constant. The polymorphic constants are defined using @{text coerce},
+so the proofs proceed by unfolding the definitions and simplifying
+with the @{text coerce_simp} rules. *}
+
+lemma fmap_llist_simps [simp]:
+  "fmap\<cdot>f\<cdot>(\<bottom>::'a\<cdot>llist) = \<bottom>"
+  "fmap\<cdot>f\<cdot>LNil = LNil"
+  "fmap\<cdot>f\<cdot>(LCons\<cdot>x\<cdot>xs) = LCons\<cdot>(f\<cdot>x)\<cdot>(fmap\<cdot>f\<cdot>xs)"
+unfolding fmap_def by simp_all
+
+lemma mplus_llist_simps [simp]:
+  "mplus\<cdot>(\<bottom>::'a\<cdot>llist)\<cdot>ys = \<bottom>"
+  "mplus\<cdot>LNil\<cdot>ys = ys"
+  "mplus\<cdot>(LCons\<cdot>x\<cdot>xs)\<cdot>ys = LCons\<cdot>x\<cdot>(mplus\<cdot>xs\<cdot>ys)"
+unfolding mplus_def by simp_all
+
+lemma bind_llist_simps [simp]:
+  "bind\<cdot>(\<bottom>::'a\<cdot>llist)\<cdot>f = \<bottom>"
+  "bind\<cdot>LNil\<cdot>f = LNil"
+  "bind\<cdot>(LCons\<cdot>x\<cdot>xs)\<cdot>f = mplus\<cdot>(f\<cdot>x)\<cdot>(bind\<cdot>xs\<cdot>f)"
+unfolding bind_def mplus_def
+by (simp_all add: coerce_simp)
+
+lemma return_llist_def:
+  "return = (\<Lambda> x. LCons\<cdot>x\<cdot>LNil)"
+unfolding return_def returnU_llist_def
+by (simp add: coerce_simp)
+
+lemma mzero_llist_def:
+  "mzero = LNil"
+unfolding mzero_def zeroU_llist_def
+by simp
+
+lemma join_llist_simps [simp]:
+  "join\<cdot>(\<bottom>::'a\<cdot>llist\<cdot>llist) = \<bottom>"
+  "join\<cdot>LNil = LNil"
+  "join\<cdot>(LCons\<cdot>xs\<cdot>xss) = mplus\<cdot>xs\<cdot>(join\<cdot>xss)"
+unfolding join_def by simp_all
+
+end
---
+++ thys/Tycon/Monad.thy
@@ -0,0 +1,188 @@
+header {* Monad Class *}
+
+theory Monad
+imports Functor
+begin
+
+subsection {* Class definition *}
+
+text {* In Haskell, class \emph{Monad} is defined as follows: *}
+
+text_raw {*
+\begin{verbatim}
+class Monad m where
+  return :: a -> m a
+  (>>=) :: m a -> (a -> m b) -> m b
+\end{verbatim}
+*}
+
+text {* We formalize class @{text monad} in a manner similar to the
+@{text functor} class: We fix monomorphic versions of the class
+constants, replacing type variables with @{text udom}, and assume
+monomorphic versions of the class axioms. *}
+
+text {* Because the monad laws imply the composition rule for @{text
+fmap}, we declare @{text prefunctor} as the superclass, and separately
+prove a subclass relationship with @{text functor}. *}
+
+class monad = prefunctor +
+  fixes returnU :: "udom \<rightarrow> udom\<cdot>'a::tycon"
+  fixes bindU :: "udom\<cdot>'a \<rightarrow> (udom \<rightarrow> udom\<cdot>'a) \<rightarrow> udom\<cdot>'a"
+  assumes fmapU_eq_bindU:
+    "\<And>f xs. fmapU\<cdot>f\<cdot>xs = bindU\<cdot>xs\<cdot>(\<Lambda> x. returnU\<cdot>(f\<cdot>x))"
+  assumes bindU_returnU:
+    "\<And>f x. bindU\<cdot>(returnU\<cdot>x)\<cdot>f = f\<cdot>x"
+  assumes bindU_bindU:
+    "\<And>xs f g. bindU\<cdot>(bindU\<cdot>xs\<cdot>f)\<cdot>g = bindU\<cdot>xs\<cdot>(\<Lambda> x. bindU\<cdot>(f\<cdot>x)\<cdot>g)"
+
+instance monad \<subseteq> functor
+proof
+  fix f g :: "udom \<rightarrow> udom" and xs :: "udom\<cdot>'a"
+  show "fmapU\<cdot>f\<cdot>(fmapU\<cdot>g\<cdot>xs) = fmapU\<cdot>(\<Lambda> x. f\<cdot>(g\<cdot>x))\<cdot>xs"
+    by (simp add: fmapU_eq_bindU bindU_bindU bindU_returnU)
+qed
+
+text {* As with @{text fmap}, we define the polymorphic @{text return}
+and @{text bind} by coercion from the monomorphic @{text returnU} and
+@{text bindU}. *}
+
+definition return :: "'a \<rightarrow> 'a\<cdot>'m::monad"
+  where "return = coerce\<cdot>(returnU :: udom \<rightarrow> udom\<cdot>'m)"
+
+definition bind :: "'a\<cdot>'m::monad \<rightarrow> ('a \<rightarrow> 'b\<cdot>'m) \<rightarrow> 'b\<cdot>'m"
+  where "bind = coerce\<cdot>(bindU :: udom\<cdot>'m \<rightarrow> _)"
+
+abbreviation bind_syn :: "'a\<cdot>'m::monad \<Rightarrow> ('a \<rightarrow> 'b\<cdot>'m) \<Rightarrow> 'b\<cdot>'m" (infixl "\<guillemotright>=" 55)
+  where "m \<guillemotright>= f \<equiv> bind\<cdot>m\<cdot>f"
+
+subsection {* Naturality of bind and return *}
+
+text {* The three class axioms imply naturality properties of @{text
+returnU} and @{text bindU}, i.e., that both commute with @{text
+fmapU}. *}
+
+lemma fmapU_returnU [coerce_simp]:
+  "fmapU\<cdot>f\<cdot>(returnU\<cdot>x) = returnU\<cdot>(f\<cdot>x)"
+by (simp add: fmapU_eq_bindU bindU_returnU)
+
+lemma fmapU_bindU [coerce_simp]:
+  "fmapU\<cdot>f\<cdot>(bindU\<cdot>m\<cdot>k) = bindU\<cdot>m\<cdot>(\<Lambda> x. fmapU\<cdot>f\<cdot>(k\<cdot>x))"
+by (simp add: fmapU_eq_bindU bindU_bindU)
+
+lemma bindU_fmapU:
+  "bindU\<cdot>(fmapU\<cdot>f\<cdot>xs)\<cdot>k = bindU\<cdot>xs\<cdot>(\<Lambda> x. k\<cdot>(f\<cdot>x))"
+by (simp add: fmapU_eq_bindU bindU_returnU bindU_bindU)
+
+subsection {* Polymorphic versions of class assumptions *}
+
+lemma monad_fmap:
+  fixes xs :: "'a\<cdot>'m::monad" and f :: "'a \<rightarrow> 'b"
+  shows "fmap\<cdot>f\<cdot>xs = xs \<guillemotright>= (\<Lambda> x. return\<cdot>(f\<cdot>x))"
+unfolding bind_def return_def fmap_def
+by (simp add: coerce_simp fmapU_eq_bindU bindU_returnU)
+
+lemma monad_left_unit [simp]: "(return\<cdot>x \<guillemotright>= f) = (f\<cdot>x)"
+unfolding bind_def return_def
+by (simp add: coerce_simp bindU_returnU)
+
+lemma bind_bind:
+  fixes m :: "'a\<cdot>'m::monad"
+  shows "((m \<guillemotright>= f) \<guillemotright>= g) = (m \<guillemotright>= (\<Lambda> x. f\<cdot>x \<guillemotright>= g))"
+unfolding bind_def
+by (simp add: coerce_simp bindU_bindU)
+
+subsection {* Derived rules *}
+
+text {* The following properties can be derived using only the
+abstract monad laws. *}
+
+lemma monad_right_unit [simp]: "(m \<guillemotright>= return) = m"
+ apply (subgoal_tac "fmap\<cdot>ID\<cdot>m = m")
+  apply (simp only: monad_fmap)
+  apply (simp add: eta_cfun)
+ apply simp
+done
+
+lemma fmap_return: "fmap\<cdot>f\<cdot>(return\<cdot>x) = return\<cdot>(f\<cdot>x)"
+by (simp add: monad_fmap)
+
+lemma fmap_bind: "fmap\<cdot>f\<cdot>(bind\<cdot>xs\<cdot>k) = bind\<cdot>xs\<cdot>(\<Lambda> x. fmap\<cdot>f\<cdot>(k\<cdot>x))"
+by (simp add: monad_fmap bind_bind)
+
+lemma bind_fmap: "bind\<cdot>(fmap\<cdot>f\<cdot>xs)\<cdot>k = bind\<cdot>xs\<cdot>(\<Lambda> x. k\<cdot>(f\<cdot>x))"
+by (simp add: monad_fmap bind_bind)
+
+text {* Bind is strict in its first argument, if its second argument
+is a strict function. *}
+
+lemma bind_strict:
+  assumes "k\<cdot>\<bottom> = \<bottom>" shows "\<bottom> \<guillemotright>= k = \<bottom>"
+proof -
+  have "\<bottom> \<guillemotright>= k \<sqsubseteq> return\<cdot>\<bottom> \<guillemotright>= k"
+    by (intro monofun_cfun below_refl minimal)
+  thus "\<bottom> \<guillemotright>= k = \<bottom>"
+    by (simp add: assms)
+qed
+
+lemma congruent_bind:
+  "(\<forall>m. m \<guillemotright>= k1 = m \<guillemotright>= k2) = (k1 = k2)"
+ apply (safe, rule cfun_eqI)
+ apply (drule_tac x="return\<cdot>x" in spec, simp)
+done
+
+subsection {* Laws for join *}
+
+definition join :: "('a\<cdot>'m)\<cdot>'m \<rightarrow> 'a\<cdot>'m::monad"
+  where "join \<equiv> \<Lambda> m. m \<guillemotright>= (\<Lambda> x. x)"
+
+lemma join_fmap_fmap: "join\<cdot>(fmap\<cdot>(fmap\<cdot>f)\<cdot>xss) = fmap\<cdot>f\<cdot>(join\<cdot>xss)"
+by (simp add: join_def monad_fmap bind_bind)
+
+lemma join_return: "join\<cdot>(return\<cdot>xs) = xs"
+by (simp add: join_def)
+
+lemma join_fmap_return: "join\<cdot>(fmap\<cdot>return\<cdot>xs) = xs"
+by (simp add: join_def monad_fmap eta_cfun bind_bind)
+
+lemma join_fmap_join: "join\<cdot>(fmap\<cdot>join\<cdot>xsss) = join\<cdot>(join\<cdot>xsss)"
+by (simp add: join_def monad_fmap bind_bind)
+
+lemma bind_def2: "m \<guillemotright>= k = join\<cdot>(fmap\<cdot>k\<cdot>m)"
+by (simp add: join_def monad_fmap eta_cfun bind_bind)
+
+subsection {* Equivalence of monad laws and fmap/join laws *}
+
+lemma "(return\<cdot>x \<guillemotright>= f) = (f\<cdot>x)"
+by (simp only: bind_def2 fmap_return join_return)
+
+lemma "(m \<guillemotright>= return) = m"
+by (simp only: bind_def2 join_fmap_return)
+
+lemma "((m \<guillemotright>= f) \<guillemotright>= g) = (m \<guillemotright>= (\<Lambda> x. f\<cdot>x \<guillemotright>= g))"
+ apply (simp only: bind_def2)
+ apply (subgoal_tac "join\<cdot>(fmap\<cdot>g\<cdot>(join\<cdot>(fmap\<cdot>f\<cdot>m))) =
+    join\<cdot>(fmap\<cdot>join\<cdot>(fmap\<cdot>(fmap\<cdot>g)\<cdot>(fmap\<cdot>f\<cdot>m)))")
+  apply (simp add: fmap_fmap)
+ apply (simp add: join_fmap_join join_fmap_fmap)
+done
+
+subsection {* Simplification of coercions *}
+
+text {* We configure rewrite rules that push coercions inwards, and
+reduce them to coercions on simpler types. *}
+
+lemma coerce_return [coerce_simp]:
+  "COERCE('a\<cdot>'m,'b\<cdot>'m::monad)\<cdot>(return\<cdot>x) = return\<cdot>(COERCE('a,'b)\<cdot>x)"
+by (simp add: coerce_functor fmap_return)
+
+lemma coerce_bind [coerce_simp]:
+  fixes m :: "'a\<cdot>'m::monad" and k :: "'a \<rightarrow> 'b\<cdot>'m"
+  shows "COERCE('b\<cdot>'m,'c\<cdot>'m)\<cdot>(m \<guillemotright>= k) = m \<guillemotright>= (\<Lambda> x. COERCE('b\<cdot>'m,'c\<cdot>'m)\<cdot>(k\<cdot>x))"
+by (simp add: coerce_functor fmap_bind)
+
+lemma bind_coerce [coerce_simp]:
+  fixes m :: "'a\<cdot>'m::monad" and k :: "'b \<rightarrow> 'c\<cdot>'m"
+  shows "COERCE('a\<cdot>'m,'b\<cdot>'m)\<cdot>m \<guillemotright>= k = m \<guillemotright>= (\<Lambda> x. k\<cdot>(COERCE('a,'b)\<cdot>x))"
+by (simp add: coerce_functor bind_fmap)
+
+end
