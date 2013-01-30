6d8d34b46b100b3f652b78a18b3879b7ccd017bf
Christian Sternagel <unknown>
2013-01-30 22:27:04 +0900

    tuned proofs

--- thys/Well_Quasi_Orders/Kruskal_Auxiliaries.thy
+++ thys/Well_Quasi_Orders/Kruskal_Auxiliaries.thy
@@ -11,76 +11,59 @@ imports
   Almost_Full_Relations
 begin
 
-lemma no_bad_of_special_shape_imp_good':
-  assumes "\<not> (\<exists>R f::nat seq. (\<forall>i. R i \<in> set (B (f i)) \<and> f 0 \<le> f i) \<and> bad P R)"
-    and refl: "reflp_on P {x. \<exists>i. x \<in> set (B i)}"
-    and elts: "\<forall>i. f i \<in> {x. \<exists>i. x \<in> set (B i)}"
-  shows "good P f"
-proof (rule ccontr)
-  let ?B = "\<lambda>i. set (B i)"
-  assume "bad P f"
-  from elts have "\<forall>i. \<exists>j. f i \<in> ?B j" by auto
-  from choice [OF this] obtain g where B: "\<forall>i. f i \<in> ?B (g i)" by auto
-  have "\<forall>i. \<exists>j>i. g 0 \<le> g j"
+lemma bad_of_special_shape':
+  assumes refl: "reflp_on P {x. \<exists>i. x \<in> set (g i)}"
+    (is "reflp_on P {x. \<exists>i. x \<in> ?G i}")
+    and "\<forall>i. f i \<in> {x. \<exists>i. x \<in> set (g i)}"
+    and "bad P f"
+  shows "\<exists>R (f::nat \<Rightarrow> nat). (\<forall>i. R i \<in> set (g (f i)) \<and> f 0 \<le> f i) \<and> bad P R"
+proof -
+  from assms have "\<forall>i. \<exists>j. f i \<in> ?G j" by auto
+  from choice [OF this] obtain \<phi> where G: "\<And>i. f i \<in> ?G (\<phi> i)" by auto
+  have "\<forall>i. \<exists>j>i. \<phi> 0 \<le> \<phi> j"
   proof (rule ccontr)
-    assume "\<not> (\<forall>i. \<exists>j>i. g 0 \<le> g j)"
-    then obtain i where *: "\<forall>j>i. g j < g 0" by force
-    let ?I = "{j. j > i}"
-    have "g ` ?I \<subseteq> {..<g 0}"
-      using * unfolding image_subset_iff by (metis lessThan_iff mem_Collect_eq)
-    moreover have "finite {..<g 0}" by auto
-    ultimately have 1: "finite (g ` ?I)" using finite_subset by blast
-    have 2: "infinite ?I" by (rule infinite_wo_prefix)
-    from pigeonhole_infinite [OF 2 1]
-      obtain k where "k > i" and 3: "infinite {j. j > i \<and> g j = g k}" by auto
-    from this [unfolded infinite_nat_iff_unbounded]
-      have "\<forall>m. \<exists>n>m. n > i \<and> g n = g k" by auto
-    from choice [OF this] obtain h where
-      **: "\<forall>m. h m > m \<and> h m > i \<and> g (h m) = g k" by auto
-    let ?g = "g \<circ> h"
-    let ?h = "\<lambda>i. (h ^^ Suc i) 0"
-    from B have "\<forall>i. f (?h i) \<in> ?B ((g \<circ> ?h) i)" by auto
-    with ** have "\<forall>i. f (?h i) \<in> ?B (g k)" by simp
-    with pigeonhole_infinite_rel [of "UNIV::nat set" "?B (g k)" "\<lambda>a b. f (?h a) = b"]
-      obtain x where "x \<in> ?B (g k)" and "infinite {a. f (?h a) = x}" by auto
-    hence all: "\<forall>m. \<exists>n>m. f (?h n) = x" unfolding infinite_nat_iff_unbounded by auto
-    from all obtain u where u: "f (?h u) = x" by auto
-    from all obtain v where "v > u" and v: "f (?h v) = x" by auto
+    assume "\<not> ?thesis"
+    then obtain i where "\<forall>j>i. \<not> (\<phi> 0 \<le> \<phi> j)" by auto
+    then have "\<phi> ` {j. j > i} \<subseteq> {..< \<phi> 0}" (is "\<phi> ` ?I \<subseteq> _") by auto
+    then have "finite (\<phi> ` ?I)" by (blast intro: finite_subset)
+    moreover have "infinite ?I" by (rule infinite_wo_prefix)
+    ultimately obtain k
+      where "k > i" and "infinite {j. j > i \<and> \<phi> j = \<phi> k}"
+      using pigeonhole_infinite [of ?I \<phi>] by auto
+    then have "\<forall>m. \<exists>n>m. n > i \<and> \<phi> n = \<phi> k"
+      by (auto simp: infinite_nat_iff_unbounded)
+    from choice [OF this] obtain \<psi>
+      where *: "\<forall>m. \<psi> m > m \<and> \<psi> m > i \<and> \<phi> (\<psi> m) = \<phi> k" by auto
+    let ?\<phi> = "\<phi> \<circ> \<psi>"
+    let ?\<psi> = "\<lambda>i. (\<psi> ^^ Suc i) 0"
+    from G have "\<forall>i. f (?\<psi> i) \<in> ?G ((\<phi> \<circ> ?\<psi>) i)" by auto
+    with * have "\<forall>i. f (?\<psi> i) \<in> ?G (\<phi> k)" by simp
+    with pigeonhole_infinite_rel [of "UNIV::nat set" "?G (\<phi> k)" "\<lambda>a b. f (?\<psi> a) = b"]
+      obtain x where "x \<in> ?G (\<phi> k)" and "infinite {a. f (?\<psi> a) = x}" by auto
+    then have "\<forall>m. \<exists>n>m. f (?\<psi> n) = x" by (auto simp: infinite_nat_iff_unbounded)
+    then obtain u v where u: "f (?\<psi> u) = x"
+      and "v > u" and v: "f (?\<psi> v) = x" by blast
 
-    from ** have "\<forall>i\<ge>0. h i > i" by auto
-    from funpow_mono [OF this] have ***: "\<And>i j. i < j \<Longrightarrow> ?h i < ?h j" by best
-    from this [OF `v > u`] have "?h u < ?h v" .
-    moreover have "P (f (?h u)) (f (?h v))"
-    proof -
-      from refl and `x \<in> ?B (g k)` have "P x x" by (auto simp: reflp_on_def)
-      thus ?thesis unfolding u v .
-    qed
+    from * have "\<forall>i\<ge>0. \<psi> i > i" by auto
+    from funpow_mono [OF this] and `v > u`
+      have "?\<psi> u < ?\<psi> v" by best
+    moreover have "P (f (?\<psi> u)) (f (?\<psi> v))"
+      using `x \<in> ?G (\<phi> k)` and refl
+      unfolding u v by (auto simp: reflp_on_def)
     ultimately show False using `bad P f` by (auto simp: good_def)
   qed
-  from choice [OF this] obtain h
-    where "\<forall>i. (h i) > i" and *: "\<And>i. g (h i) \<ge> g 0" by blast
-  hence "\<forall>i\<ge>0. (h i) > i" by auto
-  from funpow_mono [OF this] have **: "\<And>i j. i < j \<Longrightarrow> (h ^^ i) 0 < (h ^^ j) 0" by auto
-  let ?i = "\<lambda>i. (h ^^ i) 0"
-  let ?f = "\<lambda>i. g (?i i)"
-  let ?R = "\<lambda>i. f (?i i)"
-  have "\<forall>i. ?R i \<in> ?B (?f i)" using B by auto
-  moreover have "\<forall>i. ?f i \<ge> ?f 0"
-  proof
-    fix i show "?f i \<ge> ?f 0" using * by (induct i) auto
-  qed
+  from choice [OF this] obtain \<psi>
+    where \<psi>: "\<forall>i\<ge>0. (\<psi> i) > i" and *: "\<And>i. \<phi> (\<psi> i) \<ge> \<phi> 0" by blast
+  let ?\<psi> = "\<lambda>i. (\<psi> ^^ i) 0"
+  let ?\<phi> = "\<lambda>i. \<phi> (?\<psi> i)"
+  from funpow_mono [OF \<psi>]
+    have **: "\<And>i j. i < j \<Longrightarrow> ?\<psi> i < ?\<psi> j" by auto
+  let ?R = "\<lambda>i. f (?\<psi> i)"
+  have "\<forall>i. ?R i \<in> ?G (?\<phi> i)" using G by auto
+  moreover have "\<forall>i. ?\<phi> i \<ge> ?\<phi> 0" by (rule, induct_tac i) (auto simp: *)
   moreover have "bad P ?R"
-  proof
-    assume "good P ?R"
-    then obtain i j where "i < j" and "P (?R i) (?R j)" by (auto simp: good_def)
-    hence "P (f (?i i)) (f (?i j))" by simp
-    moreover from ** [OF `i < j`] have "?i i < ?i j" .
-    ultimately show False using `bad P f` by (auto simp: good_def)
-  qed
-  ultimately have "(\<forall>i. ?R i \<in> ?B (?f i) \<and> ?f i \<ge> ?f 0) \<and> bad P ?R" by auto
-  hence "\<exists>f. (\<forall>i. ?R i \<in> ?B (f i) \<and> f i \<ge> f 0) \<and> bad P ?R" by auto
-  hence "\<exists>R f. (\<forall>i. R i \<in> ?B (f i) \<and> f i \<ge> f 0) \<and> bad P R" by metis
-  with assms(1) show False by blast
+    using ** and `bad P f` by (auto simp: good_def)
+  ultimately show ?thesis by (blast intro: exI [of _ ?\<phi>] exI[of _ ?R])
 qed
 
 end
