e1b4aad770ca48bf9e8971a1c5ed0c25324767e7
kleing <unknown>
2013-06-29 13:17:38 +1000

    slightly more robust parsing of build output

--- admin/report.pl
+++ admin/report.pl
@@ -5,18 +5,28 @@
 # Parses log file and generates report of entry status.
 # Prints out diff to last report.
 #
+# ASSUMPTION: names in ROOTS = all session names
+#
+# Alternative?
+# isabelle build -d ../thys -g AFP -c -n -v | grep ^Session | cut -c 9- | grep "(AFP)" | rev | cut -c 7- | rev
 
-if ($#ARGV!=1) {
-  print "usage: report.pl <logfile> <report-file>\n";
+
+if ($#ARGV!=2) {
+  print "usage: $0 <rootsfile> <logfile> <report-file>\n";
   exit 1;
 }
 
-$logfile=$ARGV[0];
-$report=$ARGV[1];
+$roots=$ARGV[0];
+$logfile=$ARGV[1];
+$report=$ARGV[2];
 
 $FAIL="FAIL";
-$SKIPPED="skipped";
 $OK="ok";
+
+#read ROOTS file
+open (IN,$roots) || die "could not open [$roots]";
+@sessions = <IN>;
+close IN;
 
 # read log file
 open (IN,$logfile) || die "could not open [$logfile]";
@@ -24,21 +34,14 @@ open (IN,$logfile) || die "could not open [$logfile]";
 close IN;
 
 # parse log file
-for $i ( 0 .. $#lines) {
+for $i (0 .. $#lines) {
   $_ = $lines[$i];
-  if ( /^Running ([^ ]+) \.\.\./ ) {
-    $tests{$1} = 1;
-  }
-# FIXME
-# no skipping in new build; check session list from afp_build? or drop?
-  if ( /The following tests were skipped/ ) {
-    foreach $f (split (/[, \n]/,$lines[$i+1])) {
-      $skipped{$f} = 1;
-    }
-  }
+  if ( /^Finished ([^ ]+)/ ) {
+    $finished{$1} = 1;
+  }  
   if ( /Unfinished session\(s\)/ ) {
     foreach $f (split (/[, \n]/,$lines[$i])) {
-      $fail{$f} = 1;
+      $unfinished{$f} = 1;
     }
   }
 }
@@ -48,9 +51,8 @@ open (IN,$report);
 while (<IN>) {
   chop;
   ($name, $f) = split /[:]/;
-  $old_tests{$name} = 1;
-  $old_fail{$name} = ($f =~ /$FAIL/);
-  $old_skipped{$name} = ($f =~ /$SKIPPED/);
+  $old_sessions{$name} = 1;
+  $old_ok{$name} = ($f =~ /$OK/);
 }
 close IN;
 
@@ -59,30 +61,31 @@ rename ($report, $report.".old");
 
 # write new report
 open (OUT,">$report") || die "could not open [$report] for writing.";
-foreach $t (keys %tests) {
-  $status = $fail{$t} ? $FAIL : ($skipped{$t} ? $SKIPPED : $OK);
-  print OUT "$t: $status\n";
+foreach $s (@sessions) {
+  chomp($s);
+  $status = $finished{$s} ? $OK : $FAIL;
+  print OUT "$s: $status\n";
 }
 close OUT;
 
 # output diff
-foreach $t (keys %old_tests) {
-  $old_status = $old_fail{$t} ? $FAIL : ($old_skipped{$t} ? $SKIPPED : $OK);
-  $new_status = $fail{$t} ? $FAIL : ($skipped{$t} ? $SKIPPED : $OK);
-  if (!$tests{$t}) {
+foreach $t (keys %old_sessions) {
+  $old_status = $old_ok{$t} ? $OK : $FAIL;
+  $new_status = $finished{$t} ? $OK : $FAIL;
+  if (!$t ~~ @sessions) {
     print "[$t] was removed. Last status was $old_status.\n";
   }
-  elsif ($old_fail{$t} != $fail{$t}) {
+  elsif ($old_ok{$t} != $finished{$t}) {
     print "[$t] changed from $old_status to $new_status.\n";
   }
-  elsif ($fail{$t}) {
+  elsif (!$finished{$t}) {
     print "[$t] is still on $new_status.\n";
   }
 }
 
-foreach $t (keys %tests) {
-  if (!$old_tests{$t}) {
-    $new_status = $fail{$t} ? $FAIL : ($skipped{$t} ? $SKIPPED : $OK);
+foreach $t (@sessions) {
+  if (!$old_sessions{$t}) {
+    $new_status = $finished{$t} ? $OK : $FAIL;
     print "[$t] is new. Status is $new_status.\n";
   }
 }
