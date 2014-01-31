4cb9664eef0bc27f5f1a6225d0d4f22f57a617a3
Andreas Lochbihler <unknown>
2014-01-31 14:51:44 +0100

    replace [code, code del] with [[code drop:]]

--- thys/Coinductive/Lazy_LList.thy
+++ thys/Coinductive/Lazy_LList.thy
@@ -52,8 +52,7 @@ lemma lnull_lazy [code]: "lnull = Option.is_none \<circ> force"
 unfolding lnull_def
 by (rule ext) (simp add: Option.is_none_def split: llist.split)
 
-lemma [code, code del]:
-  "equal_class.equal = (equal_class.equal :: 'a :: equal llist \<Rightarrow> _)" ..
+declare [[code drop: "equal_class.equal :: 'a :: equal llist \<Rightarrow> _"]]
 
 lemma equal_llist_Lazy_llist [code]:
   "equal_class.equal (Lazy_llist xs) (Lazy_llist ys) \<longleftrightarrow>
@@ -63,7 +62,7 @@ lemma equal_llist_Lazy_llist [code]:
         | Some (y, ys') \<Rightarrow> if x = y then equal_class.equal xs' ys' else False))"
 by(auto simp add: equal_llist_def)
 
-lemma [code, code del]: "corec_llist = corec_llist" ..
+declare [[code drop: corec_llist]]
 
 lemma corec_llist_Lazy_llist [code]:
   "corec_llist IS_LNIL LHD endORmore LTL_end LTL_more b =
@@ -73,59 +72,59 @@ lemma corec_llist_Lazy_llist [code]:
        else corec_llist IS_LNIL LHD endORmore LTL_end LTL_more (LTL_more b)))"
 by(subst corec_llist_code) simp
 
-lemma [code, code del]: "unfold_llist = unfold_llist" ..
+declare [[code drop: unfold_llist]]
 
 lemma unfold_llist_Lazy_llist [code]:
   "unfold_llist IS_LNIL LHD LTL b =
   Lazy_llist (\<lambda>_. if IS_LNIL b then None else Some (LHD b, unfold_llist IS_LNIL LHD LTL (LTL b)))"
 by(subst unfold_llist_code) simp
 
-lemma [code, code del]: "case_llist = case_llist" ..
+declare [[code drop: case_llist]]
 
 lemma case_llist_Lazy_llist [code]:
   "case_llist n c (Lazy_llist xs) = (case xs () of None \<Rightarrow> n | Some (x, ys) \<Rightarrow> c x ys)"
 by simp
 
-lemma [code, code del]: "lappend = lappend" ..
+declare [[code drop: lappend]]
 
 lemma lappend_Lazy_llist [code]:
   "lappend (Lazy_llist xs) ys = 
   Lazy_llist (\<lambda>_. case xs () of None \<Rightarrow> force ys | Some (x, xs') \<Rightarrow> Some (x, lappend xs' ys))"
 by(auto split: llist.splits)
 
-lemma [code, code del]: "lmap = lmap" ..
+declare [[code drop: lmap]]
 
 lemma lmap_Lazy_llist [code]:
   "lmap f (Lazy_llist xs) = Lazy_llist (\<lambda>_. Option.map (map_pair f (lmap f)) (xs ()))"
 by simp
 
-lemma [code, code del]: "lfinite = lfinite" ..
+declare [[code drop: lfinite]]
 
 lemma lfinite_Lazy_llist [code]:
   "lfinite (Lazy_llist xs) = (case xs () of None \<Rightarrow> True | Some (x, ys) \<Rightarrow> lfinite ys)"
 by simp
 
-lemma [code, code del]: "list_of_aux = list_of_aux" ..
+declare [[code drop: list_of_aux]]
 
 lemma list_of_aux_Lazy_llist [code]:
   "list_of_aux xs (Lazy_llist ys) = 
   (case ys () of None \<Rightarrow> rev xs | Some (y, ys) \<Rightarrow> list_of_aux (y # xs) ys)"
 by(simp add: list_of_aux_code)
 
-lemma [code, code del]: "gen_llength = gen_llength" ..
+declare [[code drop: gen_llength]]
 
 lemma gen_llength_Lazy_llist [code]:
   "gen_llength n (Lazy_llist xs) = (case xs () of None \<Rightarrow> enat n | Some (_, ys) \<Rightarrow> gen_llength (n + 1) ys)"
 by(simp add: gen_llength_code)
 
-lemma [code, code del]: "ltake = ltake" ..
+declare [[code drop: ltake]]
 
 lemma ltake_Lazy_llist [code]:
   "ltake n (Lazy_llist xs) =
   Lazy_llist (\<lambda>_. if n = 0 then None else case xs () of None \<Rightarrow> None | Some (x, ys) \<Rightarrow> Some (x, ltake (n - 1) ys))"
 by(cases n rule: enat_coexhaust) auto
 
-lemma [code, code del]: "ldropn = ldropn" ..
+declare [[code drop: ldropn]]
 
 lemma ldropn_Lazy_llist [code]:
   "ldropn n (Lazy_llist xs) = 
@@ -133,42 +132,42 @@ lemma ldropn_Lazy_llist [code]:
                    case xs () of None \<Rightarrow> None | Some (x, ys) \<Rightarrow> force (ldropn (n - 1) ys))"
 by(cases n)(auto simp add: eSuc_enat[symmetric] split: llist.split)
 
-lemma [code, code del]: "ltakeWhile = ltakeWhile" ..
+declare [[code drop: ltakeWhile]]
 
 lemma ltakeWhile_Lazy_llist [code]:
   "ltakeWhile P (Lazy_llist xs) = 
   Lazy_llist (\<lambda>_. case xs () of None \<Rightarrow> None | Some (x, ys) \<Rightarrow> if P x then Some (x, ltakeWhile P ys) else None)"
 by auto
 
-lemma [code, code del]: "ldropWhile = ldropWhile" ..
+declare [[code drop: ldropWhile]]
 
 lemma ldropWhile_Lazy_llist [code]:
   "ldropWhile P (Lazy_llist xs) = 
    Lazy_llist (\<lambda>_. case xs () of None \<Rightarrow> None | Some (x, ys) \<Rightarrow> if P x then force (ldropWhile P ys) else Some (x, ys))"
 by(auto split: llist.split)
 
-lemma [code, code del]: "lzip = lzip" ..
+declare [[code drop: lzip]]
 
 lemma lzip_Lazy_llist [code]:
   "lzip (Lazy_llist xs) (Lazy_llist ys) =
   Lazy_llist (\<lambda>_. Option.bind (xs ()) (\<lambda>(x, xs'). Option.map (\<lambda>(y, ys'). ((x, y), lzip xs' ys')) (ys ())))"
 by auto
 
-lemma [code, code del]: "gen_lset = gen_lset" ..
+declare [[code drop: gen_lset]]
 
 lemma lset_Lazy_llist [code]:
   "gen_lset A (Lazy_llist xs) =
   (case xs () of None \<Rightarrow> A | Some (y, ys) \<Rightarrow> gen_lset (insert y A) ys)"
 by(auto simp add: gen_lset_code)
 
-lemma [code, code del]: "lmember = lmember" ..
+declare [[code drop: lmember]]
 
 lemma lmember_Lazy_llist [code]: 
   "lmember x (Lazy_llist xs) =
   (case xs () of None \<Rightarrow> False | Some (y, ys) \<Rightarrow> x = y \<or> lmember x ys)"
 by(simp add: lmember_def)
 
-lemma [code, code del]: "llist_all2 = llist_all2" ..
+declare [[code drop: llist_all2]]
 
 lemma llist_all2_Lazy_llist [code]:
   "llist_all2 P (Lazy_llist xs) (Lazy_llist ys) =
@@ -177,19 +176,19 @@ lemma llist_all2_Lazy_llist [code]:
                            | Some (y, ys') \<Rightarrow> P x y \<and> llist_all2 P xs' ys')"
 by auto
 
-lemma [code, code del]: "lhd = lhd" ..
+declare [[code drop: lhd]]
 
 lemma lhd_Lazy_llist [code]:
   "lhd (Lazy_llist xs) = (case xs () of None \<Rightarrow> undefined | Some (x, xs') \<Rightarrow> x)"
 by(simp add: lhd_def)
 
-lemma [code, code del]: "ltl = ltl" ..
+declare [[code drop: ltl]]
 
 lemma ltl_Lazy_llist [code]:
   "ltl (Lazy_llist xs) = Lazy_llist (\<lambda>_. case xs () of None \<Rightarrow> None | Some (x, ys) \<Rightarrow> force ys)"
 by(auto split: llist.split)
 
-lemma [code, code del]: "llast = llast" ..
+declare [[code drop: llast]]
 
 lemma llast_Lazy_llist [code]:
   "llast (Lazy_llist xs) =
@@ -199,14 +198,14 @@ lemma llast_Lazy_llist [code]:
     case force xs' of None \<Rightarrow> x | Some (x', xs'') \<Rightarrow> llast (LCons x' xs''))"
 by(auto simp add: llast_def zero_enat_def eSuc_def split: enat.split llist.splits)
 
-lemma [code, code del]: "ldistinct = ldistinct" ..
+declare [[code drop: ldistinct]]
 
 lemma ldistinct_Lazy_llist [code]:
   "ldistinct (Lazy_llist xs) =
   (case xs () of None \<Rightarrow> True | Some (x, ys) \<Rightarrow> x \<notin> lset ys \<and> ldistinct ys)"
 by(auto)
 
-lemma [code, code del]: "lprefix = lprefix" ..
+declare [[code drop: lprefix]]
 
 lemma lprefix_Lazy_llist [code]:
   "lprefix (Lazy_llist xs) (Lazy_llist ys) =
@@ -216,7 +215,7 @@ lemma lprefix_Lazy_llist [code]:
     case ys () of None \<Rightarrow> False | Some (y, ys') \<Rightarrow> x = y \<and> lprefix xs' ys')"
 by auto
 
-lemma [code, code del]: "lstrict_prefix = lstrict_prefix" ..
+declare [[code drop: lstrict_prefix]]
 
 lemma lstrict_prefix_Lazy_llist [code]:
   "lstrict_prefix (Lazy_llist xs) (Lazy_llist ys) \<longleftrightarrow>
@@ -226,7 +225,7 @@ lemma lstrict_prefix_Lazy_llist [code]:
     case xs () of None \<Rightarrow> True | Some (x, xs') \<Rightarrow> x = y \<and> lstrict_prefix xs' ys')"
 by auto
 
-lemma [code, code del]: "llcp = llcp" ..
+declare [[code drop: llcp]]
 
 lemma llcp_Lazy_llist [code]:
   "llcp (Lazy_llist xs) (Lazy_llist ys) =
@@ -235,7 +234,7 @@ lemma llcp_Lazy_llist [code]:
                      | Some (y, ys') \<Rightarrow> if x = y then eSuc (llcp xs' ys') else 0)"
 by auto
 
-lemma [code, code del]: "llexord = llexord" ..
+declare [[code drop: llexord]]
 
 lemma llexord_Lazy_llist [code]:
   "llexord r (Lazy_llist xs) (Lazy_llist ys) \<longleftrightarrow>
@@ -245,7 +244,7 @@ lemma llexord_Lazy_llist [code]:
     case ys () of None \<Rightarrow> False | Some (y, ys') \<Rightarrow> r x y \<or> x = y \<and> llexord r xs' ys')"
 by auto
 
-lemma [code, code del]: "lfilter = lfilter" ..  
+declare [[code drop: lfilter]]
 
 lemma lfilter_Lazy_llist [code]:
   "lfilter P (Lazy_llist xs) =
@@ -253,7 +252,7 @@ lemma lfilter_Lazy_llist [code]:
                   | Some (x, ys) \<Rightarrow> if P x then Some (x, lfilter P ys) else force (lfilter P ys))"
 by(auto split: llist.split)
 
-lemma [code, code del]: "lconcat = lconcat" ..  
+declare [[code drop: lconcat]]
 
 lemma lconcat_Lazy_llist [code]:
   "lconcat (Lazy_llist xss) =
