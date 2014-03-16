97263ee4ebc5ac4f439f99c04f5fccdf5c1f01d7
haftmann <unknown>
2014-03-16 14:19:36 +0100

    repaired slip from 2dfe26665e91

--- thys/MonoBoolTranAlgebra/Statements.thy
+++ thys/MonoBoolTranAlgebra/Statements.thy
@@ -1,6 +1,6 @@
 header {* Program statements, Hoare and refinement rules *}
 
-theory  Statements
+theory Statements
 imports Assertion_Algebra
 begin
 
@@ -426,6 +426,20 @@ lemma while_pres_disj: "(x::'a::mbt_algebra) \<in> disjunctive \<Longrightarrow>
 lemma while_pres_conj: "(x::'a::mbt_algebra_fusion) \<in> conjunctive \<Longrightarrow> (While p do x) \<in> conjunctive"
   apply(unfold while_def)
   by (simp add: comp_pres_conj omega_pres_conj)
+
+no_notation
+  bot ("\<bottom>") and
+  top ("\<top>") and
+  inf (infixl "\<sqinter>" 70) and
+  sup (infixl "\<squnion>" 65) and
+  Inf ("\<Sqinter>_" [900] 900) and
+  Sup ("\<Squnion>_" [900] 900)
+
+no_syntax (xsymbols)
+  "_INF1"     :: "pttrns \<Rightarrow> 'b \<Rightarrow> 'b"           ("(3\<Sqinter>_./ _)" [0, 10] 10)
+  "_INF"      :: "pttrn \<Rightarrow> 'a set \<Rightarrow> 'b \<Rightarrow> 'b"  ("(3\<Sqinter>_\<in>_./ _)" [0, 0, 10] 10)
+  "_SUP1"     :: "pttrns \<Rightarrow> 'b \<Rightarrow> 'b"           ("(3\<Squnion>_./ _)" [0, 10] 10)
+  "_SUP"      :: "pttrn \<Rightarrow> 'a set \<Rightarrow> 'b \<Rightarrow> 'b"  ("(3\<Squnion>_\<in>_./ _)" [0, 0, 10] 10)
 
 end
 
