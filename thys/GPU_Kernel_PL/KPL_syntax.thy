d6e483124c4302a5c04d9c9e23788637923fbf24
kleing <unknown>
2014-04-06 08:57:39 +1000

    GPU kernel PL entry by John Wickerson

---
+++ thys/GPU_Kernel_PL/KPL_syntax.thy
@@ -0,0 +1,79 @@
+header {* Syntax of KPL *}
+
+theory KPL_syntax imports 
+  Misc
+begin
+
+text{* Locations of local variables *}
+typedecl V 
+
+text {* C strings *}
+typedecl name
+
+text {* Procedure names *}
+typedecl proc_name
+
+text {* Local-id, group-id *}
+type_synonym lid = nat
+type_synonym gid = nat
+
+text {* Fully-qualified thread-id *}
+type_synonym tid = "gid \<times> lid"
+
+text {* Let @{term "(G,T)"} range over threadsets *}
+type_synonym threadset = "gid set \<times> (gid \<rightharpoonup> lid set)"
+
+text {* Returns the set of tids in a threadset *}
+fun tids :: "threadset \<Rightarrow> tid set"
+where
+  "tids (G,T) = {(i,j) | i j. i \<in> G \<and> j \<in> the (T i)}"
+
+type_synonym word = nat  (* should really be machine words *)
+
+datatype loc =
+  Name name
+| Var V
+
+text {* Local expressions *}
+datatype local_expr =
+  Loc loc
+| Gid
+| Lid
+| eTrue
+| eConj local_expr local_expr  (infixl "\<and>*" 50)
+| eNot local_expr              ("\<not>*")
+
+text {* Basic statements *}
+datatype basic_stmt =
+  Assign loc local_expr
+| Read loc local_expr
+| Write local_expr local_expr
+
+text {* Statements *}
+datatype stmt =
+  Basic basic_stmt
+| Seq stmt stmt (infixl ";;" 50)
+| Local name stmt
+| If local_expr stmt stmt
+| While local_expr stmt
+| WhileDyn local_expr stmt
+| Call proc_name local_expr
+| Barrier
+| Break
+| Continue
+| Return
+
+text {* Procedures comprise a procedure name, parameter name, and a body statement *}
+record proc = 
+  proc_name :: proc_name
+  param :: name
+  body :: stmt
+
+text {* Kernels *}
+record kernel = 
+  groups :: nat
+  threads :: nat
+  procs :: "proc list"
+  main :: stmt
+
+end
---
+++ thys/GPU_Kernel_PL/KPL_wellformedness.thy
@@ -0,0 +1,89 @@
+header {* Well-formedness of KPL kernels *}
+
+theory KPL_wellformedness imports 
+  KPL_syntax
+begin
+
+text {* 
+  Well-formed local expressions. @{term "wf_local_expr ns e"} 
+  means that 
+  \begin{itemize} 
+  \item @{term e} does not mention any internal locations, and
+  \item any name mentioned by @{term e} is in the set @{term ns}.
+  \end{itemize}
+*}
+fun wf_local_expr :: "name set \<Rightarrow> local_expr \<Rightarrow> bool"
+where
+  "wf_local_expr ns (Loc (Var j)) = False"
+| "wf_local_expr ns (Loc (Name n)) = (n \<in> ns)"
+| "wf_local_expr ns (e1 \<and>* e2) =
+  (wf_local_expr ns e1 \<and> wf_local_expr ns e2)"
+| "wf_local_expr ns (\<not>* e) = wf_local_expr ns e"
+| "wf_local_expr ns _ = True"
+
+text {* 
+  Well-formed basic statements. @{term "wf_basic_stmt ns b"} 
+  means that 
+  \begin{itemize}
+  \item @{term b} does not mention any internal locations, and
+  \item any name mentioned by @{term b} is in the set @{term ns}.
+  \end{itemize}
+*}
+fun wf_basic_stmt :: "name set \<Rightarrow> basic_stmt \<Rightarrow> bool"
+where
+  "wf_basic_stmt ns (Assign x e) = wf_local_expr ns e"
+| "wf_basic_stmt ns (Read x e) = wf_local_expr ns e"
+| "wf_basic_stmt ns (Write e1 e2) = 
+  (wf_local_expr ns e1 \<and> wf_local_expr ns e2)"
+
+text {* 
+  Well-formed statements. @{term "wf_stmt ns F S"} means:
+  \begin{itemize}
+  \item @{term S} only calls procedures whose name is in @{term F},
+  \item @{term S} does not contain @{term WhileDyn}, 
+  \item @{term S} does not mention internal variables, 
+  \item @{term S} only mentions names in @{term ns}, and
+  \item @{term S} does not declare the same name twice, e.g. @{term "Local x (Local x foo)"}.
+  \end{itemize}
+*}
+fun wf_stmt :: "name set \<Rightarrow> proc_name set \<Rightarrow> stmt \<Rightarrow> bool"
+where
+  "wf_stmt ns F (Basic b) = wf_basic_stmt ns b"
+| "wf_stmt ns F (S1 ;; S2) = (wf_stmt ns F S1 \<and> wf_stmt ns F S2)"
+| "wf_stmt ns F (Local n S) = (n \<notin> ns \<and> wf_stmt ({n} \<union> ns) F S)"
+| "wf_stmt ns F (If e S1 S2) = 
+  (wf_local_expr ns e \<and> wf_stmt ns F S1 \<and> wf_stmt ns F S2)"
+| "wf_stmt ns F (While e S) = 
+  (wf_local_expr ns e \<and> wf_stmt ns F S)"
+| "wf_stmt ns F (WhileDyn _ _) = False"
+| "wf_stmt ns F (Call f e) = (f \<in> F \<and> wf_local_expr ns e)"
+| "wf_stmt _ _ _ = True"
+
+text {* @{term "no_return S"} holds if @{term S} does not contain a @{term Return} statement *}
+fun no_return :: "stmt \<Rightarrow> bool"
+where
+  "no_return (S1 ;; S2) = (no_return S1 \<and> no_return S2)"
+| "no_return (Local n S) = no_return S"
+| "no_return (If e S1 S2) = (no_return S1 \<and> no_return S2)"
+| "no_return (While e S) = (no_return S)"
+| "no_return Return = False"
+| "no_return _ = True"
+
+text {* Well-formed kernel *}
+definition wf_kernel :: "kernel \<Rightarrow> bool"
+where
+  "wf_kernel P \<equiv>
+  let F = set (map proc_name (procs P)) in
+ 
+  (* The main statement must not refer to *any*
+     variable, except those it locally defines. *)
+  wf_stmt {} F (main P)
+
+  (* The main statement contains no return statement. *)
+\<and> no_return (main P)
+ 
+  (* A procedure body may refer only to its argument. *)
+\<and> list_all (\<lambda>f. wf_stmt {param f} F (body f)) (procs P)"
+
+
+end
