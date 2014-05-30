147613e08eef92cfec652e9f5a02f266547f66c2
paulson <lp15@cam.ac.uk>
2014-05-30 15:33:22 +0100

    new examples

--- thys/Sturm_Sequences/Examples/Sturm_Ex.thy
+++ thys/Sturm_Sequences/Examples/Sturm_Ex.thy
@@ -31,9 +31,7 @@ lemma "card {x::real. x^3 + x = 2*x^2 \<and> x^3 - 6*x^2 + 11*x = 6} = 1"
 by sturm
 
 
-
 schematic_lemma "card {x::real. x^3 + x = 2*x^2 \<or> x^3 - 6*x^2 + 11*x = 6} = ?n" by sturm
-
 
 lemma
   "card {x::real. -0.010831 < x \<and> x < 0.010831 \<and> 
@@ -43,5 +41,17 @@ lemma
 lemma "\<forall>x::real. x*x \<noteq> 0 \<or> x*x - 1 \<noteq> 2*x" by sturm
 
 lemma "(x::real)*x+1 \<noteq> 0 \<and> (x^2+1)*(x^2+2) \<noteq> 0" by sturm
+
+text{*3 examples related to continued fraction approximants to exp: LCP*}
+lemma fixes x::real
+  shows "-7.29347719 \<le> x \<Longrightarrow> 0 < x^5 + 30*x^4 + 420*x^3 + 3360*x^2 + 15120*x + 30240"
+by sturm
+
+lemma fixes x::real
+  shows "0 < x^6 + 42*x^5 + 840*x^4 + 10080*x^3 + 75600*x\<^sup>2 + 332640*x + 665280"
+by sturm
+
+schematic_lemma "card {x::real. x^7 + 56*x^6 + 1512*x^5 + 25200*x^4 + 277200*x^3 + 1995840*x^2 + 8648640*x = -17297280} = ?n" 
+by sturm
 
 end
