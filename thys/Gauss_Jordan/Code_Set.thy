618f3cff0c19a2168994c4f5e53b8017758dab31
paulson <lp15@cam.ac.uk>
2014-09-03 15:25:16 +0100

    new entry Gauss_Jordan

---
+++ thys/Gauss_Jordan/Code_Set.thy
@@ -0,0 +1,42 @@
+(*  
+    Title:      Code_Set.thy
+    Author:     Jose Divasón <jose.divasonm at unirioja.es>
+    Author:     Jesús Aransay <jesus-maria.aransay at unirioja.es>
+*)
+
+header "Code set"
+
+theory Code_Set
+imports
+  "~~/src/HOL/Library/Cardinality"
+begin
+
+text{*The following setup could help to get code generation for List.coset, 
+  but it neither works correctly it complains that code
+  equations for remove are missed, even when List.coset should be rewritten
+  to an enum:*}
+
+declare minus_coset_filter [code del]
+declare remove_code(2) [code del]
+declare insert_code(2) [code del]
+declare inter_coset_fold [code del]
+declare compl_coset[code del]
+declare Cardinality.card'_code(2)[code del]
+
+code_datatype set
+
+text{*The following code equation could be useful to avoid the problem of
+  code generation for List.coset []:*}
+
+lemma [code]: "List.coset (l::'a::enum list) = set (enum_class.enum) - set l"
+  by (metis Compl_eq_Diff_UNIV coset_def enum_UNIV)
+
+text{*Now the following examples work:*}
+
+value "UNIV::bool set"
+value "List.coset ([]::bool list)"
+value "UNIV::Enum.finite_2 set"
+value "List.coset ([]::Enum.finite_2 list)"
+value "List.coset ([]::Enum.finite_5 list)"
+
+end
---
+++ thys/Gauss_Jordan/Rank.thy
@@ -0,0 +1,46 @@
+(*  
+    Title:      Rank.thy
+    Author:     Jose Divasón <jose.divasonm at unirioja.es>
+    Author:     Jesús Aransay <jesus-maria.aransay at unirioja.es>
+    Maintainer: Jose Divasón <jose.divasonm at unirioja.es>
+*)
+
+header{*Rank of a matrix*}
+
+theory Rank
+imports 
+      "../Rank_Nullity_Theorem/Dim_Formula"
+begin
+
+subsection{*Row rank, column rank and rank*}
+
+text{*Definitions of row rank, column rank and rank*}
+
+definition row_rank :: "'a::{field}^'n^'m=>nat"
+  where "row_rank A = vec.dim (row_space A)"
+
+definition col_rank :: "'a::{field}^'n^'m=>nat"
+  where "col_rank A = vec.dim (col_space A)"
+
+definition rank :: "'a::{field}^'n^'m=>nat"
+  where "rank A = row_rank A"
+
+subsection{*Properties*}
+
+lemma rrk_is_preserved:
+fixes A::"'a::{field}^'cols^'rows::{finite, wellorder}"
+  and P::"'a::{field}^'rows::{finite, wellorder}^'rows::{finite, wellorder}"
+assumes inv_P: "invertible P"
+shows "row_rank A = row_rank (P**A)"
+by (metis row_space_is_preserved row_rank_def inv_P)
+
+lemma crk_is_preserved:
+fixes A::"'a::{field}^'cols::{finite, wellorder}^'rows"
+  and P::"'a::{field}^'rows^'rows"
+assumes inv_P: "invertible P"
+shows "col_rank A = col_rank (P**A)"
+  using rank_nullity_theorem_matrices unfolding ncols_def 
+  by (metis col_rank_def inv_P nat_add_left_cancel null_space_is_preserved) 
+
+end
+
