d36b0f143d35eb74f1aac00de5a2715fc0863c7b
Christian Sternagel <unknown>
2014-09-04 13:48:59 +0200

    removed IsaFoR/CeTA license statement

--- thys/Transitive-Closure/RBT_Map_Set_Extension.thy
+++ thys/Transitive-Closure/RBT_Map_Set_Extension.thy
@@ -5,28 +5,11 @@
     License:     LGPL
 *)
 
-(*
-Copyright 2011 Christian Sternagel, René Thiemann
-
-This file is part of IsaFoR/CeTA.
-
-IsaFoR/CeTA is free software: you can redistribute it and/or modify it under the
-terms of the GNU Lesser General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
-
-IsaFoR/CeTA is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along
-with IsaFoR/CeTA. If not, see <http://www.gnu.org/licenses/>.
-*)
-
 header {* Accessing values via keys *}
 
 theory RBT_Map_Set_Extension
-imports "../Collections/ICF/impl/RBTMapImpl" 
+imports
+  "../Collections/ICF/impl/RBTMapImpl" 
   "../Collections/ICF/impl/RBTSetImpl"
   "../Matrix/Utility"
 begin
