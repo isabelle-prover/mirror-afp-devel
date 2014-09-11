e7d35dafad1ec5afcc29191ce8b08f7034da4902
blanchet <unknown>
2014-09-11 11:53:04 +0200

    ported LightweightJava to new datatypes

--- thys/LightweightJava/Lightweight_Java_Definition.thy
+++ thys/LightweightJava/Lightweight_Java_Definition.thy
@@ -19,26 +19,26 @@ type_synonym "meth" = "string"
 type_synonym "var" = "string"
 type_synonym "dcl" = "string"
 type_synonym "oid" = "nat"
-datatype "fqn" =
+datatype_new "fqn" =
    fqn_def "dcl"
 
-datatype "cl" =
+datatype_new "cl" =
    cl_object
  | cl_fqn "fqn"
 
-datatype "x" =
+datatype_new "x" =
    x_var "var"
  | x_this
 
-datatype "vd" =
+datatype_new "vd" =
    vd_def "cl" "var"
 
 type_synonym "X" = "x list"
-datatype "ctx" =
+datatype_new "ctx" =
    ctx_def
 
 type_synonym "vds" = "vd list"
-datatype "s" =
+datatype_new "s" =
    s_block "s list"
  | s_ass "var" "x"
  | s_read "var" "x" "f"
@@ -47,29 +47,29 @@ datatype "s" =
  | s_new "var" "ctx" "cl"
  | s_call "var" "x" "meth" "X"
 
-datatype "meth_sig" =
+datatype_new "meth_sig" =
    meth_sig_def "cl" "meth" "vds"
 
-datatype "meth_body" =
+datatype_new "meth_body" =
    meth_body_def "s list" "x"
 
-datatype "fd" =
+datatype_new "fd" =
    fd_def "cl" "f"
 
-datatype "meth_def" =
+datatype_new "meth_def" =
    meth_def_def "meth_sig" "meth_body"
 
 type_synonym "fds" = "fd list"
 type_synonym "meth_defs" = "meth_def list"
-datatype "cld" =
+datatype_new "cld" =
    cld_def "dcl" "cl" "fds" "meth_defs"
 
 type_synonym "ctxcld" = "(ctx \<times> cld)"
-datatype "ty" =
+datatype_new "ty" =
    ty_top
  | ty_def "ctx" "dcl"
 
-datatype "v" =
+datatype_new "v" =
    v_null
  | v_oid "oid"
 
@@ -80,7 +80,7 @@ type_synonym "ty_opt" = "ty option"
 type_synonym "tys" = "ty list"
 type_synonym "L" = "x \<rightharpoonup> v"
 type_synonym "H" = "oid \<rightharpoonup> (ty \<times> (f \<rightharpoonup> v))"
-datatype "Exception" =
+datatype_new "Exception" =
    ex_npe
 
 type_synonym "P" = "clds"
@@ -89,18 +89,18 @@ type_synonym "nn" = "nat"
 type_synonym "ctxclds_opt" = "ctxclds option"
 type_synonym "fs_opt" = "fs option"
 type_synonym "meths" = "meth list"
-datatype "ty_opt_bot" =
+datatype_new "ty_opt_bot" =
    ty_opt_bot_opt "ty_opt"
  | ty_opt_bot_bot
 
 type_synonym "meth_def_opt" = "meth_def option"
 type_synonym "ctxmeth_def_opt" = "(ctx \<times> meth_def) option"
-datatype "mty" =
+datatype_new "mty" =
    mty_def "tys" "ty"
 
 type_synonym "\<Gamma>" = "x \<rightharpoonup> ty"
 type_synonym "v_opt" = "v option"
-datatype "config" =
+datatype_new "config" =
    config_normal "P" "L" "H" "s list"
  | config_ex "P" "L" "H" "Exception"
 
