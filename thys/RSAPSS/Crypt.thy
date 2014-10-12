cbfc9a78b84121bf522c06fb7e08971821f42214
haftmann <unknown>
2014-10-12 16:31:45 +0200

    eliminiated clone

--- thys/RSAPSS/Crypt.thy
+++ thys/RSAPSS/Crypt.thy
@@ -7,7 +7,7 @@
 header "Definition of rsacrypt"
 
 theory Crypt
-imports Mod
+imports Mod "~~/src/HOL/Parity"
 begin
 
 text {* 
@@ -15,22 +15,16 @@ text {*
   exponentiation. An proof, that this function calculates RSA is also given
 *}
 
-definition
-  even :: "nat \<Rightarrow> bool" where
-  "even n \<longleftrightarrow> 2 dvd n"
-
 fun rsa_crypt :: "nat \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> nat"
 where
   "rsa_crypt M 0 n = 1"
-| "rsa_crypt M (Suc e) n = (if even (Suc e)
+| "rsa_crypt M (Suc e) n = (if 2 dvd Suc e
        then (rsa_crypt M (Suc e div 2) n) ^ 2 mod n
        else (M * ((rsa_crypt M (Suc e div 2) n) ^ 2 mod n)) mod n)"
 
-lemma div_2_times_2: "(if (even m) then  (m div 2 * 2 = m) else (m div 2 * 2 = m - 1))"
-  by (simp add: even_def dvd_eq_mod_eq_0 mult.commute mult_div_cancel)
-
-theorem cryptcorrect: "n \<noteq> 0 \<Longrightarrow> n \<noteq> 1 \<Longrightarrow> rsa_crypt M e n = M^e mod n"
-  by (induct M e n rule: rsa_crypt.induct) 
-    (auto simp add: power_mult [symmetric] div_2_times_2 remainderexp timesmod1)
+theorem cryptcorrect: "n \<noteq> 0 \<Longrightarrow> n \<noteq> 1 \<Longrightarrow> rsa_crypt M e n = M ^ e mod n"
+  by (induct M e n rule: rsa_crypt.induct)
+    (auto simp add: dvd_eq_mod_eq_0 div_mod_equality' power_mult [symmetric] remainderexp timesmod1)
 
 end
+
