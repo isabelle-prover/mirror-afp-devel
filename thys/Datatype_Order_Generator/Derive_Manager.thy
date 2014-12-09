bfbec4b0ef9ee6657892ae1b70699da562f113f1
Christian Sternagel <unknown>
2014-12-09 10:58:30 +0100

    dropped IsaFoR licensing statement

--- thys/Datatype_Order_Generator/Derive_Manager.thy
+++ thys/Datatype_Order_Generator/Derive_Manager.thy
@@ -1,25 +1,7 @@
-(*  Title:       Deriving class instances for datatypes
-    Author:      René Thiemann       <rene.thiemann@uibk.ac.at>
+(*  Title:       Derive_Manager
+    Author:      René Thiemann <rene.thiemann@uibk.ac.at>
     Maintainer:  René Thiemann
     License:     LGPL
-*)
-
-(*
-Copyright 2013 René Thiemann
-
-This file is part of IsaFoR/CeTA.
-
-IsaFoR/CeTA is free software: you can redistribute it and/or modify it under the
-terms of the GNU Lesser General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
-
-IsaFoR/CeTA is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along
-with IsaFoR/CeTA. If not, see <http://www.gnu.org/licenses/>.
 *)
 
 section {* Derive manager *}
