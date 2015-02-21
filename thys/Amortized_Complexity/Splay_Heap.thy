4e9be765658c4a9a6d171b22a372ab4ee21b4243
nipkow <unknown>
2015-02-21 18:00:24 +0100

    tuned proofs

--- thys/Amortized_Complexity/Splay_Heap.thy
+++ thys/Amortized_Complexity/Splay_Heap.thy
@@ -7,12 +7,6 @@ begin
 text{* Splay heaps were invented by Okasaki~\cite{Okasaki}. *}
 
 
-(* mv *)
-fun (in linorder) bst_eq :: "'a tree \<Rightarrow> bool" where
-"bst_eq Leaf \<longleftrightarrow> True" |
-"bst_eq (Node l a r) \<longleftrightarrow>
- bst_eq l \<and> bst_eq r \<and> (\<forall>x\<in>set_tree l. x \<le> a) \<and> (\<forall>x\<in>set_tree r. a \<le> x)"
-
 subsection{* Definition and Correctness *}
 
 fun partition :: "'a::linorder \<Rightarrow> 'a tree \<Rightarrow> 'a tree * 'a tree" where
@@ -38,9 +32,8 @@ definition insert :: "'a::linorder \<Rightarrow> 'a tree \<Rightarrow> 'a tree" 
 fun del_min :: "'a::linorder tree \<Rightarrow> 'a tree" where
 "del_min Leaf = Leaf" |
 "del_min (Node Leaf _ r) = r" |
-"del_min (Node (Node Leaf _ l) b r) = Node l b r" |
-"del_min (Node (Node ll a lr) b r) = Node (del_min ll) a (Node lr b r)"
-
+"del_min (Node (Node ll a lr) b r) =
+  (if ll = Leaf then Node lr b r else Node (del_min ll) a (Node lr b r))"
 
 lemma size_partition: "partition p t = (l',r') \<Longrightarrow> size t = size l' + size r'"
 by (induction p t arbitrary: l' r' rule: partition.induct)
@@ -144,14 +137,13 @@ qed
 
 
 lemma size_del_min[simp]: "size(del_min t) = size t - 1"
-by(induction t rule: del_min.induct) auto
+by(induction t rule: del_min.induct) (auto simp: neq_Leaf_iff)
 
 lemma set_del_min: "set_tree(del_min t) \<le> set_tree t"
-by(induction t rule: del_min.induct) auto
+by (induction t rule: del_min.induct) auto
 
 lemma bst_del_min: "bst_eq t \<Longrightarrow> bst_eq(del_min t)"
 apply(induction t rule: del_min.induct)
-   apply simp
   apply simp
  apply simp
 using set_del_min by fastforce
@@ -160,25 +152,23 @@ using set_del_min by fastforce
 subsection{* Analysis *}
 
 fun t_part :: "'a::linorder \<Rightarrow> 'a tree \<Rightarrow> nat" where
-"t_part p Leaf = 0" |
+"t_part p Leaf = 1" |
 "t_part p (Node l a r) =
   (if a \<le> p then
      case r of
-       Leaf \<Rightarrow> 0 |
+       Leaf \<Rightarrow> 1 |
        Node rl b rr \<Rightarrow> if b \<le> p then t_part p rr + 1 else t_part p rl + 1
    else case l of
-       Leaf \<Rightarrow> 0 |
+       Leaf \<Rightarrow> 1 |
        Node ll b lr \<Rightarrow> if b \<le> p then t_part p lr + 1 else t_part p ll + 1)" 
 
 definition t_in :: "'a::linorder \<Rightarrow> 'a tree \<Rightarrow> nat" where
 "t_in x h = t_part x h"
 
 fun t_dm :: "'a::linorder tree \<Rightarrow> nat" where
-"t_dm Leaf = 0" |
-"t_dm (Node Leaf _ r) = 0" |
-"t_dm (Node (Node Leaf _ l) b r) = 0" |
-"t_dm (Node (Node ll a lr) b r) = t_dm ll + 1"
-
+"t_dm Leaf = 1" |
+"t_dm (Node Leaf _ r) = 1" |
+"t_dm (Node (Node ll a lr) b r) = (if ll=Leaf then 1 else t_dm ll + 1)"
 
 abbreviation "\<phi> t == log 2 (size1 t)"
 
@@ -209,44 +199,48 @@ proof-
     using assms 1 by(simp add: powr_add log_powr[symmetric] powr_numeral)
 qed
 
-lemma amor_del_min: "t_dm t + \<Phi> (del_min t) - \<Phi> t \<le> 2 * \<phi> t"
-proof(induction t rule: del_min.induct)
-  case (3 a l b r)
-  let ?t = "Node (Node Leaf a l) b r"
-  have 1: "log 2 (real (size1 l) + real (size1 r))
-      \<le> 3 * log 2 (real (size1 l) + real (size1 r) + 1)" (is "?l \<le> 3 * ?r")
-  proof -
-    have "?l \<le> ?r" by(simp add: size1_def)
-    also have "\<dots> \<le> 3 * ?r" by(simp)
-    finally show ?thesis .
+lemma amor_del_min: "t_dm t + \<Phi> (del_min t) - \<Phi> t \<le> 2 * \<phi> t + 1"
+proof(induction t rule: t_dm.induct)
+  case (3 ll a lr b r)
+  let ?t = "Node (Node ll a lr) b r"
+  show ?case
+  proof cases
+    assume [simp]: "ll = Leaf"
+    have 1: "log 2 (real (size1 lr) + real (size1 r))
+        \<le> 3 * log 2 (real (size1 lr) + real (size1 r) + 1)" (is "?l \<le> 3 * ?r")
+    proof -
+      have "?l \<le> ?r" by(simp add: size1_def)
+      also have "\<dots> \<le> 3 * ?r" by(simp)
+      finally show ?thesis .
+    qed
+    have 2: "log 2 (real (size1 lr) + 1) \<ge> 0" by simp
+    thus ?case apply(simp add: real_of_nat_Suc) using 1 2 by linarith
+  next
+    assume ll[simp]: "\<not> ll = Leaf"
+    let ?l' = "del_min ll"
+    let ?s = "Node ll a lr"  let ?t = "Node ?s b r"
+    let ?s' = "Node lr b r"  let ?t' = "Node ?l' a ?s'"
+    have 0: "\<phi> ?t' \<le> \<phi> ?t" by(simp add: size1_def)
+    have 1: "\<phi> ll < \<phi> ?s" by(simp add: size1_def)
+    have 2: "log 2 (size1 ll + size1 ?s') \<le> log 2 (size1 ?t)" by(simp add: size1_def)
+    have "t_dm ?t + \<Phi> (del_min ?t) - \<Phi> ?t
+        = 1 + t_dm ll + \<Phi> (del_min ?t) - \<Phi> ?t" by simp
+    also have "\<dots> \<le> 2 + 2 * \<phi> ll + \<Phi> ll - \<Phi> ?l'  + \<Phi> (del_min ?t) - \<Phi> ?t"
+      using 3 ll by linarith
+    also have "\<dots> = 2 + 2 * \<phi> ll + \<phi> ?t' + \<phi> ?s' - \<phi> ?t - \<phi> ?s" by(simp)
+    also have "\<dots> \<le> 2 + \<phi> ll + \<phi> ?s'" using 0 1 by linarith
+    also have "\<dots> < 2 * \<phi> ?t + 1" using 2 add_log_log1[of "size1 ll" "size1 ?s'"]
+      by (simp add: size1_def real_of_nat_Suc)
+    finally show ?case by simp
   qed
-  have 2: "log 2 (real (size1 l) + 1) \<ge> 0" by simp
-  thus ?case apply(simp add: real_of_nat_Suc) using 1 2 by linarith
-next
-  case (4 lx x ly a r b u)
-  let ?l = "Node lx x ly"  let ?l' = "del_min ?l"
-  let ?s = "Node ?l a r"  let ?t = "Node ?s b u"
-  let ?s' = "Node r b u"  let ?t' = "Node ?l' b ?s'"
-  have 1: "\<phi> ?t' < \<phi> ?t" by(simp add: size1_def)
-  have 2: "\<phi> ?l < \<phi> ?s" by(simp add: size1_def)
-  have 3: "log 2 (size1 ?l + size1 ?s') \<le> log 2 (size1 ?t)" by(simp add: size1_def)
-  have "t_dm ?t + \<Phi> (del_min ?t) - \<Phi> ?t
-      = 1 + t_dm ?l + \<Phi> (del_min ?t) - \<Phi> ?t" by simp
-  also have "\<dots> \<le> 1 + 2 * \<phi> ?l + \<Phi> ?l - \<Phi> ?l'  + \<Phi> (del_min ?t) - \<Phi> ?t"
-    using 4 by linarith
-  also have "\<dots> = 1 + 2 * \<phi> ?l + \<phi> ?t' + \<phi> ?s' - \<phi> ?t - \<phi> ?s" by(simp)
-  also have "\<dots> \<le> 1 + \<phi> ?l + \<phi> ?s'" using 1 2 by linarith
-  also have "\<dots> < 2 * \<phi> ?t" using 3 add_log_log1[of "size1 ?l" "size1 ?s'"]
-    by (simp add: size1_def real_of_nat_Suc)
-  finally show ?case by simp
 qed auto
 
-lemma zig_zag:
+lemma zig_zig:
 fixes s u r r1' r2' T a b
 defines "t == Node s a (Node u b r)" and "t' == Node (Node s a u) b r1'"
 assumes "size r1' \<le> size r"
-    "t_part p r + \<Phi> r1' + \<Phi> r2' - \<Phi> r \<le> 2 * \<phi> r"
-shows "t_part p r + 1 + \<Phi> t' + \<Phi> r2' - \<Phi> t \<le> 2 * \<phi> t"
+    "t_part p r + \<Phi> r1' + \<Phi> r2' - \<Phi> r \<le> 2 * \<phi> r + 1"
+shows "t_part p r + 1 + \<Phi> t' + \<Phi> r2' - \<Phi> t \<le> 2 * \<phi> t + 1"
 proof -
   have 1: "\<phi> r \<le> \<phi> (Node u b r)" by (simp add: size1_def)
   have 2: "log 2 (real (size1 s + size1 u + size1 r1')) \<le> \<phi> t"
@@ -257,12 +251,12 @@ proof -
   thus ?thesis using assms 1 2 by (simp add: real_of_nat_Suc algebra_simps)
 qed
 
-lemma zig_zig:
+lemma zig_zag:
 fixes s u r r1' r2' a b
 defines "t \<equiv> Node s a (Node r b u)" and "t1' == Node s a r1'" and "t2' \<equiv> Node u b r2'"
 assumes "size r = size r1' + size r2'"
-    "t_part p r + \<Phi> r1' + \<Phi> r2' - \<Phi> r \<le> 2 * \<phi> r"
-shows "t_part p r + 1 + \<Phi> t1' + \<Phi> t2' - \<Phi> t \<le> 2 * \<phi> t"
+    "t_part p r + \<Phi> r1' + \<Phi> r2' - \<Phi> r \<le> 2 * \<phi> r + 1"
+shows "t_part p r + 1 + \<Phi> t1' + \<Phi> t2' - \<Phi> t \<le> 2 * \<phi> t + 1"
 proof -
   have 1: "\<phi> r \<le> \<phi> (Node u b r)" by (simp add: size1_def)
   have 2: "\<phi> r \<le> \<phi> t" by (simp add: t_def size1_def)
@@ -273,7 +267,7 @@ proof -
 qed
 
 lemma amor_partition: "bst_eq t \<Longrightarrow> partition p t = (l',r')
-  \<Longrightarrow> t_part p t + \<Phi> l' + \<Phi> r' - \<Phi> t \<le> 2 * log 2 (size1 t)"
+  \<Longrightarrow> t_part p t + \<Phi> l' + \<Phi> r' - \<Phi> t \<le> 2 * log 2 (size1 t) + 1"
 proof(induction p t arbitrary: l' r' rule: partition.induct)
   case 1 thus ?case by simp
 next
@@ -296,7 +290,7 @@ next
         have "size rrl \<le> size rr"
           using size_partition[OF 0(1)] by (simp add: size1_def)
         with 0 `a \<le> p` `b \<le> p` "2.prems"(1) "2.IH"(1)[OF _ Node , of rrl r']
-          zig_zag[where s=l and u=rl and r=rr and r1'=rrl and r2'=r' and p=p, of a b]
+          zig_zig[where s=l and u=rl and r=rr and r1'=rrl and r2'=r' and p=p, of a b]
         show ?thesis by (simp add: real_of_nat_Suc algebra_simps)
       next
         assume "\<not> b \<le> p"
@@ -305,7 +299,7 @@ next
           by (auto split: tree.splits prod.splits)
         from 0 `a \<le> p` `\<not> b \<le> p` "2.prems"(1) "2.IH"(2)[OF _ Node, of rll rlr]
           size_partition[OF 0(1)]
-          zig_zig[where s=l and u=rr and r=rl and r1'=rll and r2'=rlr and p=p, of a b]
+          zig_zag[where s=l and u=rr and r=rl and r1'=rll and r2'=rlr and p=p, of a b]
         show ?thesis by (simp add: real_of_nat_Suc algebra_simps)
       qed
     qed
@@ -325,7 +319,7 @@ next
           by (auto split: tree.splits prod.splits)
         from 0 `\<not> a \<le> p` `b \<le> p` "2.prems"(1) "2.IH"(3)[OF _ Node, of lrl lrr]
           size_partition[OF 0(1)]
-          zig_zig[where s=r and u=ll and r=lr and r1'=lrr and r2'=lrl and p=p, of a b]
+          zig_zag[where s=r and u=ll and r=lr and r1'=lrr and r2'=lrl and p=p, of a b]
         show ?thesis by (auto simp: real_of_nat_Suc algebra_simps)
       next
         assume "\<not> b \<le> p"
@@ -335,7 +329,7 @@ next
         have "size llr \<le> size ll"
           using size_partition[OF 0(1)] by (simp add: size1_def)
         with 0 `\<not> a \<le> p` `\<not> b \<le> p` "2.prems"(1) "2.IH"(4)[OF _ Node, of l' llr]
-          zig_zag[where s=r and u=lr and r=ll and r1'=llr and r2'=l' and p=p, of a b]
+          zig_zig[where s=r and u=lr and r=ll and r1'=llr and r2'=l' and p=p, of a b]
         show ?thesis by (auto simp: real_of_nat_Suc algebra_simps)
       qed
     qed
@@ -355,7 +349,7 @@ fun t\<^sub>p\<^sub>q :: "'a::linorder op\<^sub>p\<^sub>q \<Rightarrow> 'a tree 
 interpretation splay_heap: amor
 where init = "Leaf" and nxt = "nxt\<^sub>p\<^sub>q" and inv = "bst_eq"
 and t = t\<^sub>p\<^sub>q and \<Phi> = \<Phi>
-and U = "\<lambda>f s. case f of Delmin \<Rightarrow> 2 * \<phi> s | Insert _ \<Rightarrow> 3 * log 2 (size1 s + 1)"
+and U = "\<lambda>f s. case f of Delmin \<Rightarrow> 2 * \<phi> s + 1 | Insert _ \<Rightarrow> 3 * log 2 (size1 s + 1) + 1"
 proof
   case goal1 show ?case by simp
 next
