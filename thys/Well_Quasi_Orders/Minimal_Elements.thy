c4dbbfd1433b4ad95c092d783092516f214d79ed
Christian Sternagel <unknown>
2016-01-04 14:45:15 +0100

    "minimal_element" (equivalently) requires P to be a well-founed partial order; added fact about chains on LEX

--- thys/Well_Quasi_Orders/Minimal_Elements.thy
+++ thys/Well_Quasi_Orders/Minimal_Elements.thy
@@ -14,7 +14,7 @@ begin
 
 locale minimal_element =
   fixes P A
-  assumes trans: "transp_on P A"
+  assumes po: "po_on P A"
     and wf: "wfp_on P A"
 begin
 
@@ -34,7 +34,7 @@ proof (induction rule: wfp_on_induct)
     case False
     then obtain y where "y \<in> A" and "P y x" and "Q y" by blast
     with less show ?thesis
-      using trans [unfolded transp_on_def, rule_format, of _ y x] by blast
+      using po [THEN po_on_imp_transp_on, unfolded transp_on_def, rule_format, of _ y x] by blast
   qed
 qed
 
@@ -102,6 +102,33 @@ using min_elt_minimal [OF non_empty_ith, folded lexmin] .
 lemma lexmin_mem:
   "C \<subseteq> SEQ A \<Longrightarrow> C \<noteq> {} \<Longrightarrow> lexmin C i \<in> ith (eq_upto C (lexmin C) i) i"
 using min_elt_mem [OF non_empty_ith, folded lexmin] .
+
+lemma LEX_chain_on_eq_upto_imp_ith_chain_on:
+  assumes "chain_on (LEX P) (eq_upto C f i) (SEQ A)"
+  shows "chain_on P (ith (eq_upto C f i) i) A"
+using assms
+proof -
+  { fix x y assume "x \<in> ith (eq_upto C f i) i" and "y \<in> ith (eq_upto C f i) i"
+      and "\<not> P x y" and "y \<noteq> x"
+    then obtain g h where *: "g \<in> eq_upto C f i" "h \<in> eq_upto C f i"
+      and [simp]: "x = g i" "y = h i" and eq: "\<forall>j<i. g j = f j \<and> h j = f j"
+      by (auto simp: ith_def eq_upto_def)
+    with assms and \<open>y \<noteq> x\<close> have "LEX P g h \<or> LEX P h g" by (auto simp: chain_on_def)
+    moreover
+    { assume "LEX P g h"
+      with eq and \<open>y \<noteq> x\<close> have "P x y" using assms and *
+        by (auto simp: LEX_def)
+           (metis SEQ_iff chain_on_imp_subset linorder_neqE_nat minimal subsetCE)
+      with \<open>\<not> P x y\<close> have "P y x" .. }
+    moreover
+    { assume "LEX P h g"
+      with eq and \<open>y \<noteq> x\<close> have "P y x" using assms and *
+        by (auto simp: LEX_def)
+           (metis SEQ_iff chain_on_imp_subset linorder_neqE_nat minimal subsetCE) }
+    ultimately have "P y x" by (auto simp: chain_on_def LEX_def) }
+  then show ?thesis using assms by (auto simp: chain_on_def) blast
+qed
+
 
 end
 
