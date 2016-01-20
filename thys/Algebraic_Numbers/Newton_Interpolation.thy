04ccd1f6c30375f5dd6c23bddb5aac52e3cd9055
Rene Thiemann <rene.thiemann@uibk.ac.at>
2016-01-20 20:30:50 +0100

    tuning of Newton interpolation

--- thys/Algebraic_Numbers/Newton_Interpolation.thy
+++ thys/Algebraic_Numbers/Newton_Interpolation.thy
@@ -19,17 +19,10 @@ begin
 context
 fixes 
   ty :: "'a :: field itself"
-  and x :: "nat \<Rightarrow> 'a"
-  and f :: "nat \<Rightarrow> 'a"
   and xs :: "'a list"
   and fs :: "'a list"
 begin
 
-private function xij_f :: "nat \<Rightarrow> nat \<Rightarrow> 'a" where
-  "xij_f i j = (if i < j then (xij_f (i + 1) j - xij_f i (j - 1)) / (x j - x i) else f i)"
-  by pat_completeness auto
-
-termination by (relation "measure (\<lambda> (i,j). j - i)", auto)
 
 private fun combine_rows :: "'a list \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> 'a list \<Rightarrow> 'a list" where
   "combine_rows [] fj xj xis = [fj]"
@@ -47,31 +40,22 @@ private fun newton_coefficients_main :: "'a list \<Rightarrow> 'a list \<Rightar
     in new_row # rec)"
 | "newton_coefficients_main _ _ = []"
 
-private definition c :: "nat \<Rightarrow> 'a" where
-  "c i = xij_f 0 i"
-
-private fun newton_coefficients :: "nat \<Rightarrow> 'a list" where
-  "newton_coefficients n = map hd (newton_coefficients_main (rev fs) (rev xs))"
-
-private fun b :: "nat \<Rightarrow> nat \<Rightarrow> 'a poly" where
-  "b 0 n = [:c n:]"
-| "b (Suc j) i = b j (i + 1) * [: - x i, 1 :] + [:c i:]"
+private definition newton_coefficients :: "'a list" where
+  "newton_coefficients = map hd (newton_coefficients_main (rev fs) (rev xs))"
 
 private fun newton_composition :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a poly" where
   "newton_composition [cn] xis = [:cn:]"
 | "newton_composition (ci # cs) (xi # xis) = newton_composition cs xis * [:- xi, 1:] + [:ci:]"
 | "newton_composition _ _ = 0"
 
-definition newton_poly :: "nat \<Rightarrow> 'a poly" where
-  "newton_poly n = b n 0"
+definition newton_poly_impl :: "'a poly" where
+  "newton_poly_impl = newton_composition (rev (newton_coefficients)) xs"
 
-definition newton_poly_impl :: "nat \<Rightarrow> 'a poly" where
-  "newton_poly_impl n = newton_composition (rev (newton_coefficients n)) xs"
 end
 
 definition newton_interpolation_poly :: "('a :: field \<times> 'a)list \<Rightarrow> 'a poly" where
-  "newton_interpolation_poly x_fs = (let n = length x_fs - 1;
+  "newton_interpolation_poly x_fs = (let 
     xs = map fst x_fs; fs = map snd x_fs in
-    newton_poly_impl xs fs n)"
+    newton_poly_impl xs fs)"
 
 end
