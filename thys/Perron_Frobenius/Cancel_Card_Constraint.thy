8e88f807df99ec14796f7308598a2a77a249bf72
Rene Thiemann <rene.thiemann@uibk.ac.at>
2016-06-03 12:25:08 +0200

    documentation

--- thys/Perron_Frobenius/Cancel_Card_Constraint.thy
+++ thys/Perron_Frobenius/Cancel_Card_Constraint.thy
@@ -30,4 +30,44 @@ qed
 
 ML_file "cancel_card_constraint.ML"
 
+
+(* below you find an example what the attribute cancel_card_constraint can do and how
+   it works internally *)
+
+(*
+(* input: some type based lemma with CARD inside, like t0 *)
+consts P :: "nat \<Rightarrow> nat \<Rightarrow> bool" 
+axiomatization where t0: "P (CARD ('a :: finite)) (CARD('a) * m)"
+
+(* t0 is converted into a property without the cardinality constraint via the new attribute *)
+lemma t_1_to_6: "n \<noteq> 0 \<Longrightarrow> P n (n * m)"
+  by (rule t0[cancel_card_constraint])
+
+(* The internal steps are as follows. *)
+
+(* 1st step: pull out CARD and introduce new variable n *)
+lemma t1: "CARD('a :: finite) = n \<Longrightarrow> P n (n * m)" 
+  using t0[where 'a = 'a] by blast
+
+(* 2nd step: get rid of sorts *)
+lemma t2: "class.finite TYPE('a) \<Longrightarrow> CARD('a) = n \<Longrightarrow> P n (n * m)"
+  by (rule t1[internalize_sort "?'a::finite"])
+
+(* 3rd step: restructure thm so that first two assumptions are merged into one *)
+lemma t3: "class.finite TYPE('a) \<and> CARD('a) = n \<Longrightarrow> P n (n * m)" 
+  using t2 by blast
+
+(* 4th step: choose an appropriate carrier set *)
+lemma t4: "\<exists>Rep Abs. type_definition Rep Abs {0..<n} \<Longrightarrow> P n (n * m)"
+  by (rule t3[OF type_impl_card_n])
+
+(* 5th step: cancel type definition *)
+lemma t5: "{0..<n} \<noteq> {} \<Longrightarrow> P n (n * m)"
+  by (rule t4[cancel_type_definition])
+
+(* 6th step: simplify non-empty assumption to obtain final theorem *)
+lemma t6: "n \<noteq> 0 \<Longrightarrow> P n (n * m)" 
+  by (rule t5[OF n_zero_nonempty])
+*)
+
 end
