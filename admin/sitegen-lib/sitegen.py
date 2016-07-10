ec171d16ca44c5529ea98831ad9393c1425e0167
Lars Hupel <lars.hupel@mytum.de>
2016-07-10 18:31:58 +0200

    tuned

--- admin/sitegen-lib/sitegen.py
+++ admin/sitegen-lib/sitegen.py
@@ -14,9 +14,9 @@
 # Cross-python compatibility
 from __future__ import print_function
 try:
-    import configparser
+	import configparser
 except ImportError:
-    from six.moves import configparser
+	from six.moves import configparser
 
 from collections import OrderedDict
 from optparse import OptionParser
@@ -313,77 +313,78 @@ def normpath(path, *paths):
 	"""Return a normalized and absolute path (depending on current working directory)"""
 	return os.path.abspath(os.path.join(path, *paths))
 
-def theory_dict(articles):
+def theory_dict(entries):
 	"""Creates a dict with .thy files as key and the corresponding theory as value"""
 	thys = dict()
-	for a in articles:
-		for thy in articles[a]['thys']:
-			thys[thy] = a
+	for entry in entries:
+		for thy in entries[entry]['thys']:
+			thys[thy] = entry
 	return thys
 
-def add_theories(articles, thy_dir):
+def add_theories(entries, thy_dir):
 	"""Add a set of paths to .thy files of an entry to entries[e]['thys']"""
-	for a in articles:
-		articles[a]['thys'] = set()
-		for root, _dirnames, filenames in os.walk(os.path.join(thy_dir, a)):
+	for entry in entries:
+		entries[entry]['thys'] = set()
+		for root, _dirnames, filenames in os.walk(os.path.join(thy_dir, entry)):
 			for f in filenames:
 				if f.endswith(".thy"):
-					articles[a]['thys'].add(normpath(root, f))
+					entries[entry]['thys'].add(normpath(root, f))
 
-def add_theory_dependencies(articles):
+def add_theory_dependencies(entries):
 	"""Adds dependencies by checking .thy-files"""
-	thys = theory_dict(articles)
+	thys = theory_dict(entries)
 	pattern0 = re.compile("imports(.*?)begin", re.DOTALL)
 	for t in thys:
 		with open(t, 'r') as f:
 			content = f.read()
 		match0 = pattern0.search(content)
 		if match0 is not None:
-			#Go through imports and check if its a file in the AFP
-			#if yes, add dependency
+			# Go through imports and check if its a file in the AFP
+			# if yes, add dependency
 			imps = [normpath(os.path.dirname(t), x.strip(' \t"') + ".thy")
 				for x in match0.group(1).split()]
 			for i in imps:
 				if i in thys:
-					articles[thys[t]]['depends-on'].add(thys[i])
+					entries[thys[t]]['depends-on'].add(thys[i])
 
-def add_root_dependencies(articles, thy_dir):
+def add_root_dependencies(entries, thy_dir):
 	"""Adds dependencies by checking ROOT files"""
-	thys = theory_dict(articles)
-	for a in articles:
-		root = normpath(thy_dir, a, "ROOT")
+	thys = theory_dict(entries)
+	for entry in entries:
+		root = normpath(thy_dir, entry, "ROOT")
 		with open(root, 'r') as root_file:
 			root_content = root_file.read()
 		#check for imports of the form "session ... = ... (AFP) +"
 		for line in root_content.splitlines():
 			if line.startswith("session"):
 				for word in [x.strip(' \t"') for x in line.split()]:
-					if word in articles:
-						articles[a]['depends-on'].add(word)
+					if word in entries:
+						entries[entry]['depends-on'].add(word)
 		#run through every word in the root file and check if there's a corresponding AFP file
 		for word in [x.strip(' \t"') for x in root_content.split()]:
 			#catch Unicode error, since paths can only contain ASCII chars
 			try:
-				theory = normpath(thy_dir, a, word + ".thy")
+				theory = normpath(thy_dir, entry, word + ".thy")
 				if theory in thys:
-					articles[a]['depends-on'].add(thys[theory])
+					entries[entry]['depends-on'].add(thys[theory])
 			except UnicodeDecodeError:
 					pass
 
-def remove_self_imps(articles):
+def remove_self_imps(entries):
 	"""Remove self imports in "depends-on"-field"""
-	for a in articles:
+	for entry in entries:
 		try:
-			articles[a]['depends-on'].remove(a)
+			entries[entry]['depends-on'].remove(entry)
 		except KeyError:
 			pass
 
-def add_used_by(articles):
-	for a in articles:
-		articles[a]['used_by'] = set()
-	for a in articles:
-		for d in articles[a]['depends-on']:
-			articles[d]['used_by'].add(a)
+def add_used_by(entries):
+	for entry in entries:
+		entries[entry]['used_by'] = set()
+	for entry in entries:
+		for d in entries[entry]['depends-on']:
+			entries[d]['used_by'].add(entry)
+
 
 if __name__ == "__main__":
 	parser = OptionParser(usage = "Usage: %prog [--no-warn] [--debug] [--check] [--dest=DEST_DIR] --metadata=METADATA_DIR THYS_DIR")
