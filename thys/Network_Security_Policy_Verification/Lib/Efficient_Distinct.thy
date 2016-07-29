69a89e8266d43aa3dc152df20123336f83ea4e03
immler <unknown>
2016-07-29 11:27:08 +0200

    Product_Lexorder for IP_Addresses and Network_Security_Policy_Verification

--- thys/Network_Security_Policy_Verification/Lib/Efficient_Distinct.thy
+++ thys/Network_Security_Policy_Verification/Lib/Efficient_Distinct.thy
@@ -1,6 +1,7 @@
 theory Efficient_Distinct
 imports 
   "../../Automatic_Refinement/Lib/Misc" (*mergesort*)
+  "~~/src/HOL/Library/Product_Lexorder"
   "~~/src/HOL/Library/List_lexord"
   "~~/src/HOL/Library/Code_Char"
   TopoS_Util
