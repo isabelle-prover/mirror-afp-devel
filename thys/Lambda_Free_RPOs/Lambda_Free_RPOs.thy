a6794d18ad7ba71fcef96584546d3b4f40bbd77f
paulson <lp15@cam.ac.uk>
2016-10-05 16:58:20 +0100

    Lambda_Free_RPOs

---
+++ thys/Lambda_Free_RPOs/Lambda_Free_RPOs.thy
@@ -0,0 +1,14 @@
+(*  Title:       Recursive Path Orders for Lambda-Free Higher-Order Terms
+    Author:      Jasmin Blanchette <jasmin.blanchette at inria.fr>, 2016
+    Author:      Uwe Waldmann <waldmann at mpi-inf.mpg.de>, 2016
+    Author:      Daniel Wand <dwand at mpi-inf.mpg.de>, 2016
+    Maintainer:  Jasmin Blanchette <jasmin.blanchette at inria.fr>
+*)
+
+section \<open>Recursive Path Orders for Lambda-Free Higher-Order Terms\<close>
+
+theory Lambda_Free_RPOs
+imports Lambda_Free_RPO_App Lambda_Free_RPO_Optim
+begin
+
+end
