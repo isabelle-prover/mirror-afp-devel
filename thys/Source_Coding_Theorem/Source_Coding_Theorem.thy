78b9d3fdaf60415157b0caaf139cd51f7b2b64ee
nipkow <unknown>
2016-10-20 08:08:56 +0200

    updated to Isabelle/devel

--- thys/Source_Coding_Theorem/Source_Coding_Theorem.thy
+++ thys/Source_Coding_Theorem/Source_Coding_Theorem.thy
@@ -118,7 +118,7 @@ definition \<K> :: "real" where
 lemma pos_cw_len: "0 < 1 / b ^ cw_len i" using b_gt_1 by simp
 
 lemma \<K>_pos: "0 < \<K>"
-    using emp_L fin_L pos_cw_len setsum_pos \<K>_def
+    using emp_L fin_L pos_cw_len sum_pos \<K>_def
     by metis
 
 lemma \<K>_pow: "\<K> = (\<Sum>i\<in>L. 1 / b powr cw_len i)"
@@ -190,8 +190,8 @@ lemma cartesian_product:
   assumes "finite A"
   assumes "finite B"
 shows "(\<Sum>b\<in>B. g b) * (\<Sum>a\<in>A. f a) = (\<Sum>ab\<in>A\<times>B. f (fst ab) * g (snd ab))"
-    using bilinear_times bilinear_setsum[where h="(\<lambda>x y. x * y)" and f="f" and g="g"] assms
-    by (metis (erased, lifting) setsum.cong split_beta' Groups.ab_semigroup_mult_class.mult.commute)
+    using bilinear_times bilinear_sum[where h="(\<lambda>x y. x * y)" and f="f" and g="g"] assms
+    by (metis (erased, lifting) sum.cong split_beta' Groups.ab_semigroup_mult_class.mult.commute)
 
 lemma \<K>_power:
 shows "\<K>^k = (\<Sum>w \<in> (k_words k). 1 / b^(cw_len_concat w))"
@@ -212,7 +212,7 @@ next
     also have "\<dots> = (\<Sum>wi \<in> L \<times> k_words n. 1 / b^cw_len_concat (fst wi # snd wi))"
       by (metis (erased, lifting) add.commute comp_apply foldr.simps(2))
     also have "\<dots> = (\<Sum>w \<in> (k_words (Suc n)). 1 / b^(cw_len_concat w))"
-      using bij_k_words setsum.reindex_bij_betw by fastforce
+      using bij_k_words sum.reindex_bij_betw by fastforce
     finally show ?case by simp
 qed
 
@@ -236,7 +236,7 @@ next
     let ?rr = "(\<Sum>m = 0..<bd. ?ff m (insert x F))"
     have "(f x) \<in> {0..<bd}" using assms by simp
     hence "\<And>h::(nat \<Rightarrow> real). (\<Sum>m=0..<bd. h m) = (\<Sum>y\<in>({0..<bd} - {f x}).h y) + h (f x)"
-      by (metis diff_add_cancel finite_atLeastLessThan setsum_diff1_ring)
+      by (metis diff_add_cancel finite_atLeastLessThan sum_diff1_ring)
     moreover hence
     "(\<Sum>m = 0..<bd. ?ff m (insert x F))
     = (\<Sum>m\<in>{0..<bd} - {f x}. ?ff m (insert x F)) + card (f -` {f x} \<inter> F) * g (f x) + g (f x)"
@@ -336,7 +336,7 @@ proof -
     "(\<Sum>m=1..<Suc (k * max_len). card (set_of_k_words_length_m k m) / b^m)
     \<le> (\<Sum>m=1..<Suc(k * max_len). b^m / b^m)"
       using set_of_k_words_bound b_val
-    Groups_Big.setsum_mono[of "{1..<Suc(k * max_len)}"
+    Groups_Big.sum_mono[of "{1..<Suc(k * max_len)}"
     "(\<lambda>m. (card (set_of_k_words_length_m k m))/b^m)" "\<lambda>m. b^m /b^m"]
       by simp
     moreover have"(\<Sum>m=1..<Suc(k * max_len). b^m / b^m) = (\<Sum>m=1..<Suc(k *max_len). 1)"
@@ -345,9 +345,9 @@ proof -
       by simp
     ultimately have
     "(\<Sum>m = 1..<Suc (k * max_len). card (set_of_k_words_length_m k m) / b ^ m) \<le> k * max_len"
-      by (metis One_nat_def card_atLeastLessThan card_eq_setsum diff_Suc_Suc real_of_card)
+      by (metis One_nat_def card_atLeastLessThan card_eq_sum diff_Suc_Suc real_of_card)
     thus ?thesis using empty_set_k_words assms
-      by (simp add: setsum_shift_lb_Suc0_0_upt split: split_if_asm)
+      by (simp add: sum_shift_lb_Suc0_0_upt split: if_split_asm)
 qed
 
 lemma \<K>_power_bound :
@@ -399,13 +399,13 @@ proof -
     fix i
     assume "i\<in>S"
     hence "a i / (e i / d) \<le> a i / e i" using assms
-      by (metis (no_types) divide_1 frac_le less_imp_triv not_less)
+      by (metis (no_types) div_by_1 frac_le less_imp_triv not_less)
     hence "log b (a i / (e i / d)) \<le> log b (a i / e i)" using assms(1)
       by (metis (full_types) b_gt_1 divide_divide_eq_left inverse_divide le_less_linear log_le
     log_neg_const order_refl times_divide_eq_right zero_less_mult_iff)
     }
     thus "(\<Sum>i\<in>S. a i * log b (a i / (e i / d))) \<le> (\<Sum>i\<in>S. a i * log b (a i / e i))"
-      by (meson mult_left_mono assms setsum_mono)
+      by (meson mult_left_mono assms sum_mono)
 qed
 
 lemma KL_div_pos:
@@ -426,7 +426,7 @@ proof -
       using non_null
       by (simp add: order.strict_implies_order)
     have "- log b (\<Sum>i\<in>S. a i * e i / a i) \<le> (\<Sum>i\<in>S. a i * - log b (e i / a i))"
-      using convex_on_setsum[
+      using convex_on_sum[
     OF fin,
     OF nemp,OF minus_log_convex[OF b_gt_1],
     OF convex_real_interval(3),
@@ -472,7 +472,7 @@ proof -
     ultimately have
     eq: "KL_div S a d = KL_div (S \<inter> {i. 0 < a i}) a d + KL_div (S \<inter> {i. 0 = a i}) a d"
       unfolding KL_div_def
-      by (metis (mono_tags, lifting) fin finite_Un setsum.union_disjoint)
+      by (metis (mono_tags, lifting) fin finite_Un sum.union_disjoint)
     have "KL_div (S \<inter> {i. 0 = a i}) a d = 0" unfolding KL_div_def by simp
     hence "KL_div S a d = KL_div (S \<inter> {i. 0 < a i}) a d" using eq by simp
     moreover have "0 \<le> KL_div (S \<inter> {i. 0 < a i}) a d"
@@ -484,13 +484,13 @@ proof -
       let ?c = "\<lambda>i. d i / (\<Sum>j \<in>(S \<inter> {i. 0 < a i}). d j)"
       have 1: "(\<And>i. i \<in> S \<inter> {i. 0 < a i} \<Longrightarrow> 0 < a i)" by simp
       have 2: "(\<And>i. i \<in> S \<inter> {i. 0 < a i} \<Longrightarrow> 0 < ?c i)"
-        by (metis False IntD1 divide_pos_pos fin finite_Int non_null(2) setsum_pos)
+        by (metis False IntD1 divide_pos_pos fin finite_Int non_null(2) sum_pos)
       have 3: "(\<Sum>i\<in> (S \<inter> {i. 0 < a i}). a i) = 1"
-        using setsum.cong[of S, of S, of "(\<lambda>x. if x \<in> {i. 0 < a i} then a x else 0)", of a]
-      setsum.inter_restrict[OF fin, of a] non_null(1) sum_a_one
+        using sum.cong[of S, of S, of "(\<lambda>x. if x \<in> {i. 0 < a i} then a x else 0)", of a]
+      sum.inter_restrict[OF fin, of a] non_null(1) sum_a_one
         by fastforce
       have "(\<Sum>i\<in>S \<inter> {j. 0 < a j}. ?c i) = (\<Sum>i\<in>S \<inter> {j. 0 < a j}. d i) / (\<Sum>i\<in>S \<inter> {j. 0 < a j}. d i)"
-        by (metis setsum_divide_distrib)
+        by (metis sum_divide_distrib)
       hence 5: "(\<Sum>i\<in>S \<inter> {j. 0 < a j}. ?c i) = 1" using 2 False by force
       hence "0 \<le> KL_div (S \<inter> {j. 0 < a j}) a ?c"
         using KL_div_pos_gen[
@@ -498,7 +498,7 @@ proof -
       ] 1 2 3
         by (metis fin)
       have fstdb: "0 < (\<Sum>i\<in>S \<inter> {i. 0 < a i}. d i)" using non_null(2) False
-        by (metis Int_Collect fin finite_Int setsum_pos)
+        by (metis Int_Collect fin finite_Int sum_pos)
       have 6: "0 \<le> KL_div (S \<inter> {i. 0 < a i}) a (\<lambda>i. d i / (\<Sum>i\<in>(S \<inter> {i. 0 < a i}). d i))"
         using 2 3 5
         KL_div_pos_gen[
@@ -507,8 +507,8 @@ proof -
         by simp
       hence
       "KL_div (S \<inter> {j. 0 < a j}) a (\<lambda>i. d i / (\<Sum>i\<in>(S \<inter> {i. 0 < a i}). d i)) \<le> KL_div (S \<inter> {j. 0 < a j}) a d"
-        using non_null setsum.inter_restrict[OF fin, of d, of "{i. 0 < a i}"]
-        setsum_mono[of S, of "(\<lambda>x. if x \<in> {i. 0 < a i} then d x else 0)", of d] non_null(2) sum_c_one
+        using non_null sum.inter_restrict[OF fin, of d, of "{i. 0 < a i}"]
+        sum_mono[of S, of "(\<lambda>x. if x \<in> {i. 0 < a i} then d x else 0)", of d] non_null(2) sum_c_one
         non_null(2) fstdb KL_div_mul
         by force
       moreover have "0 \<le> KL_div (S \<inter> {j. 0 < a j}) a (\<lambda>i. d i / (\<Sum>i\<in>(S \<inter> {i. 0 < a i}). d i))"
@@ -522,7 +522,7 @@ lemma sum_div_1:
   fixes f::"'b \<Rightarrow> 'c::field"
   assumes "(\<Sum>i\<in>A. f i) \<noteq> 0"
 shows "(\<Sum>i\<in>A. f i / (\<Sum>j\<in>A. f j)) = 1"
-    by (metis (no_types) assms right_inverse_eq setsum_divide_distrib)
+    by (metis (no_types) assms right_inverse_eq sum_divide_distrib)
 
 theorem rate_lower_bound:
 shows "\<H>(X) \<le> code_rate enc X"
@@ -544,7 +544,7 @@ proof -
     = fi i * log b (fi i / (1 / b powr (cw_len i)))"
       by simp
     have sum_one_L: "(\<Sum> i \<in> L. fi i) = 1"
-      using simple_distributed_setsum_space[OF distr_i] by (simp add: L_def)
+      using simple_distributed_sum_space[OF distr_i] by (simp add: L_def)
     {
     fix i
     assume "i \<in> L"
@@ -565,20 +565,20 @@ proof -
     also have 2: "(\<Sum>i\<in>L. fi i * cw_len i) = (\<Sum>i \<in> L. fi i * (-log b (1/(b powr (cw_len i)))))"
       using b_gt_1 log_divide by simp
     also have "\<dots> = -1 * (\<Sum>i \<in> L. fi i * (log b (1/(b powr (cw_len i)))))"
-      using setsum_right_distrib[of "-1" "(\<lambda>i. fi i * (- 1 * log b (1 / b powr (cw_len i))))" L]
+      using sum_distrib_left[of "-1" "(\<lambda>i. fi i * (- 1 * log b (1 / b powr (cw_len i))))" L]
       by simp
     finally have
     "?cr - \<H>(X) = -(\<Sum>i \<in> L. fi i * log b (1/b powr cw_len i)) + (\<Sum>i \<in> L. fi i * log b (fi i))"
       by simp
     have "?cr - \<H>(X) = (\<Sum>i \<in> L. fi i * ((log b (1/ (1/(b powr (cw_len i))))) + log b (fi i)))"
       using b_gt_1 1
-      by (simp add: distrib_left setsum.distrib)
+      by (simp add: distrib_left sum.distrib)
     also have "\<dots> = (\<Sum>i \<in> L. fi i *((log b (fi i / (1/(b powr (cw_len i)))))))"
-      using Cartesian_Euclidean_Space.setsum_cong_aux[OF eqpi] by simp
+      using Cartesian_Euclidean_Space.sum_cong_aux[OF eqpi] by simp
     also from big_eq have
     "\<dots> = (\<Sum>i\<in>L. fi i * (log b (fi i * \<K> / (1 / b powr (cw_len i))))) + (\<Sum>i \<in> L. fi i) * log b (1/ \<K>)"
       using \<K>_pos
-      by (simp add: setsum_left_distrib setsum.distrib)
+      by (simp add: sum_distrib_right sum.distrib)
     also have "\<dots> = (\<Sum>i\<in>L. fi i * (log b (fi i * \<K> / (1 / b powr (cw_len i))))) - log b (\<K>)"
       using \<K>_pos
       by (simp add: log_inverse_eq divide_inverse sum_one_L)
@@ -599,7 +599,7 @@ proof -
     ultimately have "0 \<le> KL_div L fi ?r"
       using KL_div_pos2[OF fin_L fi_pos] by simp
     hence "log b (1 / \<K>) \<le> ?cr - \<H>(X)" using code_ent_kl_log by simp
-    moreover from McMillan assms have "0 \<le> log b (1 / \<K>)"
+    moreover from McMillan have "0 \<le> log b (1 / \<K>)"
       using \<K>_pos
       by (simp add: b_gt_1)
     ultimately show ?thesis by simp
