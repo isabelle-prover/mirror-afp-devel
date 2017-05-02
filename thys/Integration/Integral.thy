809d6f952a87205e055bc647bb69c34e99e246d3
paulson <lp15@cam.ac.uk>
2017-05-02 16:56:53 +0100

    Another sum_nonneg fix for eba08da54c6b

--- thys/Integration/Integral.thy
+++ thys/Integration/Integral.thy
@@ -1766,12 +1766,8 @@ lemma assumes (*<*)ms:(*>*) "measure_space M" and (*<*)f(*>*): "f \<in> rv M" an
             
           also 
           { fix m
-            { fix i assume "i \<in> {..<(m*2^m)}-{0}"
-              hence  "0 \<le> real i / (2::real)^m * \<chi> (A m i) t"
-                by (simp add: characteristic_function_def zero_le_divide_iff)
-            } hence "\<forall>i\<in>{..<(m*2^m)}-{0}. 0 \<le> real i / (2::real)^m * \<chi> (A m i) t"
-              by fast
-            hence "0 \<le> u m t"  by (simp add: u_def sum_nonneg)
+            have "0 \<le> u m t"
+              by (simp add: u_def characteristic_function_def zero_le_divide_iff sum_nonneg)
           }
           hence "0 \<le> u (Suc n) t" .
 
