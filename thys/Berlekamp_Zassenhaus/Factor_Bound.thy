024bf575b2ad7aeb382c55e05b598c464f0f0335
Rene Thiemann <rene.thiemann@uibk.ac.at>
2018-02-07 16:50:58 +0100

    reuse generalized theorems in Factor_Bound_2 in Mahler_Measure and Factor_Bound

--- thys/Berlekamp_Zassenhaus/Factor_Bound.thy
+++ thys/Berlekamp_Zassenhaus/Factor_Bound.thy
@@ -119,50 +119,39 @@ qed
 
 lemma mignotte_helper_coeff_int': "cmod (coeff_int (\<Prod>a\<leftarrow>lst. [:- a, 1:]) i)
     \<le> ((length lst - 1) choose i) * (\<Prod>a\<leftarrow>lst. (max 1 (cmod a))) 
-    + ((length lst - 1) choose (nat (i - 1)))"
-  by (rule order.trans[OF mignotte_helper_coeff_int], auto simp: choose_int_def)
+    + min i 1 * ((length lst - 1) choose (nat (i - 1)))"
+  by (rule order.trans[OF mignotte_helper_coeff_int], auto simp: choose_int_def min_def)
 
-lemma mignotte_helper_coeff:
+lemma mignotte_helper_coeff: 
   "cmod (coeff h i) \<le> (degree h - 1 choose i) * mahler_measure_poly h 
-      + (degree h - 1 choose (i - 1)) * cmod (lead_coeff h)"
+      + min i 1 * (degree h - 1 choose (i - 1)) * cmod (lead_coeff h)"
 proof -
   let ?r = "complex_roots_complex h"
   have "cmod (coeff h i) = cmod (coeff (smult (lead_coeff h) (\<Prod>a\<leftarrow>?r. [:- a, 1:])) i)"
     unfolding complex_roots by auto
   also have "\<dots> = cmod (lead_coeff h) * cmod (coeff (\<Prod>a\<leftarrow>?r. [:- a, 1:]) i)" by(simp add:norm_mult)
-  also have "\<dots> \<le> cmod (lead_coeff h) * ((degree h - 1 choose i) * mahler_measure_monic h + ((degree h - 1) choose nat (int i - 1)))"    
+  also have "\<dots> \<le> cmod (lead_coeff h) * ((degree h - 1 choose i) * mahler_measure_monic h + 
+    (min i 1 * ((degree h - 1) choose nat (int i - 1))))"    
     unfolding mahler_measure_monic_def
     by (rule mult_left_mono, insert mignotte_helper_coeff_int'[of ?r i], auto)
-  also have "\<dots> = (degree h - 1 choose i) * mahler_measure_poly h + cmod (lead_coeff h) * ((degree h - 1) choose nat (int i - 1))" 
+  also have "\<dots> = (degree h - 1 choose i) * mahler_measure_poly h + cmod (lead_coeff h) * (
+    min i 1 * ((degree h - 1) choose nat (int i - 1)))" 
     unfolding mahler_measure_poly_via_monic by (simp add: field_simps)
   also have "nat (int i - 1) = i - 1" by (cases i, auto)
-  finally show ?thesis by (simp add: ac_simps)
+  finally show ?thesis by (simp add: ac_simps split: if_splits)
 qed
-
-lemma cmod_through_lead_coeff[simp]:
-  "cmod (lead_coeff (of_int_poly h)) = abs (lead_coeff h)"
-  by simp
 
 lemma mignotte_coeff_helper:
   "abs (coeff h i) \<le> 
    (degree h - 1 choose i) * mahler_measure h +
-   (degree h - 1 choose (i - 1)) * abs (lead_coeff h)"
+   (min i 1 * (degree h - 1 choose (i - 1)) * abs (lead_coeff h))"
   unfolding mahler_measure_def
   using mignotte_helper_coeff[of "of_int_poly h" i] 
   by auto
 
-lemma mahler_measure_poly_ge_1:
-  assumes "h \<noteq> 0"
-  shows "(1::real) \<le> mahler_measure h"
-proof -
-  have rc: "\<bar>real_of_int i\<bar> = of_int \<bar>i\<bar>" for i by simp
-  from assms have "cmod (lead_coeff (map_poly complex_of_int h)) > 0" by simp
-  hence "cmod (lead_coeff (map_poly complex_of_int h)) \<ge> 1"
-    by(cases "lead_coeff h = 0", auto simp del: leading_coeff_0_iff)
-  from mult_mono[OF this mahler_measure_monic_ge_1 norm_ge_zero]
-  show ?thesis unfolding mahler_measure_def mahler_measure_poly_via_monic
-    by auto
-qed
+lemma cmod_through_lead_coeff[simp]:
+  "cmod (lead_coeff (of_int_poly h)) = abs (lead_coeff h)"
+  by simp
 
 lemma choose_approx: "n \<le> N \<Longrightarrow> n choose k \<le> N choose (N div 2)" 
   by (rule order.trans[OF binomial_mono_left binomial_maximum])
@@ -174,23 +163,29 @@ definition mignotte_bound :: "int poly \<Rightarrow> nat \<Rightarrow> int" wher
   "mignotte_bound f d = (let d' = d - 1; d2 = d' div 2; binom = (d' choose d2) in
      (mahler_approximation 2 binom f + binom * abs (lead_coeff f)))" 
 
-lemma mignotte_bound: fixes mm :: "nat \<Rightarrow> int poly \<Rightarrow> int" 
-    assumes "f \<noteq> 0" "g dvd f" "degree g \<le> n"
-  shows "\<bar>coeff g k\<bar> \<le> mignotte_bound f n"  
+lemma mignotte_bound_main:  
+  assumes "f \<noteq> 0" "g dvd f" "degree g \<le> n"
+  shows "\<bar>coeff g k\<bar> \<le> \<lfloor>real (n - 1 choose k) * mahler_measure f\<rfloor> +
+       int (min k 1 * (n - 1 choose (k - 1))) * \<bar>lead_coeff f\<bar>"   
 proof-
   let ?bnd = 2
-  let ?n = "(n - 1) choose ((n - 1) div 2)" 
+  let ?n = "(n - 1) choose k" 
+  let ?n' = "min k 1 * ((n - 1) choose (k - 1))" 
   let ?approx = "mahler_approximation ?bnd ?n f" 
   obtain h where gh:"g * h = f" using assms by (metis dvdE)
   have nz:"g\<noteq>0" "h\<noteq>0" using gh assms(1) by auto
   have g1:"(1::real) \<le> mahler_measure h" using mahler_measure_poly_ge_1 gh assms(1) by auto
   note g0 = mahler_measure_ge_0
-  have to_n: "(degree g - 1 choose k) \<le> real ?n" for k
-    using choose_approx[of "degree g - 1" "n - 1" k] assms(3) by auto
+  have to_n: "(degree g - 1 choose k) \<le> real ?n"
+    using binomial_mono_left[of "degree g - 1" "n - 1" k] assms(3) by auto
+  have to_n': "min k 1 * (degree g - 1 choose (k - 1)) \<le> real ?n'"
+    using binomial_mono_left[of "degree g - 1" "n - 1" "k - 1"] assms(3)
+    by (simp add: min_def)
   have "\<bar>coeff g k\<bar> \<le> (degree g - 1 choose k) * mahler_measure g
-    + real (degree g - 1 choose (k - 1)) * \<bar>lead_coeff g\<bar>" using mignotte_coeff_helper[of g k] by simp
-  also have "\<dots> \<le> ?n * mahler_measure f + real ?n * \<bar>lead_coeff f\<bar>"
-  proof (rule add_mono[OF mult_mono[OF to_n] mult_mono[OF to_n]])
+    + (real (min k 1 * (degree g - 1 choose (k - 1))) * \<bar>lead_coeff g\<bar>)" 
+    using mignotte_coeff_helper[of g k] by simp
+  also have "\<dots> \<le> ?n * mahler_measure f + real ?n' * \<bar>lead_coeff f\<bar>"
+  proof (rule add_mono[OF mult_mono[OF to_n] mult_mono[OF to_n']])
     have "mahler_measure g  \<le> mahler_measure g * mahler_measure h" using g1 g0[of g]
       using mahler_measure_poly_ge_1 nz(1) by force
     thus "mahler_measure g \<le> mahler_measure f"
@@ -206,12 +201,67 @@ proof-
     finally have "\<bar>lead_coeff g\<bar> \<le> \<bar>lead_coeff f\<bar>" by simp
     thus "real_of_int \<bar>lead_coeff g\<bar> \<le> real_of_int \<bar>lead_coeff f\<bar>" by simp
   qed (auto simp: g0)
-  finally have "\<bar>coeff g k\<bar> \<le> ?n * mahler_measure f + real_of_int (?n * \<bar>lead_coeff f\<bar>)" by simp
+  finally have "\<bar>coeff g k\<bar> \<le> ?n * mahler_measure f + real_of_int (?n' * \<bar>lead_coeff f\<bar>)" by simp
   from floor_mono[OF this, folded floor_add_int]
-  have "\<bar>coeff g k\<bar> \<le> floor (?n * mahler_measure f) + ?n * \<bar>lead_coeff f\<bar>" by linarith
+  have "\<bar>coeff g k\<bar> \<le> floor (?n * mahler_measure f) + ?n' * \<bar>lead_coeff f\<bar>" by linarith
   thus ?thesis unfolding mignotte_bound_def Let_def using mahler_approximation[of ?n f ?bnd] by auto
 qed
 
+lemma Mignotte_bound: 
+  shows "of_int \<bar>coeff g k\<bar> \<le> (degree g choose k) * mahler_measure g"
+proof (cases "k \<le> degree g \<and> g \<noteq> 0")
+  case False
+  hence "coeff g k = 0" using le_degree by (cases "g = 0", auto)
+  thus ?thesis using mahler_measure_ge_0[of g] by auto
+next
+  case kg: True
+  hence g: "g \<noteq> 0" "g dvd g" by auto
+  from mignotte_bound_main[OF g le_refl, of k]
+  have "real_of_int \<bar>coeff g k\<bar>
+    \<le> of_int \<lfloor>real (degree g - 1 choose k) * mahler_measure g\<rfloor> +
+      of_int (int (min k 1 * (degree g - 1 choose (k - 1))) * \<bar>lead_coeff g\<bar>)" by linarith
+  also have "\<dots> \<le> real (degree g - 1 choose k) * mahler_measure g 
+     + real (min k 1 * (degree g - 1 choose (k - 1))) * (of_int \<bar>lead_coeff g\<bar> * 1)"
+    by (rule add_mono, force, auto)
+  also have "\<dots> \<le> real (degree g - 1 choose k) * mahler_measure g 
+     + real (min k 1 * (degree g - 1 choose (k - 1))) * mahler_measure g"
+    by (rule add_left_mono[OF mult_left_mono], 
+    unfold mahler_measure_def mahler_measure_poly_def,
+    rule mult_mono, auto intro!: prod_list_ge1)  
+  also have "\<dots> = 
+    (real ((degree g - 1 choose k) + (min k 1 * (degree g - 1 choose (k - 1))))) * mahler_measure g" 
+    by (auto simp: field_simps)
+  also have "(degree g - 1 choose k) + (min k 1 * (degree g - 1 choose (k - 1))) = degree g choose k"
+  proof (cases "k = 0")
+    case False
+    then obtain kk where k: "k = Suc kk" by (cases k, auto)
+    with kg obtain gg where g: "degree g = Suc gg" by (cases "degree g", auto)
+    show ?thesis unfolding k g by auto
+  qed auto
+  finally show ?thesis .
+qed
+
+lemma mignotte_bound:  
+  assumes "f \<noteq> 0" "g dvd f" "degree g \<le> n"
+  shows "\<bar>coeff g k\<bar> \<le> mignotte_bound f n"  
+proof -
+  let ?bnd = 2
+  let ?n = "(n - 1) choose ((n - 1) div 2)" 
+  have to_n: "(n - 1 choose k) \<le> real ?n" for k
+    using choose_approx[OF le_refl] by auto
+  from mignotte_bound_main[OF assms, of k]
+  have "\<bar>coeff g k\<bar> \<le> 
+    \<lfloor>real (n - 1 choose k) * mahler_measure f\<rfloor> + 
+    int (min k 1 * (n - 1 choose (k - 1))) * \<bar>lead_coeff f\<bar>" .
+  also have "\<dots> \<le> \<lfloor>real (n - 1 choose k) * mahler_measure f\<rfloor> + 
+    int ((n - 1 choose (k - 1))) * \<bar>lead_coeff f\<bar>"
+    by (rule add_left_mono[OF mult_right_mono], cases k, auto)
+  also have "\<dots> \<le> mignotte_bound f n" 
+    unfolding mignotte_bound_def Let_def
+    by (rule add_mono[OF order.trans[OF floor_mono[OF mult_right_mono] 
+    mahler_approximation[of ?n f ?bnd]] mult_right_mono], insert to_n mahler_measure_ge_0, auto)
+  finally show ?thesis .
+qed
 
 text \<open>As indicated before, at the moment the only available factor bound is Mignotte's one.
   As future work one might use a combined bound.\<close>
