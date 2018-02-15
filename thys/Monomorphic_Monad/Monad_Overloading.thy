35e7ad36896013d9a5e2e0f4a64d8b80fe8d9b0e
Andreas Lochbihler <unknown>
2018-02-15 14:23:36 +0100

    more monads, more examples

--- thys/Monomorphic_Monad/Monad_Overloading.thy
+++ thys/Monomorphic_Monad/Monad_Overloading.thy
@@ -13,6 +13,7 @@ consts sample :: "('p, 'm) sample"
 consts pause :: "('o, 'i, 'm) pause"
 consts tell :: "('w, 'm) tell"
 consts alt :: "'m alt"
+consts altc :: "('c, 'm) altc"
 
 subsection \<open>Identity monad\<close>
 
@@ -72,6 +73,10 @@ unfolding return_prob_def bind_prob_def sample_prob_def by(rule monad_prob_prob)
 
 subsection \<open>Nondeterminism monad transformer\<close>
 
+text \<open>As the collection type is not determined from the type of the return operation, we can
+  only provide definitions for one collection type implementation. We choose multisets.
+  Accordingly, @{const altc} is not available.\<close>
+
 consts
   munionMT :: "'a itself \<Rightarrow> 'm \<Rightarrow> 'm \<Rightarrow> 'm"
   mUnionMT :: "'a itself \<Rightarrow> 'm multiset \<Rightarrow> 'm"
@@ -88,14 +93,16 @@ overloading
   mUnionMT \<equiv> "mUnionMT :: 'a itself \<Rightarrow> 'm multiset \<Rightarrow> 'm" (unchecked)
 begin
 
+interpretation nondetM_base return bind "mmerge return bind" "{#}" "\<lambda>x. {#x#}" "(+)" .
+
 definition return_nondetT :: "('a, ('a, 'm) nondetT) return"
-where [code_unfold, monad_unfold]: "return_nondetT = return_nondet return"
+where [code_unfold, monad_unfold]: "return_nondetT = return_nondet"
 
 definition bind_nondetT :: "('a, ('a, 'm) nondetT) bind"
-where [code_unfold, monad_unfold]: "bind_nondetT = bind_nondet return bind" 
+where [code_unfold, monad_unfold]: "bind_nondetT = bind_nondet" 
 
 definition fail_nondetT :: "('a, 'm) nondetT fail"
-where [code_unfold, monad_unfold]: "fail_nondetT = fail_nondet return"
+where [code_unfold, monad_unfold]: "fail_nondetT = fail_nondet"
 
 definition ask_nondetT :: "('r, ('a, 'm) nondetT) ask"
 where [code_unfold, monad_unfold]: "ask_nondetT = ask_nondet ask"
@@ -107,20 +114,23 @@ definition put_nondetT :: "('s, ('a, 'm) nondetT) put"
 where [code_unfold, monad_unfold]: "put_nondetT = put_nondet put"
 
 definition alt_nondetT :: "('a, 'm) nondetT alt"
-where [code_unfold, monad_unfold]: "alt_nondetT = alt_nondet return bind"
+where [code_unfold, monad_unfold]: "alt_nondetT = alt_nondet"
 
 definition munionMT :: "'a itself \<Rightarrow> 'm \<Rightarrow> 'm \<Rightarrow> 'm"
-where "munionMT _ = munionM (return :: ('a multiset, 'm) return) bind"
+where "munionMT _ m1 m2 = bind m1 (\<lambda>A. bind m2 (\<lambda>B. return (A + B :: 'a multiset)))"
 
 definition mUnionMT :: "'a itself \<Rightarrow> 'm multiset \<Rightarrow> 'm"
 where "mUnionMT _ = fold_mset (munionMT TYPE('a)) (return ({#} :: 'a multiset))"
 
 end
 
+context begin
+interpretation nondetM_base return bind "mmerge return bind" "{#}" "\<lambda>x. {#x#}" "(+)" .
+
 lemma run_bind_nondetT:
   fixes f :: "'a \<Rightarrow> ('a, 'm) nondetT" shows
   "run_nondet (bind m f) = bind (run_nondet m) (\<lambda>A. mUnionMT TYPE('a) (image_mset (run_nondet \<circ> f) A))"
-by(simp add: bind_nondetT_def mUnionMT_def munionMT_def mUnionM_def)
+by(simp add: bind_nondetT_def mUnionMT_def munionMT_def[abs_def] mmerge_def)
 
 lemma run_return_nondetT [simp]: "run_nondet (return x :: ('a, 'm) nondetT) = return {#x#}" for x :: 'a
 by(simp add: return_nondetT_def)
@@ -142,31 +152,33 @@ lemma run_alt_nondetT [simp]:
    bind (run_nondet m) (\<lambda>A :: 'a multiset. bind (run_nondet m') (\<lambda>B. return (A + B)))"
 by(simp add: alt_nondetT_def)
 
+end
+
 lemma monad_nondetT' [locale_witness]: 
   "monad_commute return (bind :: ('a multiset, 'm) bind)
   \<Longrightarrow> monad return (bind :: ('a, ('a, 'm) nondetT) bind)"
-unfolding return_nondetT_def bind_nondetT_def by(rule monad_nondetT)
+unfolding return_nondetT_def bind_nondetT_def by(rule mset_nondetMs)
 
 lemma monad_fail_nondetT' [locale_witness]:
   "monad_commute return (bind :: ('a multiset, 'm) bind)
   \<Longrightarrow> monad_fail return (bind :: ('a, ('a, 'm) nondetT) bind) fail"
-unfolding return_nondetT_def bind_nondetT_def fail_nondetT_def by(rule monad_fail_nondetT)
+unfolding return_nondetT_def bind_nondetT_def fail_nondetT_def by(rule mset_nondetMs)
 
 lemma monad_alt_nondetT' [locale_witness]: 
   "monad_commute return (bind :: ('a multiset, 'm) bind)
   \<Longrightarrow> monad_alt return (bind :: ('a, ('a, 'm) nondetT) bind) alt"
-unfolding return_nondetT_def bind_nondetT_def alt_nondetT_def by(rule monad_alt_nondetT)
+unfolding return_nondetT_def bind_nondetT_def alt_nondetT_def by(rule mset_nondetMs)
 
 lemma monad_fail_alt_nondetT' [locale_witness]:
   "monad_commute return (bind :: ('a multiset, 'm) bind)
   \<Longrightarrow> monad_fail_alt return (bind :: ('a, ('a, 'm) nondetT) bind) fail alt"
-unfolding return_nondetT_def bind_nondetT_def fail_nondetT_def alt_nondetT_def by(rule monad_fail_alt_nondetT)
+unfolding return_nondetT_def bind_nondetT_def fail_nondetT_def alt_nondetT_def by(rule mset_nondetMs)
 
 lemma monad_reader_nondetT' [locale_witness]:
   "\<lbrakk> monad_commute return (bind :: ('a multiset, 'm) bind);
      monad_reader return (bind :: ('a multiset, 'm) bind) (ask :: ('r, 'm) ask) \<rbrakk>
   \<Longrightarrow> monad_reader return (bind :: ('a, ('a, 'm) nondetT) bind) (ask :: ('r, ('a, 'm) nondetT) ask)"
-unfolding return_nondetT_def bind_nondetT_def ask_nondetT_def by(rule monad_reader_nondetT)
+unfolding return_nondetT_def bind_nondetT_def ask_nondetT_def by(rule mset_nondetMs)
 
 subsection \<open>State monad transformer\<close>
 
@@ -180,6 +192,7 @@ overloading
   sample_stateT \<equiv> "sample :: ('p, ('s, 'm) stateT) sample"
   tell_stateT \<equiv> "tell :: ('w, ('s, 'm) stateT) tell"
   alt_stateT \<equiv> "alt :: ('s, 'm) stateT alt"
+  altc_stateT \<equiv> "altc :: ('c, ('s, 'm) stateT) altc"
   pause_stateT \<equiv> "pause :: ('o, 'i, ('s, 'm) stateT) pause"
 begin
 
@@ -209,6 +222,9 @@ where [code_unfold, monad_unfold]: "tell_stateT = tell_state tell"
 
 definition alt_stateT :: "('s, 'm) stateT alt"
 where [code_unfold, monad_unfold]: "alt_stateT = alt_state alt"
+
+definition altc_stateT :: "('c, ('s, 'm) stateT) altc"
+where [code_unfold, monad_unfold]: "altc_stateT = altc_state altc"
 
 definition pause_stateT :: "('o, 'i, ('s, 'm) stateT) pause"
 where [code_unfold, monad_unfold]: "pause_stateT = pause_state pause"
@@ -242,6 +258,9 @@ by(simp add: tell_stateT_def)
 
 lemma run_alt_stateT [simp]: "run_state (alt m m') s = alt (run_state m s) (run_state m' s)"
 by(simp add: alt_stateT_def)
+
+lemma run_altc_stateT [simp]: "run_state (altc C f) s = altc C (\<lambda>x. run_state (f x) s)"
+by(simp add: altc_stateT_def)
 
 lemma run_pause_stateT [simp]: "run_state (pause out c) s = pause out (\<lambda>input. run_state (c input) s)"
 by(simp add: pause_stateT_def)
@@ -290,10 +309,25 @@ lemma monad_alt_stateT' [locale_witness]:
    \<Longrightarrow> monad_alt return (bind :: ('a, ('s, 'm) stateT) bind) alt"
 unfolding return_stateT_def bind_stateT_def alt_stateT_def by(rule monad_alt_stateT)
 
+lemma monad_state_alt_stateT' [locale_witness]:
+  "monad_alt return (bind :: ('a \<times> 's, 'm) bind) alt
+   \<Longrightarrow> monad_state_alt return (bind :: ('a, ('s, 'm) stateT) bind) (get :: ('s, ('s, 'm) stateT) get) put alt"
+unfolding return_stateT_def bind_stateT_def get_stateT_def put_stateT_def alt_stateT_def by(rule monad_state_alt_stateT)
+
 lemma monad_fail_alt_stateT' [locale_witness]:
   "monad_fail_alt return (bind :: ('a \<times> 's, 'm) bind) fail alt
    \<Longrightarrow> monad_fail_alt return (bind :: ('a, ('s, 'm) stateT) bind) fail alt"
 unfolding return_stateT_def bind_stateT_def fail_stateT_def alt_stateT_def by(rule monad_fail_alt_stateT)
+
+lemma monad_altc_stateT' [locale_witness]:
+  "monad_altc return (bind :: ('a \<times> 's, 'm) bind) (altc :: ('c, 'm) altc)
+   \<Longrightarrow> monad_altc return (bind :: ('a, ('s, 'm) stateT) bind) (altc :: ('c, ('s, 'm) stateT) altc)"
+unfolding return_stateT_def bind_stateT_def altc_stateT_def by(rule monad_altc_stateT)
+
+lemma monad_state_altc_stateT' [locale_witness]:
+  "monad_altc return (bind :: ('a \<times> 's, 'm) bind) (altc :: ('c, 'm) altc)
+   \<Longrightarrow> monad_state_altc return (bind :: ('a, ('s, 'm) stateT) bind) (get :: ('s, ('s, 'm) stateT) get) put (altc :: ('c, ('s, 'm) stateT) altc)"
+unfolding return_stateT_def bind_stateT_def get_stateT_def put_stateT_def altc_stateT_def by(rule monad_state_altc_stateT)
 
 lemma monad_resumption_stateT' [locale_witness]:
   "monad_resumption return (bind :: ('a \<times> 's, 'm) bind) (pause :: ('o, 'i, 'm) pause)
@@ -313,6 +347,7 @@ overloading
   sample_optionT \<equiv> "sample :: ('p, ('a, 'm) optionT) sample"
   tell_optionT \<equiv> "tell :: ('w, ('a, 'm) optionT) tell"
   alt_optionT \<equiv> "alt :: ('a, 'm) optionT alt"
+  altc_optionT \<equiv> "altc :: ('c, ('a, 'm) optionT) altc"
   pause_optionT \<equiv> "pause :: ('o, 'i, ('a, 'm) optionT) pause"
 begin
 
@@ -345,6 +380,9 @@ where [code_unfold, monad_unfold]: "tell_optionT = tell_option tell"
 
 definition alt_optionT :: "('a, 'm) optionT alt"
 where [code_unfold, monad_unfold]: "alt_optionT = alt_option alt"
+
+definition altc_optionT :: "('c, ('a, 'm) optionT) altc"
+where [code_unfold, monad_unfold]: "altc_optionT = altc_option altc"
 
 definition pause_optionT :: "('o, 'i, ('a, 'm) optionT) pause"
 where [code_unfold, monad_unfold]: "pause_optionT = pause_option pause"
@@ -384,6 +422,9 @@ by(simp add: tell_optionT_def)
 
 lemma run_alt_optionT [simp]: "run_option (alt m m') = alt (run_option m) (run_option m')"
 by(simp add: alt_optionT_def)
+
+lemma run_altc_optionT [simp]: "run_option (altc C f) = altc C (run_option \<circ> f)"
+by(simp add: altc_optionT_def o_def)
 
 lemma run_pause_optionT [simp]: "run_option (pause out c) = pause out (\<lambda>input. run_option (c input))"
 by(simp add: pause_optionT_def)
@@ -443,6 +484,21 @@ lemma monad_alt_optionT' [locale_witness]:
   \<Longrightarrow> monad_alt return (bind :: ('a, ('a, 'm) optionT) bind) alt"
 unfolding return_optionT_def bind_optionT_def alt_optionT_def by(rule monad_alt_optionT)
 
+lemma monad_state_alt_optionT' [locale_witness]:
+  "monad_state_alt return (bind :: ('a option, 'm) bind) (get :: ('s, 'm) get) put alt
+  \<Longrightarrow> monad_state_alt return (bind :: ('a, ('a, 'm) optionT) bind) (get :: ('s, ('a, 'm) optionT) get) put alt"
+unfolding return_optionT_def bind_optionT_def alt_optionT_def get_optionT_def put_optionT_def by(rule monad_state_alt_optionT)
+
+lemma monad_altc_optionT' [locale_witness]:
+  "monad_altc return (bind :: ('a option, 'm) bind) (altc :: ('c, 'm) altc)
+  \<Longrightarrow> monad_altc return (bind :: ('a, ('a, 'm) optionT) bind) (altc :: ('c, ('a, 'm) optionT) altc)"
+unfolding return_optionT_def bind_optionT_def altc_optionT_def by(rule monad_altc_optionT)
+
+lemma monad_state_altc_optionT' [locale_witness]:
+  "monad_state_altc return (bind :: ('a option, 'm) bind) (get :: ('s, 'm) get) put (altc :: ('c, 'm) altc)
+  \<Longrightarrow> monad_state_altc return (bind :: ('a, ('a, 'm) optionT) bind) (get :: ('s, ('a, 'm) optionT) get) put (altc :: ('c, ('a, 'm) optionT) altc)"
+unfolding return_optionT_def bind_optionT_def altc_optionT_def get_optionT_def put_optionT_def by(rule monad_state_altc_optionT)
+
 lemma monad_resumption_optionT' [locale_witness]:
   "monad_resumption return (bind :: ('a option, 'm) bind) (pause :: ('o, 'i, 'm) pause)
   \<Longrightarrow> monad_resumption return (bind :: ('a, ('a, 'm) optionT) bind) (pause :: ('o, 'i, ('a, 'm) optionT) pause)"
@@ -466,6 +522,7 @@ overloading
   ask_envT \<equiv> "ask :: ('r, ('r, 'm) envT) ask"
   catch_envT \<equiv> "catch :: ('r, 'm) envT catch"
   alt_envT \<equiv> "alt :: ('r, 'm) envT alt"
+  altc_envT \<equiv> "altc :: ('c, ('r, 'm) envT) altc"
   pause_envT \<equiv> "pause :: ('o, 'i, ('r, 'm) envT) pause"
   tell_envT \<equiv> "tell :: ('w, ('r, 'm) envT) tell"
 begin
@@ -496,6 +553,9 @@ where [code_unfold, monad_unfold]: "catch_envT = catch_env catch"
 
 definition alt_envT :: "('r, 'm) envT alt"
 where [code_unfold, monad_unfold]: "alt_envT = alt_env alt"
+
+definition altc_envT :: "('c, ('r, 'm) envT) altc"
+where [code_unfold, monad_unfold]: "altc_envT = altc_env altc"
 
 definition pause_envT :: "('o, 'i, ('r, 'm) envT) pause"
 where [code_unfold, monad_unfold]: "pause_envT = pause_env pause"
@@ -531,6 +591,9 @@ by(simp add: catch_envT_def)
 
 lemma run_alt_envT [simp]: "run_env (alt m m') r = alt (run_env m r) (run_env m' r)"
 by(simp add: alt_envT_def)
+
+lemma run_altc_envT [simp]: "run_env (altc C f) r = altc C (\<lambda>x. run_env (f x) r)"
+by(simp add: altc_envT_def)
 
 lemma run_pause_envT [simp]: "run_env (pause out c) r = pause out (\<lambda>input. run_env (c input) r)"
 by(simp add: pause_envT_def)
@@ -583,6 +646,21 @@ lemma monad_fail_alt_envT' [locale_witness]:
   \<Longrightarrow> monad_fail_alt return (bind :: ('a, ('r, 'm) envT) bind) fail alt"
 unfolding return_envT_def bind_envT_def fail_envT_def alt_envT_def by(rule monad_fail_alt_envT)
 
+lemma monad_state_alt_envT' [locale_witness]:
+  "monad_state_alt return (bind :: ('a, 'm) bind) (get :: ('s, 'm) get) put alt
+  \<Longrightarrow> monad_state_alt return (bind :: ('a, ('r, 'm) envT) bind) (get :: ('s, ('r, 'm) envT) get) put alt"
+unfolding return_envT_def bind_envT_def fail_envT_def get_envT_def put_envT_def alt_envT_def by(rule monad_state_alt_envT)
+
+lemma monad_altc_envT' [locale_witness]:
+  "monad_altc return (bind :: ('a, 'm) bind) (altc :: ('c, 'm) altc)
+  \<Longrightarrow> monad_altc return (bind :: ('a, ('r, 'm) envT) bind) (altc :: ('c, ('r, 'm) envT) altc)"
+unfolding return_envT_def bind_envT_def altc_envT_def by(rule monad_altc_envT)
+
+lemma monad_state_altc_envT' [locale_witness]:
+  "monad_state_altc return (bind :: ('a, 'm) bind) (get :: ('s, 'm) get) put (altc :: ('c, 'm) altc)
+  \<Longrightarrow> monad_state_altc return (bind :: ('a, ('r, 'm) envT) bind) (get :: ('s, ('r, 'm) envT) get) put (altc :: ('c, ('r, 'm) envT) altc)"
+unfolding return_envT_def bind_envT_def fail_envT_def get_envT_def put_envT_def altc_envT_def by(rule monad_state_altc_envT)
+
 lemma monad_resumption_envT' [locale_witness]:
   "monad_resumption return (bind :: ('a, 'm) bind) (pause :: ('o, 'i, 'm) pause)
   \<Longrightarrow> monad_resumption return (bind :: ('a, ('r, 'm) envT) bind) (pause :: ('o, 'i, ('r, 'm) envT) pause)"
@@ -614,6 +692,7 @@ overloading
   sample_writerT \<equiv> "sample :: ('p, ('w, 'a, 'm) writerT) sample"
   ask_writerT \<equiv> "ask :: ('r, ('w, 'a, 'm) writerT) ask"
   alt_writerT \<equiv> "alt :: ('w, 'a, 'm) writerT alt"
+  altc_writerT \<equiv> "altc :: ('c, ('w, 'a, 'm) writerT) altc"
   pause_writerT \<equiv> "pause :: ('o, 'i, ('w, 'a, 'm) writerT) pause"
   tell_writerT \<equiv> "tell :: ('w, ('w, 'a, 'm) writerT) tell" (unchecked)
 begin
@@ -641,6 +720,9 @@ where [code_unfold, monad_unfold]: "sample_writerT = sample_writer sample"
 
 definition alt_writerT :: "('w, 'a, 'm) writerT alt"
 where [code_unfold, monad_unfold]: "alt_writerT = alt_writer alt"
+
+definition altc_writerT :: "('c, ('w, 'a, 'm) writerT) altc"
+where [code_unfold, monad_unfold]: "altc_writerT = altc_writer altc"
 
 definition pause_writerT :: "('o, 'i, ('w, 'a, 'm) writerT) pause"
 where [code_unfold, monad_unfold]: "pause_writerT = pause_writer pause"
@@ -674,6 +756,9 @@ by(simp add: sample_writerT_def)
 
 lemma run_alt_writerT [simp]: "run_writer (alt m m') = alt (run_writer m) (run_writer m')"
 by(simp add: alt_writerT_def)
+
+lemma run_altc_writerT [simp]: "run_writer (altc C f) = altc C (run_writer \<circ> f)"
+by(simp add: altc_writerT_def o_def)
 
 lemma run_pause_writerT [simp]: "run_writer (pause out c) = pause out (\<lambda>input. run_writer (c input))"
 by(simp add: pause_writerT_def)
@@ -737,6 +822,21 @@ lemma monad_fail_alt_writerT' [locale_witness]:
   "monad_fail_alt return (bind :: ('a \<times> 'w list, 'm) bind) fail alt
   \<Longrightarrow> monad_fail_alt return (bind :: ('a, ('w, 'a, 'm) writerT) bind) fail alt"
 unfolding return_writerT_def bind_writerT_def fail_writerT_def alt_writerT_def by(rule monad_fail_alt_writerT)
+
+lemma monad_state_alt_writerT' [locale_witness]:
+  "monad_state_alt return (bind :: ('a \<times> 'w list, 'm) bind) (get :: ('s, 'm) get) put alt
+  \<Longrightarrow> monad_state_alt return (bind :: ('a, ('w, 'a, 'm) writerT) bind) (get :: ('s, ('w, 'a, 'm) writerT) get) put alt"
+unfolding return_writerT_def bind_writerT_def get_writerT_def put_writerT_def alt_writerT_def by(rule monad_state_alt_writerT)
+
+lemma monad_altc_writerT' [locale_witness]:
+  "monad_altc return (bind :: ('a \<times> 'w list, 'm) bind) (altc :: ('c, 'm) altc)
+  \<Longrightarrow> monad_altc return (bind :: ('a, ('w, 'a, 'm) writerT) bind) (altc :: ('c, ('w, 'a, 'm) writerT) altc)"
+unfolding return_writerT_def bind_writerT_def altc_writerT_def by(rule monad_altc_writerT)
+
+lemma monad_state_altc_writerT' [locale_witness]:
+  "monad_state_altc return (bind :: ('a \<times> 'w list, 'm) bind) (get :: ('s, 'm) get) put (altc :: ('c, 'm) altc)
+  \<Longrightarrow> monad_state_altc return (bind :: ('a, ('w, 'a, 'm) writerT) bind) (get :: ('s, ('w, 'a, 'm) writerT) get) put (altc :: ('c, ('w, 'a, 'm) writerT) altc)"
+unfolding return_writerT_def bind_writerT_def get_writerT_def put_writerT_def altc_writerT_def by(rule monad_state_altc_writerT)
 
 subsection \<open>Continuation monad transformer\<close>
 
