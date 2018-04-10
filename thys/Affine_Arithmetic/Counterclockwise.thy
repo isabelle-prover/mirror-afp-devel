c8b8f6931efc8386248f4abde70d91b16b878bde
immler <unknown>
2018-04-10 18:00:42 +0200

    renamed in order to avoid clash with MIN syntax

--- thys/Affine_Arithmetic/Counterclockwise.thy
+++ thys/Affine_Arithmetic/Counterclockwise.thy
@@ -43,28 +43,22 @@ lemma fold_if_in_set:
 
 subsection \<open>Sort Elements of a List\<close>
 
-no_syntax
-  "_MIN1"     :: "pttrns \<Rightarrow> 'b \<Rightarrow> 'b"           ("(3MIN _./ _)" [0, 10] 10)
-  "_MIN"      :: "pttrn \<Rightarrow> 'a set \<Rightarrow> 'b \<Rightarrow> 'b"  ("(3MIN _:_./ _)" [0, 0, 10] 10)
-  "_MIN1"     :: "pttrns \<Rightarrow> 'b \<Rightarrow> 'b"           ("(3MIN _./ _)" [0, 10] 10)
-  "_MIN"      :: "pttrn \<Rightarrow> 'a set \<Rightarrow> 'b \<Rightarrow> 'b"  ("(3MIN _\<in>_./ _)" [0, 0, 10] 10)
-
-locale linorder_list0 = fixes LE::"'a \<Rightarrow> 'a \<Rightarrow> bool"
+locale linorder_list0 = fixes le::"'a \<Rightarrow> 'a \<Rightarrow> bool"
 begin
 
-definition "MIN a b = (if LE a b then a else b)"
+definition "min_for a b = (if le a b then a else b)"
 
-lemma MIN_in[simp]: "x \<in> S \<Longrightarrow> y \<in> S \<Longrightarrow> MIN x y \<in> S"
-  by (auto simp: MIN_def)
+lemma min_for_in[simp]: "x \<in> S \<Longrightarrow> y \<in> S \<Longrightarrow> min_for x y \<in> S"
+  by (auto simp: min_for_def)
 
-lemma fold_min_eqI1: "fold MIN ys y \<notin> set ys \<Longrightarrow> fold MIN ys y = y"
+lemma fold_min_eqI1: "fold min_for ys y \<notin> set ys \<Longrightarrow> fold min_for ys y = y"
   using fold_if_in_set[of _ ys y]
-  by (auto simp: MIN_def[abs_def])
+  by (auto simp: min_for_def[abs_def])
 
 function selsort where
   "selsort [] = []"
 | "selsort (y#ys) = (let
-      xm = fold MIN ys y;
+      xm = fold min_for ys y;
       xs' = List.remove1 xm (y#ys)
     in (xm#selsort xs'))"
   by pat_completeness auto
@@ -99,7 +93,7 @@ lemma selsort_eq_empty_iff[simp]: "selsort xs = [] \<longleftrightarrow> xs = []
 
 inductive sortedP :: "'a list \<Rightarrow> bool" where
   Nil: "sortedP []"
-| Cons: "\<forall>y\<in>set ys. LE x y \<Longrightarrow> sortedP ys \<Longrightarrow> sortedP (x # ys)"
+| Cons: "\<forall>y\<in>set ys. le x y \<Longrightarrow> sortedP ys \<Longrightarrow> sortedP (x # ys)"
 
 inductive_cases
   sortedP_Nil: "sortedP []" and
@@ -109,14 +103,14 @@ inductive_simps
   sortedP_Cons_iff: "sortedP (Cons x xs)"
 
 lemma sortedP_append_iff:
-  "sortedP (xs @ ys) = (sortedP xs & sortedP ys & (\<forall>x \<in> set xs. \<forall>y \<in> set ys. LE x y))"
+  "sortedP (xs @ ys) = (sortedP xs & sortedP ys & (\<forall>x \<in> set xs. \<forall>y \<in> set ys. le x y))"
   by (induct xs) (auto intro!: Nil Cons elim!: sortedP_Cons)
 
 lemma sortedP_appendI:
-  "sortedP xs \<Longrightarrow> sortedP ys \<Longrightarrow> (\<And>x y. x \<in> set xs \<Longrightarrow> y \<in> set ys \<Longrightarrow> LE x y) \<Longrightarrow> sortedP (xs @ ys)"
+  "sortedP xs \<Longrightarrow> sortedP ys \<Longrightarrow> (\<And>x y. x \<in> set xs \<Longrightarrow> y \<in> set ys \<Longrightarrow> le x y) \<Longrightarrow> sortedP (xs @ ys)"
   by (induct xs) (auto intro!: Nil Cons elim!: sortedP_Cons)
 
-lemma sorted_nth_less: "sortedP xs \<Longrightarrow> i < j \<Longrightarrow> j < length xs \<Longrightarrow> LE (xs ! i) (xs ! j)"
+lemma sorted_nth_less: "sortedP xs \<Longrightarrow> i < j \<Longrightarrow> j < length xs \<Longrightarrow> le (xs ! i) (xs ! j)"
   by (induct xs arbitrary: i j) (auto simp: nth_Cons split: nat.split elim!: sortedP_Cons)
 
 lemma sorted_butlastI[intro, simp]: "sortedP xs \<Longrightarrow> sortedP (butlast xs)"
@@ -125,14 +119,14 @@ lemma sorted_butlastI[intro, simp]: "sortedP xs \<Longrightarrow> sortedP (butla
 lemma sortedP_right_of_append1:
   assumes "sortedP (zs@[z])"
   assumes "y \<in> set zs"
-  shows "LE y z"
+  shows "le y z"
   using assms
   by (induct zs arbitrary: y z) (auto elim!: sortedP_Cons)
 
 lemma sortedP_right_of_last:
   assumes "sortedP zs"
   assumes "y \<in> set zs" "y \<noteq> last zs"
-  shows "LE y (last zs)"
+  shows "le y (last zs)"
   using assms
   apply (intro sortedP_right_of_append1[of "butlast zs" "last zs" y])
   subgoal by (metis append_is_Nil_conv list.distinct(1) snoc_eq_iff_butlast split_list)
@@ -145,7 +139,7 @@ lemma selsort_singleton_iff: "selsort xs = [x] \<longleftrightarrow> xs = [x]"
 
 lemma hd_last_sorted:
   assumes "sortedP xs" "length xs > 1"
-  shows "LE (hd xs) (last xs)"
+  shows "le (hd xs) (last xs)"
 proof (cases xs)
   case (Cons y ys)
   note ys = this
@@ -158,7 +152,7 @@ end
 
 lemma (in comm_monoid_add) sum_list_distinct_selsort:
   assumes "distinct xs"
-  shows "sum_list (linorder_list0.selsort LE xs) = sum_list xs"
+  shows "sum_list (linorder_list0.selsort le xs) = sum_list xs"
   using assms
   apply (simp add: distinct_sum_list_conv_Sum linorder_list0.distinct_selsort)
   apply (rule sum.cong)
@@ -169,33 +163,33 @@ lemma (in comm_monoid_add) sum_list_distinct_selsort:
 declare linorder_list0.sortedP_Nil_iff[code]
   linorder_list0.sortedP_Cons_iff[code]
   linorder_list0.selsort.simps[code]
-  linorder_list0.MIN_def[code]
+  linorder_list0.min_for_def[code]
 
-locale linorder_list = linorder_list0 LE for LE::"'a::ab_group_add \<Rightarrow> _" +
+locale linorder_list = linorder_list0 le for le::"'a::ab_group_add \<Rightarrow> _" +
   fixes S
-  assumes order_refl: "a \<in> S \<Longrightarrow> LE a a"
+  assumes order_refl: "a \<in> S \<Longrightarrow> le a a"
   assumes trans': "a \<in> S \<Longrightarrow> b \<in> S \<Longrightarrow> c \<in> S \<Longrightarrow> a \<noteq> b \<Longrightarrow> b \<noteq> c \<Longrightarrow> a \<noteq> c \<Longrightarrow>
-    LE a b \<Longrightarrow> LE b c \<Longrightarrow> LE a c"
-  assumes antisym: "a \<in> S \<Longrightarrow> b \<in> S \<Longrightarrow> LE a b \<Longrightarrow> LE b a \<Longrightarrow> a = b"
-  assumes linear': "a \<in> S \<Longrightarrow> b \<in> S \<Longrightarrow> a \<noteq> b \<Longrightarrow> LE a b \<or> LE b a"
+    le a b \<Longrightarrow> le b c \<Longrightarrow> le a c"
+  assumes antisym: "a \<in> S \<Longrightarrow> b \<in> S \<Longrightarrow> le a b \<Longrightarrow> le b a \<Longrightarrow> a = b"
+  assumes linear': "a \<in> S \<Longrightarrow> b \<in> S \<Longrightarrow> a \<noteq> b \<Longrightarrow> le a b \<or> le b a"
 begin
 
-lemma trans: "a \<in> S \<Longrightarrow> b \<in> S \<Longrightarrow> c \<in> S \<Longrightarrow> LE a b \<Longrightarrow> LE b c \<Longrightarrow> LE a c"
+lemma trans: "a \<in> S \<Longrightarrow> b \<in> S \<Longrightarrow> c \<in> S \<Longrightarrow> le a b \<Longrightarrow> le b c \<Longrightarrow> le a c"
   by (cases "a = b" "b = c" "a = c"
     rule: bool.exhaust[case_product bool.exhaust[case_product bool.exhaust]])
     (auto simp: order_refl intro: trans')
 
-lemma linear: "a \<in> S \<Longrightarrow> b \<in> S \<Longrightarrow> LE a b \<or> LE b a"
+lemma linear: "a \<in> S \<Longrightarrow> b \<in> S \<Longrightarrow> le a b \<or> le b a"
   by (cases "a = b") (auto simp: linear' order_refl)
 
-lemma MIN_le1: "w \<in> S \<Longrightarrow> y \<in> S \<Longrightarrow> LE (MIN w y) y"
-  and MIN_le2: "w \<in> S \<Longrightarrow> y \<in> S \<Longrightarrow> LE (MIN w y) w"
+lemma min_le1: "w \<in> S \<Longrightarrow> y \<in> S \<Longrightarrow> le (min_for w y) y"
+  and min_le2: "w \<in> S \<Longrightarrow> y \<in> S \<Longrightarrow> le (min_for w y) w"
   using linear
-  by (auto simp: MIN_def refl)
+  by (auto simp: min_for_def refl)
 
 lemma fold_min:
   assumes "set xs \<subseteq> S"
-  shows "list_all (\<lambda>y. LE (fold MIN (tl xs) (hd xs)) y) xs"
+  shows "list_all (\<lambda>y. le (fold min_for (tl xs) (hd xs)) y) xs"
 proof (cases xs)
   case (Cons y ys)
   hence subset: "set (y#ys) \<subseteq> S" using assms
@@ -205,24 +199,24 @@ proof (cases xs)
     using subset
   proof (induct ys arbitrary: y)
     case (Cons z zs)
-    hence IH: "\<And>y. y \<in> S \<Longrightarrow> list_all (LE (fold MIN zs y)) (y # zs)"
+    hence IH: "\<And>y. y \<in> S \<Longrightarrow> list_all (le (fold min_for zs y)) (y # zs)"
       by simp
-    let ?f = "fold MIN zs (MIN z y)"
-    have "?f \<in> set ((MIN z y)#zs)"
-      unfolding MIN_def[abs_def]
+    let ?f = "fold min_for zs (min_for z y)"
+    have "?f \<in> set ((min_for z y)#zs)"
+      unfolding min_for_def[abs_def]
       by (rule fold_if_in_set)
     also have "\<dots> \<subseteq> S" using Cons.prems by auto
     finally have "?f \<in> S" .
 
-    have "LE ?f (MIN z y)"
-      using IH[of "MIN z y"] Cons.prems
+    have "le ?f (min_for z y)"
+      using IH[of "min_for z y"] Cons.prems
       by auto
-    moreover have "LE (MIN z y) y" "LE (MIN z y) z" using Cons.prems
-      by (auto intro!: MIN_le1 MIN_le2)
-    ultimately have "LE ?f y" "LE ?f z" using Cons.prems \<open>?f \<in> S\<close>
-      by (auto intro!: trans[of ?f "MIN z y"])
+    moreover have "le (min_for z y) y" "le (min_for z y) z" using Cons.prems
+      by (auto intro!: min_le1 min_le2)
+    ultimately have "le ?f y" "le ?f z" using Cons.prems \<open>?f \<in> S\<close>
+      by (auto intro!: trans[of ?f "min_for z y"])
     thus ?case
-      using IH[of "MIN z y"]
+      using IH[of "min_for z y"]
       using Cons.prems
       by auto
   qed (simp add: order_refl)
@@ -237,8 +231,7 @@ proof (induction xs rule: selsort.induct)
   case (2 z zs)
   from this fold_min[of "z#zs"]
   show ?case
-    by (cases "fold MIN zs z = z")
-      (fastforce simp: list_all_iff Let_def
+    by (fastforce simp: list_all_iff Let_def
         simp del: remove1.simps
         intro: Cons intro!: 2(1)[OF refl refl]
         dest!: set_rev_mp[OF _ set_remove1_subset])+
