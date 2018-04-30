0909104ba7f5d15f13c57a87f585e0b5b8df7939
conrad-watt <cnrdwtt@gmail.com>
2018-04-30 18:48:05 +0100

    WebAssembly: fix OCaml code generation

--- thys/WebAssembly/Wasm_Printing/Wasm_Printing.thy
+++ thys/WebAssembly/Wasm_Printing/Wasm_Printing.thy
@@ -1,8 +1,5 @@
 theory Wasm_Printing imports Wasm_Checker_Printing Wasm_Interpreter_Printing Wasm_Type_Abs_Printing "HOL-Library.Code_Target_Nat" begin
 
-(* FIXME F32Wrapper missing
-export_code open typing run (*in OCaml*)
-  checking OCaml?
-  *)
+export_code open typing run in OCaml
 
 end
