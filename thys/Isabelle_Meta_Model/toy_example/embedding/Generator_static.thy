5236c70f892fe3edd27a21fac9301a5b5a9bfbc3
Frédéric Tuong <unknown>
2018-07-23 09:47:00 -0400

    restore the optimization on the ML reflected code size introduced before a2daf48983d1 , synchronize with citadelle-devel/2dddcf8d0c6bf8d9b8839c923c795bf420bffd3c

--- thys/Isabelle_Meta_Model/toy_example/embedding/Generator_static.thy
+++ thys/Isabelle_Meta_Model/toy_example/embedding/Generator_static.thy
@@ -48,6 +48,8 @@ imports Printer
 begin
 (*<*)
 ML_file "~~/src/Doc/antiquote_setup.ML"
+
+declare[[cartouche_type' = "abr_string"]]
 (*>*)
 
 text \<open>In the ``static'' solution: the user manually generates
@@ -60,9 +62,9 @@ subsection\<open>Giving an Input to Translate\<close>
 definition "Design =
  (let n = \<lambda>n1 n2. ToyTyObj (ToyTyCore_pre n1) (case n2 of None \<Rightarrow> [] | Some n2 \<Rightarrow> [[ToyTyCore_pre n2]])
     ; mk = \<lambda>n l. toy_class_raw.make n l [] False in
-  [ mk (n \<langle>''Galaxy''\<rangle> None) [(\<langle>''sound''\<rangle>, ToyTy_raw \<langle>''unit''\<rangle>), (\<langle>''moving''\<rangle>, ToyTy_raw \<langle>''bool''\<rangle>)]
-  , mk (n \<langle>''Planet''\<rangle> (Some \<langle>''Galaxy''\<rangle>)) [(\<langle>''weight''\<rangle>, ToyTy_raw \<langle>''nat''\<rangle>)]
-  , mk (n \<langle>''Person''\<rangle> (Some \<langle>''Planet''\<rangle>)) [(\<langle>''salary''\<rangle>, ToyTy_raw \<langle>''int''\<rangle>)] ])"
+  [ mk (n \<open>Galaxy\<close> None) [(\<open>sound\<close>, ToyTy_raw \<open>unit\<close>), (\<open>moving\<close>, ToyTy_raw \<open>bool\<close>)]
+  , mk (n \<open>Planet\<close> (Some \<open>Galaxy\<close>)) [(\<open>weight\<close>, ToyTy_raw \<open>nat\<close>)]
+  , mk (n \<open>Person\<close> (Some \<open>Planet\<close>)) [(\<open>salary\<close>, ToyTy_raw \<open>int\<close>)] ])"
 
 text \<open>Since we are in a Isabelle session, at this time, it becomes possible to inspect with 
 the command @{command value} the result of the translations applied with @{term Design}. 
@@ -77,14 +79,14 @@ definition "main =
    (compiler_env_config.extend
      (compiler_env_config_empty True None (oidInit (Oid 0)) Gen_only_design (None, False)
         \<lparr> D_output_disable_thy := False
-        , D_output_header_thy := Some (\<langle>''Design_generated''\<rangle>
-                                      ,[\<langle>''../Toy_Library''\<rangle>]
-                                      ,\<langle>''../embedding/Generator_dynamic''\<rangle>) \<rparr>)
+        , D_output_header_thy := Some (\<open>Design_generated\<close>
+                                      ,[\<open>../Toy_Library\<close>]
+                                      ,\<open>../embedding/Generator_dynamic\<close>) \<rparr>)
      ( L.map (META_class_raw Floor1) Design
        @@@@ [ META_association (toy_association.make
                                   ToyAssTy_association
-                                  (ToyAssRel [ (n \<langle>''Person''\<rangle>, ToyMult (Mult_star, None) None)
-                                             , (n \<langle>''Person''\<rangle>, ToyMult (Mult_nat 0, Some (Mult_nat 1)) (Some \<langle>''boss''\<rangle>))]))
+                                  (ToyAssRel [ (n \<open>Person\<close>, ToyMult (Mult_star, None) None)
+                                             , (n \<open>Person\<close>, ToyMult (Mult_nat 0, Some (Mult_nat 1)) (Some \<open>boss\<close>))]))
           , META_flush_all ToyFlushAll]
      , None)))"
 
