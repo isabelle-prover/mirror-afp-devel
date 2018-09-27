1194e113b11e157f4f0c8d914c4fc0c57919ae3c
Lars Hupel <lars.hupel@mytum.de>
2018-09-27 10:51:48 +0200

    CakeML: tuned tests

--- thys/CakeML/CakeML_Compiler.thy
+++ thys/CakeML/CakeML_Compiler.thy
@@ -71,22 +71,4 @@ section \<open>Setup\<close>
 
 ML_file "compiler.ML"
 
-section \<open>Tests\<close>
-
-consts default_loc :: locs \<comment> \<open>no code equations needed, locs are never printed\<close>
-
-definition simple_print :: Ast.prog where
-"simple_print =
-  [Ast.Tdec (Ast.Dlet default_loc Ast.Pany (Ast.App Ast.Opapp [Ast.Var (Short ''print''), Ast.Lit (Ast.StrLit ''hi'')]))]"
-
-cakeml (literal) \<open>print "hi1";\<close>
-cakeml (literal) \<open>print "hi2";\<close>
-
-ML\<open>
-  val out = CakeML_Compiler.eval_source \<open>val x = 3 + 4; print (Int.toString x);\<close>;
-  @{assert} (out = "7")
-\<close>
-
-cakeml (prog) \<open>simple_print\<close>
-
 end
