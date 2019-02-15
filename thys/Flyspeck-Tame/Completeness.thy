ba271390e99ef263108a29a906fdd862d8d47340
haftmann <unknown>
2019-02-15 07:11:13 +0000

    separate session for Flyspeck computations

--- thys/Flyspeck-Tame/Completeness.thy
+++ thys/Flyspeck-Tame/Completeness.thy
@@ -1,64 +1,30 @@
 (*  Author: Tobias Nipkow  *)
 
-section \<open>Combining All Completeness Proofs\<close>
+section \<open>Combining Computations With All Completeness Proofs\<close>
 
 theory Completeness
-imports ArchCompProps ArchComp
+imports ArchComp "Flyspeck-Tame.Relative_Completeness"
 begin
 
-definition Archive :: "vertex fgraph set" where
-"Archive \<equiv> set(Tri @ Quad @ Pent @ Hex)"
+global_interpretation relative?: archive_by_computation
+by standard (fact pre_iso_test3 pre_iso_test4 pre_iso_test5 pre_iso_test6
+  same3 same4 same5 same6)+
 
 theorem TameEnum_Archive:  "fgraph ` TameEnum \<subseteq>\<^sub>\<simeq> Archive"
-using combine_evals_filter[OF pre_iso_test3 same3]
-      combine_evals_filter[OF pre_iso_test4 same4]
-      combine_evals_filter[OF pre_iso_test5 same5]
-      combine_evals_filter[OF pre_iso_test6 same6]
-by(fastforce simp:TameEnum_def Archive_def image_def qle_gr.defs
-       eval_nat_numeral le_Suc_eq)
-
+by (fact TameEnum_Archive)
 
 lemma TameEnum_comp:
 assumes "Seed\<^bsub>p\<^esub> [next_plane\<^bsub>p\<^esub>]\<rightarrow>* g" and "final g" and "tame g"
 shows "Seed\<^bsub>p\<^esub> [next_tame\<^bsub>p\<^esub>]\<rightarrow>* g"
-proof -
-  from assms have "Seed\<^bsub>p\<^esub> [next_tame0 p]\<rightarrow>* g" by(rule next_tame0_comp)
-  with assms show "Seed\<^bsub>p\<^esub> [next_tame\<^bsub>p\<^esub>]\<rightarrow>* g" by(blast intro: next_tame_comp)
-qed
+using assms by (fact TameEnum_comp)
 
-(* final not necessary but slightly simpler because of lemmas *)
 lemma tame5:
 assumes g: "Seed\<^bsub>p\<^esub> [next_plane0\<^bsub>p\<^esub>]\<rightarrow>* g" and "final g" and "tame g"
 shows "p \<le> 3"
-proof -
-  from \<open>tame g\<close> have bound: "\<forall>f \<in> \<F> g. |vertices f| \<le> 6"
-    by (simp add: tame_def tame9a_def)
-  show "p \<le> 3"
-  proof (rule ccontr)
-    assume 6: "\<not> p \<le> 3"
-    obtain f where "f \<in> set (finals g) \<and> |vertices f| = p+3"
-      using max_face_ex[OF g] by auto
-    also from \<open>final g\<close> have "set (finals g) = set (faces g)" by simp
-    finally have "f \<in> \<F> g \<and> 6 < |vertices f|" using 6 by arith
-    with bound show False by auto
-  qed
-qed
-
+using assms by (fact tame5)
 
 theorem completeness:
 assumes "g \<in> PlaneGraphs" and "tame g" shows "fgraph g \<in>\<^sub>\<simeq> Archive"
-proof -
-  from \<open>g \<in> PlaneGraphs\<close> obtain p where g1: "Seed\<^bsub>p\<^esub> [next_plane\<^bsub>p\<^esub>]\<rightarrow>* g"
-    and "final g"
-    by(auto simp:PlaneGraphs_def PlaneGraphsP_def)
-  have "Seed\<^bsub>p\<^esub> [next_plane0\<^bsub>p\<^esub>]\<rightarrow>* g"
-    by(rule RTranCl_subset2[OF g1])
-      (blast intro:inv_mgp inv_Seed mgp_next_plane0_if_next_plane
-        dest:RTranCl_inv[OF inv_inv_next_plane])
-  with \<open>tame g\<close> \<open>final g\<close> have "p \<le> 3" by(blast intro:tame5)
-  with g1 \<open>tame g\<close> \<open>final g\<close> show ?thesis using TameEnum_Archive
-    by(simp add: qle_gr.defs TameEnum_def TameEnumP_def)
-      (blast intro: TameEnum_comp)
-qed
+using assms by (fact completeness)
 
 end
