6ea634d14041e677d98bfba4024b72a783f8b148
wenzelm <unknown>
2019-03-28 21:34:39 +0100

    adapted to Isabelle/435fb018e8ee: still uses legacy physical file-system (!);

--- thys/Isabelle_Meta_Model/isabelle_home/src/Tools/Code/Isabelle_code_target.thy
+++ thys/Isabelle_Meta_Model/isabelle_home/src/Tools/Code/Isabelle_code_target.thy
@@ -91,8 +91,7 @@ val parse_inst_ident = Parse.name --| @{keyword "::"} -- Parse.class;
 
 (* code generation *)
 
-fun prep_destination "" = NONE
-  | prep_destination s = SOME (Path.explode s);
+fun prep_destination s = ({physical = true}, Path.explode s);
 
 fun export_code_cmd all_public raw_cs seris thy =
   let
@@ -100,7 +99,8 @@ fun export_code_cmd all_public raw_cs seris thy =
     val cs = Code_Thingol.read_const_exprs ctxt raw_cs;
   in
     thy |> Named_Target.theory_map
-      (Code_Target.export_code all_public cs ((map o apfst o apsnd o Option.map) prep_destination seris))
+      (Code_Target.export_code all_public cs
+        ((map o apfst o apsnd o Option.map) prep_destination seris))
   end;
 
 
