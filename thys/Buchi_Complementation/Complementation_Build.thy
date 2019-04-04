9e32a3ae2d056aeaba052147bdb5d7817e790b1c
wenzelm <unknown>
2019-04-04 13:00:28 +0200

    prefer strict access to settings; tuned signature (see Isabelle/96fe857a7a6f);

--- thys/Buchi_Complementation/Complementation_Build.thy
+++ thys/Buchi_Complementation/Complementation_Build.thy
@@ -10,50 +10,49 @@ external_file \<open>code/Automaton.sml\<close>
 external_file \<open>code/Complementation.sml\<close>
 
 ML_command \<^marker>\<open>contributor Makarius\<close> \<open>
-  if getenv "ISABELLE_MLTON" = "" then warning "MLton not configured"
-  else
-    Isabelle_System.with_tmp_dir "Complementation" (fn build_dir =>
-      let
-        val thy = \<^theory>;
-        val exe = Path.append build_dir (Path.exe \<^path>\<open>Complementation\<close>);
+  Isabelle_System.with_tmp_dir "Complementation" (fn build_dir =>
+    let
+      val thy = \<^theory>;
+      val exe = Path.append build_dir (Path.exe \<^path>\<open>Complementation\<close>);
 
-        (*assemble sources*)
-        val _ =
-          List.app (fn path => Isabelle_System.copy_file (Path.append \<^master_dir> path) build_dir)
-           [\<^path>\<open>code/Complementation.mlb\<close>,
-            \<^path>\<open>code/Prelude.sml\<close>,
-            \<^path>\<open>code/Automaton.sml\<close>,
-            \<^path>\<open>code/Complementation.sml\<close>];
+      (*assemble sources*)
+      val _ =
+        List.app (fn path => Isabelle_System.copy_file (Path.append \<^master_dir> path) build_dir)
+         [\<^path>\<open>code/Complementation.mlb\<close>,
+          \<^path>\<open>code/Prelude.sml\<close>,
+          \<^path>\<open>code/Automaton.sml\<close>,
+          \<^path>\<open>code/Complementation.sml\<close>];
 
-        val exported_code =
-          Generated_Files.the_file_content \<^theory>\<open>Complementation_Final\<close>
-            \<^path>\<open>code/Complementation_Export.ML\<close>;
-        val _ = File.write (Path.append build_dir \<^path>\<open>Complementation_Export.sml\<close>) exported_code;
-        val _ = Export.export thy \<^path_binding>\<open>code/Complementation_Export.sml\<close> [exported_code];
+      val exported_code =
+        #content (Generated_Files.get_file \<^theory>\<open>Complementation_Final\<close>
+          \<^path_binding>\<open>code/Complementation_Export.ML\<close>);
+      val _ = File.write (Path.append build_dir \<^path>\<open>Complementation_Export.sml\<close>) exported_code;
+      val _ = Export.export thy \<^path_binding>\<open>code/Complementation_Export.sml\<close> [exported_code];
 
-        (*compile*)
-        val compile_rc =
-          Isabelle_System.bash ("cd " ^ File.bash_path build_dir ^
-            " && \"$ISABELLE_MLTON\" -profile time -default-type intinf Complementation.mlb");
-        val _ =
-          if compile_rc = 0 then
-            Export.export_executable_file thy
-              (Path.map_binding Path.exe \<^path_binding>\<open>code/Complementation\<close>) exe
-          else error "Compilation failed";
+      (*compile*)
+      val compile_rc =
+        Isabelle_System.bash ("cd " ^ File.bash_path build_dir ^
+          " && " ^ File.bash_path \<^path>\<open>$ISABELLE_MLTON\<close> ^
+          " -profile time -default-type intinf Complementation.mlb");
+      val _ =
+        if compile_rc = 0 then
+          Export.export_executable_file thy
+            (Path.map_binding Path.exe \<^path_binding>\<open>code/Complementation\<close>) exe
+        else error "Compilation failed";
 
-        (*test*)
-        val test_rc =
-          Isabelle_System.bash
-            ("cd " ^ File.bash_path build_dir ^ " && ./Complementation Complementation.out");
-        val _ =
-          if test_rc = 0 then
-            List.app (fn binding =>
-              Export.export_file thy binding
-                (Path.append build_dir (Path.base (#1 (Path.dest_binding binding)))))
-             [\<^path_binding>\<open>code/Complementation.out\<close>,
-              \<^path_binding>\<open>code/mlmon.out\<close>]
-          else error "Test failed";
-      in () end)
+      (*test*)
+      val test_rc =
+        Isabelle_System.bash
+          ("cd " ^ File.bash_path build_dir ^ " && ./Complementation Complementation.out");
+      val _ =
+        if test_rc = 0 then
+          List.app (fn binding =>
+            Export.export_file thy binding
+              (Path.append build_dir (Path.base (#1 (Path.dest_binding binding)))))
+           [\<^path_binding>\<open>code/Complementation.out\<close>,
+            \<^path_binding>\<open>code/mlmon.out\<close>]
+        else error "Test failed";
+    in () end)
 \<close>
 
 end
--- thys/Diophantine_Eqns_Lin_Hom/Solver_Code.thy
+++ thys/Diophantine_Eqns_Lin_Hom/Solver_Code.thy
@@ -16,39 +16,39 @@ export_code solve checking Haskell \<comment> \<open>test whether Haskell code g
 export_code solve integer_of_nat nat_of_integer in Haskell module_name HLDE file_prefix generated
 
 ML_command \<^marker>\<open>contributor Makarius\<close> \<open>
-  if getenv "ISABELLE_GHC" = "" then warning "GHC not configured"
-  else
-    Isabelle_System.with_tmp_dir "HLDE" (fn build_dir =>
-      let
-        val thy = \<^theory>;
-        val exe = Path.append build_dir (Path.exe \<^path>\<open>Main\<close>);
+  Isabelle_System.with_tmp_dir "HLDE" (fn build_dir =>
+    let
+      val thy = \<^theory>;
+      val exe = Path.append build_dir (Path.exe \<^path>\<open>Main\<close>);
 
-        (*assemble sources*)
-        val _ = Isabelle_System.copy_file (Path.append \<^master_dir> \<^path>\<open>src/Main.hs\<close>) build_dir;
-        val _ =
-          File.write (Path.append build_dir \<^path>\<open>HLDE.hs\<close>)
-            (Generated_Files.the_file_content thy \<^path>\<open>code/generated/HLDE.hs\<close>);
+      (*assemble sources*)
+      val _ = Isabelle_System.copy_file (Path.append \<^master_dir> \<^path>\<open>src/Main.hs\<close>) build_dir;
+      val _ =
+        File.write (Path.append build_dir \<^path>\<open>HLDE.hs\<close>)
+          (#content (Generated_Files.get_file thy \<^path_binding>\<open>code/generated/HLDE.hs\<close>));
 
-        (*compile*)
-        val compile_rc =
-          Isabelle_System.bash ("cd " ^ File.bash_path build_dir ^ " && \"$ISABELLE_GHC\" Main.hs");
-        val () =
-          if compile_rc = 0 then
-            Export.export_executable_file thy
-              (Path.map_binding Path.exe \<^path_binding>\<open>code/generated/hlde\<close>) exe
-          else error "Compilation failed";
+      (*compile*)
+      val compile_rc =
+        Isabelle_System.bash
+          ("cd " ^ File.bash_path build_dir ^ " && " ^
+            File.bash_path \<^path>\<open>$ISABELLE_GHC\<close> ^ " Main.hs");
+      val () =
+        if compile_rc = 0 then
+          Export.export_executable_file thy
+            (Path.map_binding Path.exe \<^path_binding>\<open>code/generated/hlde\<close>) exe
+        else error "Compilation failed";
 
-        (*test*)
-        val print_coeffs = enclose "[" "]" o commas o map string_of_int;
-        fun print_hlde (xs, ys) =
-          let
-            val test_rc =
-              Isabelle_System.bash (File.bash_path exe ^
-                " <<< '(" ^ print_coeffs xs ^ ", " ^ print_coeffs ys ^ ")'");
-          in if test_rc = 0 then () else error "Test failed" end;
-      in
-        print_hlde ([3, 5, 1], [2, 7])
-      end);
+      (*test*)
+      val print_coeffs = enclose "[" "]" o commas o map string_of_int;
+      fun print_hlde (xs, ys) =
+        let
+          val test_rc =
+            Isabelle_System.bash (File.bash_path exe ^
+              " <<< '(" ^ print_coeffs xs ^ ", " ^ print_coeffs ys ^ ")'");
+        in if test_rc = 0 then () else error "Test failed" end;
+    in
+      print_hlde ([3, 5, 1], [2, 7])
+    end)
 \<close>
 
 end
