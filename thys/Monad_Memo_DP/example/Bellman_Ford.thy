01f03bf9f546801033ee6bbede99fddaef677283
Simon Wimmer <wimmers@in.tum.de>
2019-08-02 14:35:11 +0200

    Cycle detection lemma

--- thys/Monad_Memo_DP/example/Bellman_Ford.thy
+++ thys/Monad_Memo_DP/example/Bellman_Ford.thy
@@ -12,6 +12,7 @@ theory Bellman_Ford
     Example_Misc
     "../util/Tracing"
     "../util/Ground_Function"
+    "HOL-ex.Sketch_and_Explore"
 begin
 
 subsubsection \<open>Misc\<close>
@@ -66,6 +67,10 @@ definition
       {if t = v then 0 else \<infinity>}
     )
   )"
+
+lemma weight_Nil [simp]:
+  "weight [] = 0"
+  by (simp add: weight_def)
 
 lemma weight_Cons [simp]:
   "weight (v # w # xs) = W v w + weight (w # xs)"
@@ -341,6 +346,299 @@ lemmas memoized_empty = bf\<^sub>h.memoized_empty[OF bf\<^sub>h.consistent_DP_it
 lemmas iter_heap_unfold = iter_heap_unfold
 
 end (* Fixed Memory *)
+
+
+subsubsection \<open>Detection of Negative Cycles\<close>
+
+definition
+  "shortest v = (
+    Min (
+      {weight (v # xs) | xs. set xs \<subseteq> {0..n}} \<union>
+      {if t = v then 0 else \<infinity>}
+    )
+  )"
+
+fun weight2 :: "nat list \<Rightarrow> int extended" where
+  "weight2 [s] = 0"
+| "weight2 (i # j # xs) = W i j + weight2 (j # xs)"
+
+lemma weight2_eq':
+  "weight2 (s # xs) + w = snd (fold (\<lambda>j (i, x). (j, W i j + x)) xs (s, w))"
+  by (induction xs arbitrary: s w; simp; smt add.commute add.left_commute)
+
+lemma weight2_eq:
+  "weight2 (s # xs) = snd (fold (\<lambda>j (i, x). (j, W i j + x)) xs (s, 0))"
+  by (rule weight2_eq'[of s xs 0, simplified])
+
+lemma weight_eq:
+  "weight xs = weight2 (xs @ [t])"
+  by (induction xs rule: induct_list012) auto
+
+lemma weight2_append:
+  "weight2 (xs @ a # ys) = weight2 (xs @ [a]) + weight2 (a # ys)"
+  by (induction xs rule: weight2.induct; simp add: add.assoc)
+
+definition
+  "is_path2 xs \<equiv> weight2 xs < \<infinity>"
+
+definition
+  "is_path xs \<equiv> weight xs < \<infinity>"
+
+definition
+  "has_negative_cycle \<equiv>
+  \<exists>xs a ys. set (a # xs @ ys) \<subseteq> {0..n} \<and> weight2 (a # xs @ [a]) < 0 \<and> is_path (a # ys)"
+
+definition
+  "reaches a \<equiv> \<exists> xs. is_path (a # xs) \<and> a \<le> n \<and> set xs \<subseteq> {0..n}"
+
+lemma le_add_same_cancel1:
+  "a + b \<ge> a \<longleftrightarrow> b \<ge> 0" if "a < \<infinity>" "-\<infinity> < a" for a b :: "int extended"
+  using that by (cases a; cases b) (auto simp add: zero_extended_def)
+
+lemma fold_sum_aux':
+  assumes "\<forall>u \<in> set (a # xs). \<forall>v \<in> set (xs @ [b]). f v + W u v \<ge> f u"
+  shows "sum_list (map f (a # xs)) \<le> sum_list (map f (xs @ [b])) + weight2 (a # xs @ [b])"
+  using assms
+  by (induction xs arbitrary: a; simp)
+     (smt ab_semigroup_add_class.add_ac(1) add.left_commute add_mono)
+
+lemma fold_sum_aux:
+  assumes "\<forall>u \<in> set (a # xs). \<forall>v \<in> set (a # xs). f v + W u v \<ge> f u"
+  shows "sum_list (map f (a # xs @ [a])) \<le> sum_list (map f (a # xs @ [a])) + weight2 (a # xs @ [a])"
+  using fold_sum_aux'[of a xs a f] assms
+  by auto (metis (no_types, hide_lams) add.assoc add.commute add_left_mono)
+
+lemma add_gt_minfI:
+  assumes "-\<infinity> < a" "-\<infinity> < b"
+  shows "-\<infinity> < a + b"
+  using assms by (cases a; cases b) auto
+
+lemma add_lt_infI:
+  assumes "a < \<infinity>" "b < \<infinity>"
+  shows "a + b < \<infinity>"
+  using assms by (cases a; cases b) auto
+
+lemma is_path2_remove_cycle:
+  assumes "is_path2 (as @ a # bs @ a # cs)"
+  shows "is_path2 (as @ a # cs)"
+proof -
+  have "weight2 (as @ a # bs @ a # cs) =
+    weight2 (as @ [a]) + weight2 (a # bs @ [a]) + weight2 (a # cs)"
+    by (metis Bellman_Ford.weight2_append append_Cons append_assoc)
+  with assms have "weight2 (as @ [a]) < \<infinity>" "weight2 (a # cs) < \<infinity>"
+    unfolding is_path2_def
+    by auto (metis Pinf_add_right antisym less_extended_simps(4) not_less add.commute)+
+  then show ?thesis
+    unfolding is_path2_def by (subst weight2_append) (rule add_lt_infI)
+qed
+
+lemma is_path_eq:
+  "is_path xs \<longleftrightarrow> is_path2 (xs @ [t])"
+  unfolding is_path_def is_path2_def weight_eq ..
+
+lemma is_path_remove_cycle:
+  assumes "is_path (as @ a # bs @ a # cs)"
+  shows "is_path (as @ a # cs)"
+  using assms unfolding is_path_eq by (simp add: is_path2_remove_cycle)
+
+lemma is_path_remove_cycle2:
+  assumes "is_path (as @ t # cs)"
+  shows "is_path as"
+  using assms unfolding is_path_eq by (simp add: is_path2_remove_cycle)
+
+lemma list_pidgeonhole:
+  assumes "set xs \<subseteq> S" "card S < length xs" "finite S"
+  obtains as a bs cs where "xs = as @ a # bs @ a # cs"
+proof -
+  from assms have "\<not> distinct xs"
+    by (metis card_mono distinct_card not_le)
+  then show ?thesis
+    by (metis append.assoc append_Cons not_distinct_conv_prefix split_list that)
+qed
+
+lemma is_path_shorten:
+  assumes "is_path (i # xs)" "i \<le> n" "set xs \<subseteq> {0..n}" "t \<le> n" "n > 0" "t \<noteq> i"
+  obtains xs where "is_path (i # xs)" "i \<le> n" "set xs \<subseteq> {0..n}" "length xs < n"
+proof (cases "length xs < n")
+  case True
+  with assms show ?thesis
+    by (auto intro: that)
+next
+  case False
+  then have "length xs \<ge> n"
+    by auto
+  with assms(1,3) show ?thesis
+  proof (induction "length xs" arbitrary: xs rule: less_induct)
+    case less
+    then have "length (i # xs @ [t]) > card ({0..n})"
+      by auto
+    moreover from less.prems \<open>i \<le> n\<close> \<open>t \<le> n\<close> have "set (i # xs @ [t]) \<subseteq> {0..n}"
+      by auto
+    ultimately obtain a as bs cs where
+      "i # xs @ [t] = as @ a # bs @ a # cs"
+      by (elim list_pidgeonhole) auto
+    then consider (Nil_Nil) "as = []" "cs = []" "i = a" "a = t" "xs = bs"
+      | (Nil_Cons) cs' where "as = []" "i = a" "xs = bs @ a # cs'" "cs = cs' @ [t]"
+      | (Cons_Nil) as' where "as = i # as'" "cs = []" "a = t" "xs = as' @ a # bs"
+      | (Cons_Cons) as' cs' where "as = i # as'" "cs = cs' @ [t]" "xs = as' @ a # bs @ a # cs'"
+      by (auto simp: Cons_eq_append_conv append_eq_Cons_conv append_eq_append_conv2)
+    note R = this
+    obtain ys where ys: "is_path (i # ys)" "length ys < length xs" "set (i # ys) \<subseteq> {0..n}"
+      apply atomize_elim
+    proof (cases rule: R)
+      case Nil_Nil
+      with \<open>t \<noteq> i\<close> show "\<exists>ys. is_path (i # ys) \<and> length ys < length xs \<and> set (i # ys) \<subseteq> {0..n}"
+        by auto
+    next
+      case Nil_Cons
+      then show "\<exists>ys. is_path (i # ys) \<and> length ys < length xs \<and> set (i # ys) \<subseteq> {0..n}"
+        using \<open>set (i # xs @ [t]) \<subseteq> {0..n}\<close> \<open>is_path (i # xs)\<close> is_path_remove_cycle[of "[]"]
+        by - (rule exI[where x = cs'], simp)
+    next
+      case Cons_Nil
+      then show "\<exists>ys. is_path (i # ys) \<and> length ys < length xs \<and> set (i # ys) \<subseteq> {0..n}"
+        using \<open>set (i # xs @ [t]) \<subseteq> {0..n}\<close> \<open>is_path (i # xs)\<close>
+        by - (rule exI[where x = as'], auto intro: is_path_remove_cycle2)
+    next
+      case Cons_Cons
+      then show "\<exists>ys. is_path (i # ys) \<and> length ys < length xs \<and> set (i # ys) \<subseteq> {0..n}"
+        using \<open>set (i # xs @ [t]) \<subseteq> {0..n}\<close> \<open>is_path (i # xs)\<close> is_path_remove_cycle[of "i # as'"]
+        by - (rule exI[where x = "as' @ a # cs'"], auto)
+    qed
+    then show ?thesis
+      by (cases "n \<le> length ys") (auto intro: that less)
+  qed
+qed
+
+lemma reaches_non_inf_path:
+  assumes "reaches i" "i \<le> n" "t \<le> n"
+  shows "OPT n i < \<infinity>"
+proof -
+  consider "n = 0" | "t = i" | "n > 0" "t \<noteq> i"
+    by auto
+  then show ?thesis
+  proof cases
+    case 1
+    with \<open>i \<le> n\<close> \<open>t \<le> n\<close> show ?thesis
+      unfolding OPT_def by (auto simp: zero_extended_def)
+  next
+    case 2
+    with \<open>i \<le> n\<close> \<open>t \<le> n\<close> have "OPT n i \<le> 0"
+      unfolding OPT_def
+      apply -
+      apply (rule Min_le)
+       apply auto
+      sorry
+    then show ?thesis
+      using less_linear by (fastforce simp: zero_extended_def)
+  next
+    case 3
+    from assms(1) guess xs
+      unfolding reaches_def by safe
+    note xs = this
+    then obtain xs where xs: "is_path (i # xs)" "i \<le> n" "set xs \<subseteq> {0..n}" "length xs < n"
+      using \<open>t \<noteq> i\<close> \<open>n > 0\<close> \<open>t \<le> n\<close> by (auto intro: is_path_shorten)
+    then have "weight (i # xs) < \<infinity>"
+      unfolding is_path_def by auto
+    with xs(2-) show ?thesis
+      unfolding OPT_def
+      apply -
+      apply (erule order.strict_trans1[rotated])
+      apply (rule Min.coboundedI)
+       apply auto
+      sorry
+  qed
+qed
+
+lemma sum_list_not_infI:
+  "sum_list xs < \<infinity>" if "\<forall> x \<in> set xs. x < \<infinity>" for xs :: "int extended list"
+  using that
+  apply (induction xs)
+   apply (simp add: zero_extended_def)+
+  by (smt less_extended_simps(2) plus_extended.elims)
+
+lemma sum_list_not_minfI:
+  "sum_list xs > -\<infinity>" if "\<forall> x \<in> set xs. x > -\<infinity>" for xs :: "int extended list"
+  using that by (induction xs) (auto intro: add_gt_minfI simp: zero_extended_def)
+
+context
+  assumes W_wellformed: "\<forall>i \<le> n. \<forall>j \<le> n. W i j > -\<infinity>"
+  assumes "t \<le> n"
+begin
+
+lemma weight_not_minfI:
+  "-\<infinity> < weight xs" if "set xs \<subseteq> {0..n}"
+  using that
+  apply (induction xs rule: induct_list012)
+    apply (simp add: zero_extended_def weight_def; fail)
+  using W_wellformed \<open>t \<le> n\<close> by (auto intro: add_gt_minfI)
+
+lemma OPT_not_minfI:
+  "OPT n i > -\<infinity>" if "i \<le> n"
+proof -
+  have "OPT n i \<in>
+    {weight (i # xs) |xs. length xs + 1 \<le> n \<and> set xs \<subseteq> {0..n}} \<union> {if t = i then 0 else \<infinity>}"
+    unfolding OPT_def
+    apply (rule Min_in)
+    apply auto
+    sorry
+  with that show ?thesis
+    by (auto 4 3 intro!: weight_not_minfI simp: zero_extended_def)
+qed
+
+lemma
+  assumes has_negative_cycle "t \<le> n"
+  shows "\<exists>i \<le> n. OPT (n + 1) i < OPT n i"
+proof -
+  from assms guess xs a ys
+    unfolding has_negative_cycle_def by clarsimp
+  note cycle = this
+  then have "reaches a"
+    unfolding reaches_def by auto
+  have reaches: "reaches x" if "x \<in> set xs" for x
+  proof -
+    from that obtain as bs where "xs = as @ x # bs"
+      by atomize_elim (rule split_list)
+    with cycle have "weight2 (x # bs @ [a]) < \<infinity>"
+      using weight2_append[of "a # as" x "bs @ [a]"]
+      by simp (metis Pinf_add_right Pinf_le add.commute less_eq_extended.simps(2) not_less)
+    with \<open>reaches a\<close> show ?thesis
+      unfolding reaches_def is_path_def
+      apply clarsimp
+      subgoal for cs
+        unfolding weight_eq using weight2_append[of "x # bs" a "cs @ [t]"] cycle(1-4) \<open>xs = _\<close>
+        by - (rule exI[where x = "bs @ [a] @ cs"], auto intro: add_lt_infI)
+      done
+  qed
+  let ?S = "sum_list (map (OPT n) (a # xs @ [a]))"
+  have "\<exists>v \<le> n. \<exists>w \<le> n. OPT n w + W v w < OPT n v"
+  proof (rule ccontr , simp)
+    assume A: "\<forall>v\<le>n. \<forall>w\<le>n. \<not> OPT n w + W v w < OPT n v"
+    then have A: "\<forall>v\<le>n. \<forall>w\<le>n. OPT n w + W v w \<ge> OPT n v"
+      by force
+    then have "?S \<le> ?S + weight2 (a # xs @ [a])"
+      using cycle(1-3) by (subst fold_sum_aux) (auto simp: subset_eq)
+    moreover have "sum_list (map (OPT n) (a # xs @ [a])) > -\<infinity>"
+      using cycle(1-4) by - (rule sum_list_not_minfI, auto intro!: OPT_not_minfI)
+    moreover have "sum_list (map (OPT n) (a # xs @ [a])) < \<infinity>"
+      using reaches cycle(1-4)
+      by (intro sum_list_not_infI) (auto intro: reaches_non_inf_path \<open>reaches a\<close> simp: subset_eq)
+    ultimately have "weight2 (a # xs @ [a]) \<ge> 0"
+      by (simp add: le_add_same_cancel1)
+    with \<open>weight2 _ < 0\<close> show False
+      by simp
+  qed
+  then guess u v
+    by safe
+  note relaxation = this
+  then show ?thesis
+    by -
+       (rule exI[where x = u],
+        auto 4 4 intro: Min.coboundedI min.strict_coboundedI2 elim: order.strict_trans1[rotated]
+          simp: OPT_Suc[OF \<open>t \<le> n\<close>])
+qed
+  
+end (* Wellformedness *)
 
 end (* Final Node *)
 
