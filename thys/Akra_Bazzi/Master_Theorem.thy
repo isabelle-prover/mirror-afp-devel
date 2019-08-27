c6699991e6fef9b08131513ae1f6bd578e85f707
nipkow <unknown>
2019-08-27 15:35:58 +0200

    fixed import

--- thys/Akra_Bazzi/Master_Theorem.thy
+++ thys/Akra_Bazzi/Master_Theorem.thy
@@ -7,7 +7,7 @@
 section \<open>The Master theorem\<close>
 theory Master_Theorem
 imports
-  "HOL-Analysis.Ordered_Euclidean_Space"
+  "HOL-Analysis.Equivalence_Lebesgue_Henstock_Integration"
   Akra_Bazzi_Library
   Akra_Bazzi
 begin
