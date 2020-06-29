9a7985e8398220e5bb4d7a2b4cf2ab381b522ca3
Lars Hupel <lars.hupel@mytum.de>
2020-06-29 09:24:43 +0200

    exclude another session on Mac

--- admin/jenkins/ci_build_mac.scala
+++ admin/jenkins/ci_build_mac.scala
@@ -18,7 +18,7 @@ object profile extends isabelle.CI_Profile
 
   def selection = Sessions.Selection(
     all_sessions = true,
-    exclude_sessions = List("HOL-Proofs", "HOL-ODE-Numerics", "Linear_Programming"),
+    exclude_sessions = List("HOL-Proofs", "HOL-ODE-Numerics", "Linear_Programming", "HOL-Nominal-Examples"),
     exclude_session_groups = List("slow"))
 
 }
