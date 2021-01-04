ca3c47b119bc56dc750e830b0160c8c96978bce6
Julian Brunner <julianbrunner@gmail.com>
2021-01-04 17:04:39 +0100

    updated benchmark scripts for iFM paper

--- thys/Buchi_Complementation/code/benchmark.py
+++ thys/Buchi_Complementation/code/benchmark.py
@@ -1,21 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 
 import sys, random, subprocess, time
 
-owl = "/home/Projects/owl/build/distributions/owl-minimized-20.XX-development/bin/owl"
-
 def read(path):
-	f = open(path, "r")
-	text = f.read()
-	f.close
-	return text
+	with open(path, "r") as f: return f.read()
 def write(path, text):
-	f = open(path, "w")
-	f.write(text)
-	f.close()
+	with open(path, "w") as f: f.write(text)
+
+def measure(action):
+	start = time.time()
+	result = action()
+	return result, time.time() - start
 
 def get_state_count(path):
 	result = subprocess.run(["autfilt", path, "--stats=%s"], stdout = subprocess.PIPE, text = True)
+	return int(result.stdout.strip())
+def get_transition_count(path):
+	result = subprocess.run(["autfilt", path, "--stats=%t"], stdout = subprocess.PIPE, text = True)
 	return int(result.stdout.strip())
 
 def spot_generate_automaton(state_count, proposition_count):
@@ -34,10 +35,6 @@ def owl_simplify_automaton(path):
 	result = subprocess.run(["owl", "-I", path, "---", "hoa", "---", "optimize-aut", "---", "hoa", "-s"], stdout = subprocess.PIPE, text = True)
 	return result.stdout
 
-def spot_to_buchi_automaton(path):
-	result = subprocess.run(["autfilt", "--ba", "--small", path], stdout = subprocess.PIPE, text = True, timeout = 60)
-	return result.stdout
-
 def spot_formula_to_nba(formula):
 	result = subprocess.run(["ltl2tgba", "--ba", formula], stdout = subprocess.PIPE, text = True)
 	return result.stdout
@@ -54,34 +51,57 @@ def owl_formula_to_dra_opt(formula):
 	result = subprocess.run(["owl", "-i", formula, "---", "ltl", "---", "simplify-ltl", "---", "ltl2dra", "---", "optimize-aut", "---", "hoa", "-s"], stdout = subprocess.PIPE, text = True)
 	return result.stdout
 
-def bc_complement(path1, path2):
-	subprocess.run(["./Autool", "complement_quick", path1, path2], timeout = 60)
-def bc_equivalence(path1, path2):
-	result = subprocess.run(["./Autool", "equivalence", path1, path2], timeout = 60, stdout = subprocess.PIPE, text = True)
-	return result.stdout.strip() == "true"
+def spot_complement(path_input, path_output):
+	try: _, time = measure(lambda: write(path_output, subprocess.run(["autfilt", "--complement", "--ba", path_input], timeout = 60, stdout = subprocess.PIPE, text = True).stdout))
+	except subprocess.TimeoutExpired: return None
+	state_count = get_state_count(path_output)
+	transition_count = get_transition_count(path_output)
+	return state_count, transition_count, time
 
-def complement(path_input, path_output):
-	start = time.time()
-	try: bc_complement(path_input, path_output)
-	except subprocess.TimeoutExpired: duration = "T"
-	else: duration = str(time.time() - start)
-	print("{} {}".format(get_state_count(path_input), duration))
+def goal_complement(path_input, path_output, tight):
+	try: _, time = measure(lambda: subprocess.run(["gc", "complement", "-m", "rank", "-tr" if tight else "-rd", "1", "-o", path_output, path_input], timeout = 60))
+	except subprocess.TimeoutExpired:
+		subprocess.run(["killall", "java"])
+		return None
+	state_count = int(subprocess.run(["gc", "stat", "-s", path_output], stdout = subprocess.PIPE, text = True).stdout.strip())
+	transition_count = int(subprocess.run(["gc", "stat", "-t", path_output], stdout = subprocess.PIPE, text = True).stdout.strip())
+	return state_count, transition_count, time
+
+def bc_complement(path_input, path_output):
+	try: _, time = measure(lambda: subprocess.run(["./Autool", "complement_quick", path_input, path_output], timeout = 60))
+	except subprocess.TimeoutExpired: return None
+	complement = read(path_output).splitlines()
+	state_count = int(complement[0].split()[0]) + 1
+	transition_count = len(complement)
+	return state_count, transition_count, time
+def bc_equivalence(path1, path2):
+	try: result, time = measure(lambda: subprocess.run(["./Autool", "equivalence", path1, path2], timeout = 60, stdout = subprocess.PIPE, text = True))
+	except subprocess.TimeoutExpired: return None
+	return result.stdout.strip() == "true", time
+
+def complement(path):
+	r1 = spot_complement(path, path + ".spot")
+	r2 = goal_complement(path, path + ".goaltr", True)
+	r3 = goal_complement(path, path + ".goalrd", False)
+	r4 = bc_complement(path, path + ".bc")
+	print("{} {} {} {} {}".format(get_state_count(path), r1, r2, r3, r4))
 def equivalence(path_1, path_2):
-	start = time.time()
-	try: eq = bc_equivalence(path_1, path_2)
-	except subprocess.TimeoutExpired: eq = None; duration = "T"
-	else: duration = str(time.time() - start)
-	print("{} {} {} {}".format(get_state_count(path_1), get_state_count(path_2), eq, duration))
+	r = bc_equivalence(path_1, path_2)
+	print("{} {} {}".format(get_state_count(path_1), get_state_count(path_2), r))
+	if not r: return None
+	eq, _ = r
 	return eq
 
 if sys.argv[1] == "complement_automaton":
 	while True:
 		write("a.hoa", spot_generate_automaton(20, 4))
-		complement("a.hoa", "c.txt")
+		complement("a.hoa")
 if sys.argv[1] == "complement_formula":
 	while True:
-		write("a.hoa", spot_formula_to_nba(spot_generate_formula(4)))
-		complement("a.hoa", "c.txt")
+		while True:
+			write("a.hoa", spot_formula_to_nba(spot_generate_formula(4)))
+			if get_state_count("a.hoa") == 10: break
+		complement("a.hoa")
 if sys.argv[1] == "equivalence_random":
 	while True:
 		write("a.hoa", spot_generate_automaton(100, 4))
@@ -89,7 +109,7 @@ if sys.argv[1] == "equivalence_random":
 		equivalence("a.hoa", "b.hoa")
 if sys.argv[1] == "equivalence_simplify":
 	while True:
-		write("a.hoa", spot_generate_automaton(10, 4))
+		write("a.hoa", spot_generate_automaton(20, 4))
 		write("b.hoa", spot_simplify_automaton("a.hoa"))
 		equivalence("a.hoa", "b.hoa")
 if sys.argv[1] == "equivalence_translate":
@@ -99,12 +119,3 @@ if sys.argv[1] == "equivalence_translate":
 		write("b.hoa", owl_formula_to_dra_opt(formula))
 		write("c.hoa", spot_simplify_automaton("b.hoa"))
 		if equivalence("a.hoa", "c.hoa") == False: print(formula)
-if sys.argv[1] == "equivalence_translate_collection":
-	for formula in read(sys.argv[2]).splitlines():
-		print(formula)
-		try:
-			write("a.hoa", spot_formula_to_nba(formula))
-			write("b.hoa", owl_formula_to_dra_opt(formula))
-			write("c.hoa", spot_to_buchi_automaton("b.hoa"))
-			equivalence("a.hoa", "c.hoa")
-		except subprocess.TimeoutExpired: print("T")
