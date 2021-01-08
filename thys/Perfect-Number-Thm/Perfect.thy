06e1878d7964851a36aafcc7207efbbd8e4695fc
paulson <lp15@cam.ac.uk>
2021-01-08 19:02:09 +0000

    A bit of tidying

--- thys/Perfect-Number-Thm/Perfect.thy
+++ thys/Perfect-Number-Thm/Perfect.thy
@@ -5,7 +5,7 @@ imports Sigma
 begin
 
 definition  perfect :: "nat => bool" where
-"perfect m == m>0 & 2*m = sigma m"
+  "perfect m \<equiv> m>0 \<and> 2*m = sigma m"
 
 theorem perfect_number_theorem:
   assumes even: "even m" and perfect: "perfect m"
@@ -21,7 +21,7 @@ proof
 
   have  "2^?n dvd m" by (rule multiplicity_dvd)
   hence "m = 2^?n*?A" by (simp only: dvd_mult_div_cancel) 
-  with m0 have mdef: "m=2^?n*?A & coprime 2 ?A"
+  with m0 have mdef: "m=2^?n*?A \<and> coprime 2 ?A"
     using multiplicity_decompose [of m 2] by simp
   moreover with m0 have a0: "?A>0" by (metis nat_0_less_mult_iff)
   moreover
@@ -50,33 +50,37 @@ proof
 
   have "?B = 1"
   proof (rule ccontr)
-    assume "~?B = 1"
+    assume "?B \<noteq> 1"
     with b0 bsmallera have "1<?B" "?B<?A" by auto
-    moreover from bdef have "?B : divisors ?A" by (rule mult_divisors2)
-    ultimately have "1+?B+?A <= sigma ?A" by (rule sigma_third_divisor)
-    with nplarger have "?np*(1+?A+?B) <= ?np*(sigma ?A)"
+    moreover from bdef have "?B : divisors ?A"
+      by (metis divisors_eq_dvd dvd_triv_right)
+    ultimately have "1+?B+?A \<le> sigma ?A"
+      using sigma_third_divisor by blast
+    with nplarger have "?np*(1+?A+?B) \<le> ?np*(sigma ?A)"
       by (auto simp only: nat_mult_le_cancel1)
-    with bdef have "?np+?A*?np + ?A*1 <= ?np*(sigma ?A)"
-      by (simp only: add_mult_distrib_three mult.commute)
-    hence "?np+?A*(?np + 1) <= ?np*(sigma ?A)" by (simp only:add_mult_distrib2)
+    with bdef have "?np+?A*?np + ?A*1 \<le> ?np*(sigma ?A)"
+      by (simp add: mult.commute distrib_left)
+    hence "?np+?A*(?np + 1) \<le> ?np*(sigma ?A)" by (simp only:add_mult_distrib2)
     with nplarger have "2^(?n+1)*?A < ?np*(sigma ?A)" by(simp add:mult.commute)
     with formula show "False" by auto
   qed
 
   with bdef have adef: "?A=?np" by auto
-  with formula have "?np*2^(?n+1) =(?np)*sigma(?A)" by auto
+  with formula have "?np*2^(?n+1) = ?np * sigma(?A)" by auto
   with nplarger adef have "?A + 1=sigma(?A)" by auto
-  with a0 have "prime ?A" by (simp add: sigma_imp_prime)
-  with mdef adef show "m = 2^?n*(?np) & prime ?np" by simp
+  with a0 have "prime ?A"
+    by (simp add: prime_iff_sigma)
+  with mdef adef show "m = 2^?n * ?np \<and> prime ?np" by simp
 qed
 
 theorem Euclid_book9_prop36:
   assumes p: "prime (2^(n+1) - (1::nat))"
-  shows "perfect ((2^n)*(2^(n+1) - 1))"
-proof (unfold perfect_def, auto)
-  from assms show "(2::nat)*2^n > Suc 0" by (auto simp add: prime_nat_iff)
+  shows "perfect (2 ^ n * (2 ^ (n + 1) - 1))"
+  unfolding perfect_def
+proof (intro conjI; simp)
+  from assms show "2 * 2^n > Suc 0" by (auto simp add: prime_nat_iff)
 next
-  have "2 ~= ((2::nat)^(n+1) - 1)" by simp arith
+  have "2 \<noteq> ((2::nat)^(n+1) - 1)" by simp arith
   then have "coprime (2::nat) (2^(n+1) - 1)"
     by (metis p primes_coprime_nat two_is_prime_nat) 
   moreover with p have "2^(n+1) - 1 > (0::nat)"
--- thys/Perfect-Number-Thm/PerfectBasics.thy
+++ thys/Perfect-Number-Thm/PerfectBasics.thy
@@ -4,16 +4,8 @@ theory PerfectBasics
 imports Main "HOL-Computational_Algebra.Primes" "HOL-Algebra.Exponent"
 begin
 
-lemma sum_mono2_nat: "finite (B::nat set) \<Longrightarrow> A <= B \<Longrightarrow> \<Sum> A <= \<Sum> B"
-  by (auto simp add: sum_mono2)
-
-(* TODO Move *)
-lemma multiplicity_0 [simp]: "multiplicity 0 x = 0" 
-  by (cases "x = 0") (auto intro: not_dvd_imp_multiplicity_0)
-  
-
 lemma exp_is_max_div:
-   assumes m0:"m \<noteq> 0" and p: "prime p"
+   assumes m0: "m \<noteq> 0" and p: "prime p"
    shows "~ p dvd (m div (p^(multiplicity p m)))"
 proof (rule ccontr)
   assume "~ ~ p dvd (m div (p^(multiplicity p m)))"
@@ -40,24 +32,12 @@ proof (rule ccontr)
     by (auto simp add: exp_is_max_div)
 qed
 
-lemma add_mult_distrib_three: "(x::nat)*(a+b+c)=x*a+x*b+x*c" 
-proof -
-  have "(x::nat)*(a+b+c) = x*((a+b)+c)" by auto
-  hence "x*(a+b+c) = x*(a+b)+x*c" by (simp add: algebra_simps)
-  thus "x*(a+b+c) = x*a+x*b+x*c" by (simp add: algebra_simps) 
-qed
-
-lemma nat_interval_minus_zero: "{0..Suc n} = {0} Un {Suc 0..Suc n}" by auto
-lemma nat_interval_minus_zero2:
- assumes "n>0"
- shows "{0..n} = {0} Un {Suc 0..n}" by (auto simp add: nat_interval_minus_zero)
-
 theorem simplify_sum_of_powers: "(x - 1::nat) * (\<Sum>i=0 .. n . x^i)  = x^(n + 1) - 1" (is "?l = ?r")
 proof (cases)
   assume "n = 0"
   thus "?l = x^(n+1) - 1" by auto
-  next
-  assume "n~=0"
+next
+  assume "n\<noteq>0"
   hence n0: "n>0" by auto 
   have "?l  = (x::nat)*(\<Sum>i=0 .. n . x^i) - (\<Sum>i=0 .. n . x^i)"
     by (metis diff_mult_distrib nat_mult_1)
@@ -65,9 +45,8 @@ proof (cases)
     by (simp add: sum_distrib_left)
   also have "... = (\<Sum>i=Suc 0 .. Suc n . x^i)  - (\<Sum>i=0 .. n . x^i)"
     by (metis sum.shift_bounds_cl_Suc_ivl)
-  also with n0
-  have "... = ((\<Sum>i=Suc 0 .. n. x^i)+x^(Suc n)) - (x^0 + (\<Sum>i=Suc 0 .. n. x^i))"
-    by (auto simp add: sum.union_disjoint nat_interval_minus_zero2)
+  also have "... = ((\<Sum>i=Suc 0 .. n. x^i)+x^(Suc n)) - (x^0 + (\<Sum>i=Suc 0 .. n. x^i))"
+    by (simp add: sum.union_disjoint diff_add_inverse sum.atLeast_Suc_atMost)
   finally show "?thesis" by auto
 qed
 
