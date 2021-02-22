d5dfb7320cbe8446f1854d4163014b8980cb2380
wenzelm <unknown>
2021-02-22 15:01:24 +0100

    adapted to Isabelle/f0db1e4c89bc;

--- thys/Isabelle_Meta_Model/toy_example/embedding/Generator_dynamic_sequential.thy
+++ thys/Isabelle_Meta_Model/toy_example/embedding/Generator_dynamic_sequential.thy
@@ -643,10 +643,11 @@ structure Export_code_env = struct
 end
 
 fun compile l cmd =
-  let val (l, rc) = fold (fn cmd => (fn (l, 0) =>
-                                         let val {out, err, rc, ...} = Isabelle_System.bash_process cmd in
-                                         ((out, err) :: l, rc) end
-                                     | x => x)) l ([], 0)
+  let val (l, rc) = fold (fn cmd =>
+        (fn (l, 0) =>
+             let val res = Isabelle_System.bash_process cmd in
+             ((Process_Result.out res, Process_Result.err res) :: l, Process_Result.rc res) end
+         | x => x)) l ([], 0)
       val l = rev l in
   if rc = 0 then
     (l, Isabelle_System.bash_output cmd)
