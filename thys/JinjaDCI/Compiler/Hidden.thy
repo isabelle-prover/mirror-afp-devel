626cfed406890bb2b70b1c8d1bec90ad6ea87e8e
Susannah Mansky <susannahej@gmail.com>
2021-07-08 10:44:25 -0500

    Hidden.thy to Isar

--- thys/JinjaDCI/Compiler/Hidden.thy
+++ thys/JinjaDCI/Compiler/Hidden.thy
@@ -7,10 +7,8 @@ definition hidden :: "'a list \<Rightarrow> nat \<Rightarrow> bool" where
 
 
 lemma hidden_last_index: "x \<in> set xs \<Longrightarrow> hidden (xs @ [x]) (last_index xs x)"
-apply(auto simp add: hidden_def nth_append rev_nth[symmetric])
-apply(drule last_index_less[OF _ le_refl])
-apply simp
-done
+by(auto simp add: hidden_def nth_append rev_nth[symmetric]
+        dest: last_index_less[OF _ le_refl])
 
 lemma hidden_inacc: "hidden xs i \<Longrightarrow> last_index xs x \<noteq> i"
 by(auto simp add: hidden_def last_index_drop last_index_less_size_conv)
@@ -24,12 +22,19 @@ lemma fun_upds_apply:
  "(m(xs[\<mapsto>]ys)) x =
   (let xs' = take (size ys) xs
    in if x \<in> set xs' then Some(ys ! last_index xs' x) else m x)"
-apply(induct xs arbitrary: m ys)
- apply (simp add: Let_def)
-apply(case_tac ys)
- apply (simp add:Let_def)
-apply (simp add: Let_def last_index_Cons)
-done
+proof(induct xs arbitrary: m ys)
+  case Nil then show ?case by(simp add: Let_def)
+next
+  case Cons show ?case 
+  proof(cases ys)
+    case Nil
+    then show ?thesis by(simp add:Let_def)
+  next
+    case Cons': Cons
+    then show ?thesis using Cons by(simp add: Let_def last_index_Cons)
+  qed
+qed
+
 
 
 lemma map_upds_apply_eq_Some:
@@ -42,8 +47,6 @@ by(simp add:fun_upds_apply Let_def)
 lemma map_upds_upd_conv_last_index:
   "\<lbrakk>x \<in> set xs; size xs \<le> size ys \<rbrakk>
   \<Longrightarrow> m(xs[\<mapsto>]ys)(x\<mapsto>y) = m(xs[\<mapsto>]ys[last_index xs x := y])"
-apply(rule ext)
-apply(simp add:fun_upds_apply eq_sym_conv Let_def)
-done
+by(rule ext) (simp add:fun_upds_apply eq_sym_conv Let_def)
 
 end
