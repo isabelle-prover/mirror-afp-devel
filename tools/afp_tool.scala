4b167a61064a55d0dc6e809cfe9ffe4ad1d652f2
Fabian Huch <huch@in.tum.de>
2021-11-12 14:06:06 +0100

    added afp hugo component;

--- tools/afp_tool.scala
+++ tools/afp_tool.scala
@@ -9,6 +9,7 @@ import afp.migration._
 class Admin_Tools extends Isabelle_Scala_Tools(
   AFP_Migrate_Metadata.isabelle_tool,
   AFP_Build_Python.isabelle_tool,
+  AFP_Build_Hugo.isabelle_tool,
 )
 
 class Tools extends Isabelle_Scala_Tools(
