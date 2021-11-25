88a308dad1fe2df978da879c515f1550be7e2ea1
Fabian Huch <huch@in.tum.de>
2021-11-25 13:16:48 +0100

    use tmp file for python script instead of command;

--- tools/python.scala
+++ tools/python.scala
@@ -10,12 +10,19 @@ object Python
 
   val python_path = (Path.explode("$AFP_BASE") + Path.make(List("admin", "sitegen-lib"))).absolute
 
-  def run(command: String): Process_Result =
+  def run(script: String): Process_Result =
   {
     val exec =
       Path.explode(proper_string(python_home).getOrElse("No python component found")) +
       Path.make(List("bin", "python3"))
-    Isabelle_System.bash(exec.implode + " -c '" + command + "'",
+    
+    val script_file = Isabelle_System.tmp_file("isabelle_python", "py")
+    File.write(script_file, script)
+    
+    val res = Isabelle_System.bash(exec.implode + " " + script_file.getAbsolutePath,
       env = Isabelle_System.settings(List("PYTHONPATH" -> python_path.implode)))
+    
+    script_file.delete()
+    res
   }
 }
