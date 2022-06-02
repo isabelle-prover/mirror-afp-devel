f3db71d0f9edcd285f34f753449dd7cf2e8e5a9b
Fabian Huch <huch@in.tum.de>
2022-06-02 15:37:32 +0200

    replaced ci_profiles by statically compiled afp_build tool (Scala 3 preparations);

--- tools/afp_tool.scala
+++ tools/afp_tool.scala
@@ -17,3 +17,11 @@ class Tools extends Isabelle_Scala_Tools(
   AFP_Check_Roots.isabelle_tool,
   AFP_Dependencies.isabelle_tool,
 )
+
+class Build_Tools extends Isabelle_Scala_Tools(
+  AFP_Build.ci_build_afp,
+  AFP_Build.ci_build_all,
+  AFP_Build.ci_build_mac,
+  AFP_Build.ci_build_slow,
+  AFP_Build.ci_build_testboard
+)
