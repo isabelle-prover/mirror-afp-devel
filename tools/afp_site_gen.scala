2990790731c06dede41ab378a111d49c935ff5b5
Fabian Huch <huch@in.tum.de>
2022-06-09 19:04:40 +0200

    replaced python afp stats with Isabelle/Scala tool;

--- tools/afp_site_gen.scala
+++ tools/afp_site_gen.scala
@@ -7,7 +7,7 @@ package afp
 
 import isabelle._
 
-import afp.Metadata.{Affiliation, Author, Email, Homepage}
+import afp.Metadata.{Affiliation, Author, Email, Entry, Homepage}
 
 
 object AFP_Site_Gen {
@@ -93,6 +93,68 @@ object AFP_Site_Gen {
     val keyword_cmd = "from keywords import *; print_keywords(" + arg + ")"
 
     Python.run(keyword_cmd).check.out_lines
+  }
+
+
+  /* stats */
+
+  def afp_stats(deps: Sessions.Deps, structure: AFP_Structure, entries: List[Entry], progress: Progress): JSON.T = {
+    def round(int: Int): Int = Math.round(int.toFloat / 100) * 100
+
+    def nodes(entry: Entry): List[Document.Node.Name] =
+      structure.entry_sessions(entry.name).flatMap(session => deps(session.name).session_theories)
+
+    var entry_lines = Map.empty[Entry, Int]
+    var entry_lemmas = Map.empty[Entry, Int]
+    for {
+      entry <- entries
+      node <- nodes(entry)
+      lines = split_lines(File.read(node.path)).map(_.trim)
+    } {
+      entry_lines += entry -> (entry_lines.getOrElse(entry, 0) + lines.count(_.nonEmpty))
+      entry_lemmas += entry -> (entry_lemmas.getOrElse(entry, 0) +
+        lines.count(line => List("lemma", "theorem", "corollary").exists(line.startsWith)))
+    }
+
+    val first_year = entries.flatMap(_.releases).map(_.date.getYear).min
+    def years(upto: Int): List[Int] = Range.inclusive(first_year, upto).toList
+
+    val current_year = Date.now().rep.getYear
+    val all_years = years(current_year)
+
+    // per Isabelle release year
+    val by_year = entries.groupBy(_.date.getYear)
+    val size_by_year = by_year.view.mapValues(_.length).toMap
+    val loc_by_year = by_year.view.mapValues(_.map(entry_lines).sum).toMap
+    val authors_by_year = by_year.view.mapValues(_.flatMap(_.authors).map(_.author)).toMap
+
+    val num_lemmas = entries.map(entry_lemmas).sum
+    val num_lines = entries.map(entry_lines).sum
+
+    // accumulated
+    def total_articles(year: Int): Int =
+      years(year).map(size_by_year.getOrElse(_, 0)).sum
+
+    def total_loc(year: Int): Int =
+      round(years(year).map(loc_by_year.getOrElse(_, 0)).sum)
+
+    def total_authors(year: Int): Int =
+      years(year).flatMap(authors_by_year.getOrElse(_, Nil)).distinct.length
+
+    def fresh_authors(year: Int): Int =
+      total_authors(year) - total_authors(year - 1)
+
+    val sorted = entries.sortBy(_.date)
+
+    isabelle.JSON.Object(
+      "num_lemmas" -> num_lemmas,
+      "num_loc" -> num_lines,
+      "articles_year" -> all_years.map(total_articles),
+      "loc_years" -> all_years.map(total_loc),
+      "author_years" -> all_years.map(fresh_authors),
+      "author_years_cumulative" -> all_years.map(total_authors),
+      "loc_articles" -> sorted.map(entry_lines),
+      "all_articles" -> sorted.map(_.name))
   }
 
 
@@ -234,15 +296,9 @@ object AFP_Site_Gen {
 
     progress.echo("Preparing statistics...")
 
-    val statistics_cmd = "from statistics import *; add_statistics(" +
-      commas_quote(
-        List(
-          Path.explode("$AFP_BASE").absolute.implode,
-          Path.explode("$AFP").absolute.implode,
-          layout.data_dir.implode)) +
-      ")"
-    Python.run(statistics_cmd).check
-    (layout.data_dir + Path.basic("statistics.html")).file.delete()
+    val statistics_json = afp_stats(sessions_deps, afp_structure, entries, progress)
+
+    layout.write_data(Path.basic("statistics.json"), statistics_json)
 
 
     /* project */
