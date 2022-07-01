23f9b0d3512713ca85a3ca0a600de7188e18b102
mohammadabdulaziz <unknown>
2022-07-01 20:05:05 +0200

    Removed ML code from AFP entry

--- thys/AI_Planning_Languages_Semantics/PDDL_STRIPS_Checker.thy
+++ thys/AI_Planning_Languages_Semantics/PDDL_STRIPS_Checker.thy
@@ -403,4 +403,458 @@ context ast_domain begin
       (ERR (shows ''No such action schema '' o shows n))"
 end \<comment> \<open>Context of \<open>ast_domain\<close>\<close>
 
+context ast_problem begin
+
+(*TODO: change to this valuation definition to hanlde equalities nicely
+definition "valuation s \<equiv> \<lambda>x. case x of (atom.Atom P ARGS) \<Rightarrow>
+                                                ((formula.Atom x) \<in> s)
+                                    | (atom.Eq t1 t2) \<Rightarrow> (t1 = t2)"
+
+primrec holds :: "'a formula set \<Rightarrow> 'a formula \<Rightarrow> bool" where
+"holds s (Atom k) = ((Atom k) \<in> s)" |
+"holds _ \<bottom> = False" |
+"holds s (Not F) = (\<not> (holds s F))" |
+"holds s (And F G) = (holds s F \<and> holds s G)" |
+"holds s (Or F G) = (holds s F \<or> holds s G)" |
+"holds s (Imp F G) = (holds s F \<longrightarrow> holds s G)"
+
+  lemma holds_for_valid_formulas:
+        assumes "\<forall>x\<in>s. \<exists>x'. x = formula.Atom x'"
+        shows "s \<TTurnstile> F \<Longrightarrow> holds s F"
+        unfolding holds_def entailment_def
+        using assms
+        apply (induction F; auto)
+        subgoal for x apply(cases x)*)
+
+
+  text \<open>We define a function to determine whether a formula holds in
+    a world model\<close>
+  definition "holds M F \<equiv> (valuation M) \<Turnstile> F"
+
+  text \<open>Justification of this function\<close>
+
+  lemma holds_for_wf_fmlas:
+    assumes "wm_basic s"
+    shows "holds s F \<longleftrightarrow> close_world s \<TTurnstile> F"
+    unfolding holds_def using assms valuation_iff_close_world
+    by blast
+
+  (*
+  lemma holds_for_wf_fmlas:
+    assumes "\<forall>x\<in>s. is_Atom x" "wf_fmla Q F"
+    shows "holds s F \<longleftrightarrow> s \<TTurnstile> F"
+    unfolding holds_def entailment_def valuation_def
+    using assms
+  proof (induction F)
+    case (Atom x)
+    then show ?case
+      apply auto
+      by (metis formula_semantics.simps(1) is_Atom.elims(2) valuation_def)
+  next
+    case (Or F1 F2)
+    then show ?case
+      apply simp apply (safe; clarsimp?)
+      by (metis (mono_tags) is_Atom.elims(2) formula_semantics.simps(1))
+  qed auto
+  *)
+
+  text \<open>The first refinement summarizes the enabledness check and the execution
+    of the action. Moreover, we implement the precondition evaluation by our
+     @{const holds} function. This way, we can eliminate redundant resolving
+     and instantiation of the action.
+  \<close>
+
+  definition en_exE :: "plan_action \<Rightarrow> world_model \<Rightarrow> _+world_model" where
+    "en_exE \<equiv> \<lambda>(PAction n args) \<Rightarrow> \<lambda>s. do {
+      a \<leftarrow> resolve_action_schemaE n;
+      check (action_params_match a args) (ERRS ''Parameter mismatch'');
+      let ai = instantiate_action_schema a args;
+      check (wf_effect_inst (effect ai)) (ERRS ''Effect not well-formed'');
+      check ( holds s (precondition ai)) (ERRS ''Precondition not satisfied'');
+      Error_Monad.return (apply_effect (effect ai) s)
+    }"
+
+  text \<open>Justification of implementation.\<close>
+  lemma (in wf_ast_problem) en_exE_return_iff:
+    assumes "wm_basic s"
+    shows "en_exE a s = Inr s'
+      \<longleftrightarrow> plan_action_enabled a s \<and> s' = execute_plan_action a s"
+    apply (cases a)
+    using assms holds_for_wf_fmlas wf_domain
+    unfolding plan_action_enabled_def execute_plan_action_def
+      and execute_ground_action_def en_exE_def wf_domain_def
+    by (auto
+        split: option.splits
+        simp: resolve_action_schemaE_def return_iff)
+
+  text \<open>Next, we use the efficient implementation @{const is_obj_of_type_impl}
+    for the type check, and omit the well-formedness check, as effects obtained
+    from instantiating well-formed action schemas are always well-formed
+    (@{thm [source] wf_effect_inst_weak}).\<close>
+  abbreviation "action_params_match2 stg mp a args
+    \<equiv> list_all2 (is_obj_of_type_impl stg mp)
+        args (map snd (ast_action_schema.parameters a))"
+
+  definition en_exE2
+    :: "_ \<Rightarrow> (object, type) mapping \<Rightarrow> plan_action \<Rightarrow> world_model \<Rightarrow> _+world_model"
+  where
+    "en_exE2 G mp \<equiv> \<lambda>(PAction n args) \<Rightarrow> \<lambda>M. do {
+      a \<leftarrow> resolve_action_schemaE n;
+      check (action_params_match2 G mp a args) (ERRS ''Parameter mismatch'');
+      let ai = instantiate_action_schema a args;
+      check (holds M (precondition ai)) (ERRS ''Precondition not satisfied'');
+      Error_Monad.return (apply_effect (effect ai) M)
+    }"
+
+  text \<open>Justification of refinement\<close>
+  lemma (in wf_ast_problem) wf_en_exE2_eq:
+    shows "en_exE2 STG mp_objT pa s = en_exE pa s"
+    apply (cases pa; simp add: en_exE2_def en_exE_def Let_def)
+    apply (auto
+      simp: return_iff resolve_action_schemaE_def resolve_action_wf
+      simp: wf_effect_inst_weak action_params_match_def
+      split: error_monad_bind_split)
+    done
+
+  text \<open>Combination of the two refinement lemmas\<close>
+  lemma (in wf_ast_problem) en_exE2_return_iff:
+    assumes "wm_basic M"
+    shows "en_exE2 STG mp_objT a M = Inr M'
+      \<longleftrightarrow> plan_action_enabled a M \<and> M' = execute_plan_action a M"
+    unfolding wf_en_exE2_eq
+    apply (subst en_exE_return_iff)
+    using assms
+    by (auto)
+
+  lemma (in wf_ast_problem) en_exE2_return_iff_compact_notation:
+    "\<lbrakk>wm_basic s\<rbrakk> \<Longrightarrow>
+      en_exE2 STG mp_objT a s = Inr s'
+      \<longleftrightarrow> plan_action_enabled a s \<and> s' = execute_plan_action a s"
+    using en_exE2_return_iff .
+
+end \<comment> \<open>Context of \<open>ast_problem\<close>\<close>
+
+subsubsection \<open>Checking of Plan\<close>
+
+context ast_problem begin
+
+  text \<open>First, we combine the well-formedness check of the plan actions and
+    their execution into a single iteration.\<close>
+  fun valid_plan_from1 :: "world_model \<Rightarrow> plan \<Rightarrow> bool" where
+    "valid_plan_from1 s [] \<longleftrightarrow> close_world s \<TTurnstile> (goal P)"
+  | "valid_plan_from1 s (\<pi>#\<pi>s)
+      \<longleftrightarrow> plan_action_enabled \<pi> s
+        \<and> (valid_plan_from1 (execute_plan_action \<pi> s) \<pi>s)"
+
+  lemma valid_plan_from1_refine: "valid_plan_from s \<pi>s = valid_plan_from1 s \<pi>s"
+  proof(induction \<pi>s arbitrary: s)
+    case Nil
+    then show ?case by (auto simp add: plan_action_path_def valid_plan_from_def)
+  next
+    case (Cons a \<pi>s)
+    then show ?case
+      by (auto
+        simp: valid_plan_from_def plan_action_path_def plan_action_enabled_def
+        simp: execute_ground_action_def execute_plan_action_def)
+  qed
+
+  text \<open>Next, we use our efficient combined enabledness check and execution
+    function, and transfer the implementation to use the error monad: \<close>
+  fun valid_plan_fromE
+    :: "_ \<Rightarrow> (object, type) mapping \<Rightarrow> nat \<Rightarrow> world_model \<Rightarrow> plan \<Rightarrow> _+unit"
+  where
+    "valid_plan_fromE stg mp si s []
+      = check (holds s (goal P)) (ERRS ''Postcondition does not hold'')"
+  | "valid_plan_fromE stg mp si s (\<pi>#\<pi>s) = do {
+        s \<leftarrow> en_exE2 stg mp \<pi> s
+          <+? (\<lambda>e _. shows ''at step '' o shows si o shows '': '' o e ());
+        valid_plan_fromE stg mp (si+1) s \<pi>s
+      }"
+
+
+  text \<open>For the refinement, we need to show that the world models only
+    contain atoms, i.e., containing only atoms is an invariant under execution
+    of well-formed plan actions.\<close>
+  lemma (in wf_ast_problem) wf_actions_only_add_atoms:
+    "\<lbrakk> wm_basic s; wf_plan_action a \<rbrakk>
+      \<Longrightarrow> wm_basic (execute_plan_action a s)"
+    using wf_problem wf_domain
+    unfolding wf_problem_def wf_domain_def
+    apply (cases a)
+    apply (clarsimp
+      split: option.splits
+      simp: wf_fmla_atom_alt execute_plan_action_def wm_basic_def
+      simp: execute_ground_action_def)
+    subgoal for n args schema fmla
+      apply (cases "effect (instantiate_action_schema schema args)"; simp)
+      by (metis ground_action.sel(2) ast_domain.wf_effect.simps
+            ast_domain.wf_fmla_atom_alt resolve_action_wf
+            wf_ground_action.elims(2) wf_instantiate_action_schema)
+    done
+
+  text \<open>Refinement lemma for our plan checking algorithm\<close>
+  lemma (in wf_ast_problem) valid_plan_fromE_return_iff[return_iff]:
+    assumes "wm_basic s"
+    shows "valid_plan_fromE STG mp_objT k s \<pi>s = Inr () \<longleftrightarrow> valid_plan_from s \<pi>s"
+    using assms unfolding valid_plan_from1_refine
+  proof (induction stg\<equiv>STG mp\<equiv>mp_objT k s \<pi>s rule: valid_plan_fromE.induct)
+    case (1 si s)
+    then show ?case
+      using wf_problem holds_for_wf_fmlas
+      by (auto
+        simp: return_iff Let_def wf_en_exE2_eq wf_problem_def
+        split: plan_action.split)
+  next
+    case (2 si s \<pi> \<pi>s)
+    then show ?case
+      apply (clarsimp
+        simp: return_iff en_exE2_return_iff
+        split: plan_action.split)
+      by (meson ast_problem.plan_action_enabled_def wf_actions_only_add_atoms)
+  qed
+
+  lemmas valid_plan_fromE_return_iff'[return_iff]
+    = wf_ast_problem.valid_plan_fromE_return_iff[of P, OF wf_ast_problem.intro]
+
+  (* TODO: This function is unused! *)
+  (*fun apply_effect_exec''
+    :: "object atom ast_effect \<Rightarrow> world_model \<Rightarrow> world_model"
+    where
+    "apply_effect_exec'' (Effect (adds) (dels)) s
+      = fold (%add s. insert add s)
+          (map formula.Atom adds)
+          (fold (%del s. Set.remove del s) (map formula.Atom dels) s)"
+  *)
+
+
+end \<comment> \<open>Context of \<open>ast_problem\<close>\<close>
+
+subsection \<open>Executable Plan Checker\<close>
+text \<open>We obtain the main plan checker by combining the well-formedness check
+  and executability check. \<close>
+
+
+definition "check_all_list P l msg msgf \<equiv>
+  forallM (\<lambda>x. check (P x) (\<lambda>_::unit. shows msg o shows '': '' o msgf x) ) l <+? snd"
+
+lemma check_all_list_return_iff[return_iff]: "check_all_list P l msg msgf = Inr () \<longleftrightarrow> (\<forall>x\<in>set l. P x)"
+  unfolding check_all_list_def
+  by (induction l) (auto)
+
+definition "check_wf_types D \<equiv> do {
+  check_all_list (\<lambda>(_,t). t=''object'' \<or> t\<in>fst`set (types D)) (types D) ''Undeclared supertype'' (shows o snd)
+}"
+
+lemma check_wf_types_return_iff[return_iff]: "check_wf_types D = Inr () \<longleftrightarrow> ast_domain.wf_types D"
+  unfolding ast_domain.wf_types_def check_wf_types_def
+  by (force simp: return_iff)
+
+definition "check_wf_domain D stg conT \<equiv> do {
+  check_wf_types D;
+  check (distinct (map (predicate_decl.pred) (predicates D))) (ERRS ''Duplicate predicate declaration'');
+  check_all_list (ast_domain.wf_predicate_decl D) (predicates D) ''Malformed predicate declaration'' (shows o predicate.name o predicate_decl.pred);
+  check (distinct (map fst (consts D))) (ERRS  ''Duplicate constant declaration'');
+  check (\<forall>(n,T)\<in>set (consts D). ast_domain.wf_type D T) (ERRS ''Malformed type'');
+  check (distinct (map ast_action_schema.name (actions D))  ) (ERRS ''Duplicate action name'');
+  check_all_list (ast_domain.wf_action_schema' D stg conT) (actions D) ''Malformed action'' (shows o ast_action_schema.name)
+
+}"
+
+
+
+lemma check_wf_domain_return_iff[return_iff]:
+  "check_wf_domain D stg conT = Inr () \<longleftrightarrow> ast_domain.wf_domain' D stg conT"
+proof -
+  interpret ast_domain D .
+  show ?thesis
+    unfolding check_wf_domain_def wf_domain'_def
+    by (auto simp: return_iff)
+qed
+
+definition "prepend_err_msg msg e \<equiv> \<lambda>_::unit. shows msg o shows '': '' o e ()"
+
+definition "check_wf_problem P stg conT mp \<equiv> do {
+  let D = ast_problem.domain P;
+  check_wf_domain D stg conT <+? prepend_err_msg ''Domain not well-formed'';
+  check (distinct (map fst (objects P) @ map fst (consts D))) (ERRS ''Duplicate object declaration'');
+  check ((\<forall>(n,T)\<in>set (objects P). ast_domain.wf_type D T)) (ERRS ''Malformed type'');
+  check (distinct (init P)) (ERRS ''Duplicate fact in initial state'');
+  check (\<forall>f\<in>set (init P). ast_problem.wf_fmla_atom2' P mp stg f) (ERRS ''Malformed formula in initial state'');
+  check (ast_domain.wf_fmla' D (Mapping.lookup mp) stg (goal P)) (ERRS ''Malformed goal formula'')
+}"
+
+lemma check_wf_problem_return_iff[return_iff]:
+  "check_wf_problem P stg conT mp = Inr () \<longleftrightarrow> ast_problem.wf_problem' P stg conT mp"
+proof -
+  interpret ast_problem P .
+  show ?thesis
+    unfolding check_wf_problem_def wf_problem'_def
+    by (auto simp: return_iff)
+qed
+
+definition "check_plan P \<pi>s \<equiv> do {
+  let stg=ast_domain.STG (ast_problem.domain P);
+  let conT = ast_domain.mp_constT (ast_problem.domain P);
+  let mp = ast_problem.mp_objT P;
+  check_wf_problem P stg conT mp;
+  ast_problem.valid_plan_fromE P stg mp 1 (ast_problem.I P) \<pi>s
+} <+? (\<lambda>e. String.implode (e () ''''))"
+
+text \<open>Correctness theorem of the plan checker: It returns @{term "Inr ()"}
+  if and only if the problem is well-formed and the plan is valid.
+\<close>
+theorem check_plan_return_iff[return_iff]: "check_plan P \<pi>s = Inr ()
+  \<longleftrightarrow> ast_problem.wf_problem P \<and> ast_problem.valid_plan P \<pi>s"
+proof -
+  interpret ast_problem P .
+  show ?thesis
+    unfolding check_plan_def
+    by (auto
+      simp: return_iff wf_world_model_def wf_fmla_atom_alt I_def wf_problem_def isOK_iff
+      simp: wf_problem'_correct ast_problem.I_def ast_problem.valid_plan_def wm_basic_def
+      )
+qed
+
+
+
+subsection \<open>Code Setup\<close>
+
+text \<open>In this section, we set up the code generator to generate verified
+  code for our plan checker.\<close>
+
+subsubsection \<open>Code Equations\<close>
+text \<open>We first register the code equations for the functions of the checker.
+  Note that we not necessarily register the original code equations, but also
+  optimized ones.
+\<close>
+
+lemmas wf_domain_code =
+  ast_domain.sig_def
+  ast_domain.wf_types_def
+  ast_domain.wf_type.simps
+  ast_domain.wf_predicate_decl.simps
+  ast_domain.STG_def
+  ast_domain.is_of_type'_def
+  ast_domain.wf_atom'.simps
+  ast_domain.wf_pred_atom'.simps
+  ast_domain.wf_fmla'.simps
+  ast_domain.wf_fmla_atom1'.simps
+  ast_domain.wf_effect'.simps
+  ast_domain.wf_action_schema'.simps
+  ast_domain.wf_domain'_def
+  ast_domain.subst_term.simps
+  ast_domain.mp_constT_def
+(*
+  ast_domain.wf_domain_def
+  ast_domain.wf_action_schema.simps
+  ast_domain.wf_effect.simps
+  ast_domain.wf_fmla.simps
+  ast_domain.wf_atom.simps
+  ast_domain.is_of_type_def
+  ast_domain.of_type_code
+*)
+
+declare wf_domain_code[code]
+
+lemmas wf_problem_code =
+  ast_problem.wf_problem'_def
+  ast_problem.wf_fact'_def
+  (*ast_problem.objT_def*)
+  ast_problem.is_obj_of_type_alt
+  (*ast_problem.wf_object_def*)
+  ast_problem.wf_fact_def
+  ast_problem.wf_plan_action.simps
+  (*ast_problem.wf_effect_inst_weak.simps*)
+
+  ast_domain.subtype_edge.simps
+declare wf_problem_code[code]
+
+lemmas check_code =
+  ast_problem.valid_plan_def
+  ast_problem.valid_plan_fromE.simps
+  ast_problem.en_exE2_def
+  ast_problem.resolve_instantiate.simps
+  ast_domain.resolve_action_schema_def
+  ast_domain.resolve_action_schemaE_def
+  ast_problem.I_def
+  ast_domain.instantiate_action_schema.simps
+  ast_domain.apply_effect_exec.simps
+  (*ast_domain.apply_effect_exec'.simps*)
+  ast_domain.apply_effect_eq_impl_eq
+  (*ast_domain.apply_atomic.simps*)
+  ast_problem.holds_def
+  ast_problem.mp_objT_def
+  ast_problem.is_obj_of_type_impl_def
+  ast_problem.wf_fmla_atom2'_def
+  valuation_def
+declare check_code[code]
+
+subsubsection \<open>Setup for Containers Framework\<close>
+
+derive ceq predicate atom object formula
+derive ccompare predicate atom object formula
+derive (rbt) set_impl atom formula
+
+derive (rbt) mapping_impl object
+
+derive linorder predicate object atom "object atom formula"
+
+subsubsection \<open>More Efficient Distinctness Check for Linorders\<close>
+(* TODO: Can probably be optimized even more. *)
+fun no_stutter :: "'a list \<Rightarrow> bool" where
+  "no_stutter [] = True"
+| "no_stutter [_] = True"
+| "no_stutter (a#b#l) = (a\<noteq>b \<and> no_stutter (b#l))"
+
+lemma sorted_no_stutter_eq_distinct: "sorted l \<Longrightarrow> no_stutter l \<longleftrightarrow> distinct l"
+  apply (induction l rule: no_stutter.induct)
+  apply (auto simp: )
+  done
+
+definition distinct_ds :: "'a::linorder list \<Rightarrow> bool"
+  where "distinct_ds l \<equiv> no_stutter (quicksort l)"
+
+lemma [code_unfold]: "distinct = distinct_ds"
+  apply (intro ext)
+  unfolding distinct_ds_def
+  apply (auto simp: sorted_no_stutter_eq_distinct)
+  done
+
+subsubsection \<open>Code Generation\<close>
+
+(* TODO/FIXME: Code_Char was removed from Isabelle-2018! 
+  Check for performance regression of generated code!
+*)
+export_code
+  check_plan
+  nat_of_integer integer_of_nat Inl Inr
+  predAtm Eq predicate Pred Either Var Obj PredDecl BigAnd BigOr
+  formula.Not formula.Bot Effect ast_action_schema.Action_Schema
+  map_atom Domain Problem PAction
+  term.CONST term.VAR (* I want to export the entire type, but I can only export the constructor because term is already an isabelle keyword. *)
+  String.explode String.implode
+  in SML
+  module_name PDDL_Checker_Exported
+  file "code/PDDL_STRIPS_Checker_Exported.sml"
+
+export_code ast_domain.apply_effect_exec in SML module_name ast_domain
+
+(* Usage example from within Isabelle *)
+(*
+ML_val \<open>
+  let
+    val prefix="/home/lammich/devel/isabelle/planning/papers/pddl_validator/experiments/results/"
+
+    fun check name =
+      (name,@{code parse_check_dpp_impl}
+        (file_to_string (prefix ^ name ^ ".dom.pddl"))
+        (file_to_string (prefix ^ name ^ ".prob.pddl"))
+        (file_to_string (prefix ^ name ^ ".plan")))
+
+  in
+    (*check "IPC5_rovers_p03"*)
+    check "Test2_rover_untyped_pfile07"
+  end
+\<close>
+*)
 end \<comment> \<open>Theory\<close>
