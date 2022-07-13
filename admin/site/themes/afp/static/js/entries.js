3359e1d8a16c20f2947a6b2a18f92fbb39e60b04
Fabian Huch <huch@in.tum.de>
2022-07-13 09:57:38 +0200

    tuned;

--- admin/site/themes/afp/static/js/entries.js
+++ admin/site/themes/afp/static/js/entries.js
@@ -1,13 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const content = document.getElementById("copyText").innerHTML
-  const filename = document.getElementById("bibtexFileName").innerHTML
-  document.getElementById("copyBibtex").addEventListener("click", () => {
+/* constants */
+
+const ID_COPY_TEXT = 'copy-text'
+const ID_COPY_BIBTEX = 'copy-bibtex'
+const ID_BIBTEX_FILE = 'bibtex-filename'
+const ID_DOWNLOAD_BIBTEX = 'download-bibtex'
+
+
+/* setup */
+
+const init_entry = () => {
+  const content = document.getElementById(ID_COPY_TEXT).innerHTML
+  const filename = document.getElementById(ID_BIBTEX_FILE).innerHTML
+  document.getElementById(ID_COPY_BIBTEX).addEventListener('click', () => {
     navigator.clipboard.writeText(content)
   })
 
-  const a = document.getElementById("downloadBibtex")
+  const download_bibtex = document.getElementById(ID_DOWNLOAD_BIBTEX)
   const blob = new Blob([content], {type: "text/plain"})
   const url = URL.createObjectURL(blob)
-  a.setAttribute("href", url)
-  a.setAttribute("download", filename + ".bib")
-})
+  download_bibtex.setAttribute('href', url)
+  download_bibtex.setAttribute('download', filename + '.bib')
+}
+
+document.addEventListener('DOMContentLoaded', init_entry)
