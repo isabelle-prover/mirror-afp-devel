1a0773a0a82bc1352c5bf0dc65ea3996cb23cc74
Rene Thiemann <rene.thiemann@uibk.ac.at>
2023-07-31 20:46:00 +0200

    relation between has-maximum and bdd_above

--- thys/LP_Duality/Minimum_Maximum.thy
+++ thys/LP_Duality/Minimum_Maximum.thy
@@ -65,4 +65,16 @@ lemma Maximum_Max: assumes "finite S" "S \<noteq> {}"
   shows "Maximum S = Max S" 
   by (rule eqMaximumI, insert assms, auto)
 
+text \<open>For natural numbers, having a maximum is the same as being bounded from above and non-empty,
+ or being finite and non-empty.\<close>
+lemma has_Maximum_nat_iff_bdd_above: "has_Maximum (A :: nat set) \<longleftrightarrow> bdd_above A \<and> A \<noteq> {}" 
+  unfolding has_Maximum_def
+  by (metis bdd_above.I bdd_above_nat emptyE finite_has_maximal nat_le_linear)
+
+lemma has_Maximum_nat_iff_finite: "has_Maximum (A :: nat set) \<longleftrightarrow> finite A \<and> A \<noteq> {}" 
+  unfolding has_Maximum_nat_iff_bdd_above bdd_above_nat ..
+
+lemma bdd_above_Maximum_nat: "(x :: nat) \<in> A \<Longrightarrow> bdd_above A \<Longrightarrow> x \<le> Maximum A" 
+  by (rule has_MaximumD, auto simp: has_Maximum_nat_iff_bdd_above)
+
 end
