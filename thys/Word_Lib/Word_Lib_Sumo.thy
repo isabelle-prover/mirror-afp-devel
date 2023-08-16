00b0dadfaa5de8164e3b8a678e8fe73391b1334b
Gerwin Klein <kleing@unsw.edu.au>
2023-08-16 11:03:50 +0100

    Word_Lib: import new material from l4v

--- thys/Word_Lib/Word_Lib_Sumo.thy
+++ thys/Word_Lib/Word_Lib_Sumo.thy
@@ -34,6 +34,7 @@ imports
   Rsplit
   Signed_Words
   Syntax_Bundles
+  Sgn_Abs
   Typedef_Morphisms
   Type_Syntax
   Word_EqI
@@ -127,11 +128,5 @@ notation (input)
   bit ("testBit")
 
 lemmas cast_simps = cast_simps ucast_down_bl
-
-(* shadows the slightly weaker Word.nth_ucast *)
-lemma nth_ucast:
-  "(ucast (w::'a::len word)::'b::len word) !! n =
-   (w !! n \<and> n < min LENGTH('a) LENGTH('b))"
-  by (auto simp add: bit_simps not_le dest: bit_imp_le_length)
 
 end
