3b5f35cbac5c6b51d73aa8056a192c65a2b85cc2
wenzelm <unknown>
2023-08-29 17:33:15 +0200

    follow naming conventions of Isabelle/2a99fcb283ee;

--- tools/afp_tool.scala
+++ tools/afp_tool.scala
@@ -9,7 +9,7 @@ import afp.migration.{AFP_Migrate_Metadata, AFP_Obfuscate_Emails}
 class Admin_Tools extends Isabelle_Scala_Tools(
   AFP_Migrate_Metadata.isabelle_tool,
   AFP_Obfuscate_Emails.isabelle_tool,
-  AFP_Build_Hugo.isabelle_tool,
+  AFP_Component_Hugo.isabelle_tool,
 )
 
 class Tools extends Isabelle_Scala_Tools(
