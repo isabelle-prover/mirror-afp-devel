8a1edbaab6a597de69de66c9ffebe67111de318d
Fabian Huch <huch@in.tum.de>
2023-10-10 13:02:11 +0200

    moved hugo component build tool to Isabelle distribution;

--- tools/afp_tool.scala
+++ tools/afp_tool.scala
@@ -4,9 +4,6 @@ package afp
 import isabelle._
 
 
-class Admin_Tools extends Isabelle_Scala_Tools(
-  AFP_Component_Hugo.isabelle_tool)
-
 class Tools extends Isabelle_Scala_Tools(
   AFP_Site_Gen.isabelle_tool,
   AFP_Check_Roots.isabelle_tool,
