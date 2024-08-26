1ac3e937e0d9d35785ad6656f79e880db410ab34
nipkow <unknown>
2024-08-26 17:41:01 +0200

    tuned

--- thys/Amortized_Complexity/Pairing_Heap_Tree_Analysis2.thy
+++ thys/Amortized_Complexity/Pairing_Heap_Tree_Analysis2.thy
@@ -13,8 +13,7 @@ begin
 
 text
 \<open>Verification of logarithmic bounds on the amortized complexity of pairing heaps.
-As in \<^cite>\<open>"FredmanSST86" and "Brinkop"\<close>, except that the treatment of @{const pass\<^sub>1} is simplified.
-TODO: convert the other Pairing Heap analyses to this one.\<close>
+As in \<^cite>\<open>"FredmanSST86" and "Brinkop"\<close>, except that the treatment of @{const pass\<^sub>1} is simplified.\<close>
 
 
 subsubsection \<open>Analysis\<close>
