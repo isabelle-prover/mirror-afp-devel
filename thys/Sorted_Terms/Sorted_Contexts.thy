feeb15c4f8dae64fed66c6ddbcf08d59779599e8
Rene Thiemann <rene.thiemann@uibk.ac.at>
2024-09-25 14:38:55 +0900

    refactor sorted context

--- thys/Sorted_Terms/Sorted_Contexts.thy
+++ thys/Sorted_Terms/Sorted_Contexts.thy
@@ -6,6 +6,82 @@ theory Sorted_Contexts
     Sorted_Terms
 begin
 
+fun aContext where
+  "aContext F A (Hole,\<sigma>) = Some \<sigma>"
+| "aContext F A (More f ls C rs, \<sigma>) = do {
+    \<rho>s \<leftarrow> those (map A ls);
+    \<mu> \<leftarrow> aContext F A (C,\<sigma>);
+    \<nu>s \<leftarrow> those (map A rs);
+    F (f, \<rho>s @ \<mu> # \<nu>s)}"
+
+lemma Hole_hastype[simp]: "Hole : \<sigma> \<rightarrow> \<tau> in aContext F A \<longleftrightarrow> \<sigma> = \<tau>"
+  and More_hastype: "More f ls C rs : \<sigma> \<rightarrow> \<tau> in aContext F A \<longleftrightarrow> (\<exists>\<rho>s \<mu> \<nu>s.
+    f : \<rho>s @ \<mu> # \<nu>s \<rightarrow> \<tau> in F \<and>
+    ls :\<^sub>l \<rho>s in A \<and>
+    C : \<sigma> \<rightarrow> \<mu> in aContext F A \<and>
+    rs :\<^sub>l \<nu>s in A)"
+   by (auto simp: hastype_list_iff_those bind_eq_Some_conv fun_hastype_def
+       intro!: hastypeI)
+
+lemma More_hastypeI:
+  assumes "f : \<rho>s @ \<mu> # \<nu>s \<rightarrow> \<tau> in F"
+    and "ls :\<^sub>l \<rho>s in A"
+    and "C : \<sigma> \<rightarrow> \<mu> in aContext F A"
+    and "rs :\<^sub>l \<nu>s in A"
+  shows "More f ls C rs : \<sigma> \<rightarrow> \<tau> in aContext F A"
+  using assms by (auto simp: More_hastype)
+
+lemma hastype_aContext_induct[consumes 1, case_names Hole More]:
+  assumes C: "C : \<sigma> \<rightarrow> \<tau> in aContext F A"
+    and hole: "P \<box> \<sigma>"
+    and more: "\<And>f \<mu>s \<rho> \<nu>s \<tau> ls C rs.
+      f : \<mu>s @ \<rho> # \<nu>s \<rightarrow> \<tau> in F \<Longrightarrow>
+      ls :\<^sub>l \<mu>s in A \<Longrightarrow>
+      C : \<sigma> \<rightarrow> \<rho> in aContext F A \<Longrightarrow>
+      P C \<rho> \<Longrightarrow>
+      rs :\<^sub>l \<nu>s in A \<Longrightarrow>
+      P (More f ls C rs) \<tau>"
+  shows "P C \<tau>"
+  using C
+proof (induct C arbitrary: \<tau>)
+  case Hole
+  with hole show ?case by auto
+next
+  case (More f ls C rs)
+  from \<open>More f ls C rs : \<sigma> \<rightarrow> \<tau> in aContext F A\<close>
+    [unfolded More_hastype]
+  obtain \<rho>s \<mu> \<nu>s
+    where f: "f : \<rho>s @ \<mu> # \<nu>s \<rightarrow> \<tau> in F"
+      and ls: "ls :\<^sub>l \<rho>s in A"
+      and C: "C : \<sigma> \<rightarrow> \<mu> in aContext F A"
+      and rs: "rs :\<^sub>l \<nu>s in A" by auto
+  show ?case
+    using More(1)[OF C] more[OF f ls C _ rs]
+    by (auto simp: bind_eq_Some_conv)
+qed
+
+
+context sorted_algebra begin
+
+lemma intp_ctxt_hastype:
+  assumes C: "C : \<sigma> \<rightarrow> \<tau> in aContext F A" and a: "a : \<sigma> in A"
+  shows "I\<langle>C;a\<rangle> : \<tau> in A"
+  using C
+proof (induct arbitrary: \<tau>)
+  case Hole
+  with a show ?case by simp
+next
+  case (More f ls C rs)
+  then show ?case by (auto intro!: sort_matches list_all2_appendI simp: More_hastype)
+qed
+
+lemma ctxt_has_same_type:
+  assumes C: "C : \<sigma> \<rightarrow> \<tau> in aContext F A" and "a : \<sigma> in A"
+  shows "I\<langle>C;a\<rangle> : \<tau>' in A \<longleftrightarrow> \<tau>' = \<tau>"
+  using assms by (auto simp: has_same_type intp_ctxt_hastype)
+
+end
+
 lemma subt_in_dom:
   assumes s: "s \<in> dom \<T>(F,V)" and st: "s \<unrhd> t" shows "t \<in> dom \<T>(F,V)"
   using st s
@@ -18,42 +94,20 @@ next
   show ?case by auto
 qed
 
+text \<open>Term contexts are abstract contexts in the term algebra.\<close>
 
-(* sorted contexts *)
+abbreviation Context ("(2\<C>'(_,/_'))" [1,1]50) where
+  "\<C>(F,V) \<equiv> aContext F \<T>(F,V)"
 
-inductive has_type_context :: "('f,'s) ssig \<Rightarrow> ('v \<rightharpoonup> 's) \<Rightarrow> 's \<Rightarrow> ('f,'v)ctxt \<Rightarrow> 's \<Rightarrow> bool"
-   for F V \<sigma> where
-  Hole: "has_type_context F V \<sigma> Hole \<sigma>"
-| More: "f : \<sigma>b @ \<rho> # \<sigma>a \<rightarrow> \<tau> in F \<Longrightarrow>
-  bef :\<^sub>l \<sigma>b in \<T>(F,V) \<Longrightarrow> has_type_context F V \<sigma> C \<rho> \<Longrightarrow> aft :\<^sub>l \<sigma>a in \<T>(F,V) \<Longrightarrow>
-  has_type_context F V \<sigma> (More f bef C aft) \<tau>" 
-
-hide_fact (open) Hole More
-
-abbreviation has_type_context' (\<open>((_) :\<^sub>c/ (_) \<rightarrow> (_) in/ \<T>'(_,_'))\<close> [50,61,51,51,51]50)
-  where "C :\<^sub>c \<sigma> \<rightarrow> \<tau> in \<T>(F,V) \<equiv> has_type_context F V \<sigma> C \<tau>"
-
-lemma hastype_context_apply:
-  assumes "C :\<^sub>c \<sigma> \<rightarrow> \<tau> in \<T>(F,V)" and "t : \<sigma> in \<T>(F,V)" 
-  shows "C\<langle>t\<rangle> : \<tau> in \<T>(F,V)"
-  using assms
-proof induct
-  case (More f \<sigma>b \<rho> \<sigma>a \<tau> bef C aft)
-  show ?case unfolding intp_actxt.simps
-  proof (intro Fun_hastypeI[OF More(1)])
-    show "bef @ C\<langle>t\<rangle> # aft :\<^sub>l \<sigma>b @ \<rho> # \<sigma>a in \<T>(F,V)" 
-      using More(2,5) More(4)[OF More(6)]
-      by (simp add: list_all2_appendI)
-  qed
-qed auto
+lemmas hastype_context_apply = term.intp_ctxt_hastype
 
 lemma hastype_context_decompose:
   assumes "C\<langle>t\<rangle> : \<tau> in \<T>(F,V)"
-  shows "\<exists> \<sigma>. C :\<^sub>c \<sigma> \<rightarrow> \<tau> in \<T>(F,V) \<and> t : \<sigma> in \<T>(F,V)"
+  shows "\<exists> \<sigma>. C : \<sigma> \<rightarrow> \<tau> in \<C>(F,V) \<and> t : \<sigma> in \<T>(F,V)"
   using assms
 proof (induct C arbitrary: \<tau>)
   case Hole
-  then show ?case by (auto intro: has_type_context.Hole)
+  then show ?case by auto
 next
   case (More f bef C aft \<tau>)
   from More(2) have "Fun f (bef @ C\<langle>t\<rangle> # aft) : \<tau> in \<T>(F,V)" by auto
@@ -72,10 +126,10 @@ next
     by (metis (no_types, lifting) append_eq_conv_conj list_all2_append1)
   from list have aft: "aft :\<^sub>l drop (Suc ?i) \<sigma>s in \<T>(F,V)"
     by (metis (no_types, lifting) Cons_nth_drop_Suc append_eq_conv_conj drop_all length_greater_0_conv linorder_le_less_linear list.rel_inject(2) list.simps(3) list_all2_append1)
-  from More(1)[OF Ct] obtain \<sigma> where C: "C :\<^sub>c \<sigma> \<rightarrow> \<sigma>s ! ?i in \<T>(F,V)" and t: "t : \<sigma> in \<T>(F,V)" 
+  from More(1)[OF Ct] obtain \<sigma> where C: "C : \<sigma> \<rightarrow> \<sigma>s ! ?i in \<C>(F,V)" and t: "t : \<sigma> in \<T>(F,V)" 
     by auto
   show ?case 
-    by (intro exI[of _ \<sigma>] conjI has_type_context.More[OF _ bef _ aft, of _ "\<sigma>s ! ?i"] C t, unfold id, rule f)
+    by (intro exI[of _ \<sigma>] conjI More_hastypeI[OF _ bef _ aft, of _ "\<sigma>s ! ?i"] C t, unfold id, rule f)
 qed
 
 lemma apply_ctxt_in_dom_imp_in_dom:
@@ -85,24 +139,24 @@ lemma apply_ctxt_in_dom_imp_in_dom:
 
 lemma apply_ctxt_hastype_imp_hastype_context:
   assumes C: "C\<langle>t\<rangle> : \<tau> in \<T>(F,V)" and t: "t : \<sigma> in \<T>(F,V)"
-  shows "C :\<^sub>c \<sigma> \<rightarrow> \<tau> in \<T>(F,V)"
+  shows "C : \<sigma> \<rightarrow> \<tau> in \<C>(F,V)"
   using hastype_context_decompose[OF C] t by (auto simp: has_same_type)
 
 lemma subst_apply_ctxt_sorted:
-  assumes "C :\<^sub>c \<sigma> \<rightarrow> \<tau> in \<T>(F,X)" and "\<theta> :\<^sub>s X \<rightarrow> \<T>(F,V)"
-  shows "C \<cdot>\<^sub>c \<theta> :\<^sub>c \<sigma> \<rightarrow> \<tau> in \<T>(F,V)"
+  assumes "C : \<sigma> \<rightarrow> \<tau> in \<C>(F,X)" and "\<theta> :\<^sub>s X \<rightarrow> \<T>(F,V)"
+  shows "C \<cdot>\<^sub>c \<theta> : \<sigma> \<rightarrow> \<tau> in \<C>(F,V)"
   using assms
-proof(induct arbitrary: \<theta> rule: has_type_context.induct)
+proof(induct arbitrary: \<theta> rule: hastype_aContext_induct)
   case (Hole)
-  then show ?case by (simp add: has_type_context.Hole)
+  then show ?case by simp
 next
   case (More f \<sigma>b \<rho> \<sigma>a \<tau> bef C aft)
   have fssig: "f : \<sigma>b @ \<rho> # \<sigma>a \<rightarrow> \<tau> in F" using More(1) .
   have bef:"bef :\<^sub>l \<sigma>b in \<T>(F,X)" using More(2) .
-  have Cssig:"C :\<^sub>c \<sigma> \<rightarrow> \<rho> in \<T>(F,X)" using More(3) .
+  have Cssig:"C : \<sigma> \<rightarrow> \<rho> in \<C>(F,X)" using More(3) .
   have aft:"aft :\<^sub>l \<sigma>a in \<T>(F,X)" using More(5) .
   have theta:"\<theta> :\<^sub>s X \<rightarrow> \<T>(F,V)" using More(6) .
-  hence ctheta:"C \<cdot>\<^sub>c \<theta> :\<^sub>c \<sigma> \<rightarrow> \<rho> in \<T>(F,V)" using More(4) by simp
+  hence ctheta:"C \<cdot>\<^sub>c \<theta> : \<sigma> \<rightarrow> \<rho> in \<C>(F,V)" using More(4) by simp
   have len_bef:"length bef = length \<sigma>b" using bef list_all2_iff by blast
   have len_aft:"length aft = length \<sigma>a" using aft list_all2_iff by blast
   { fix i
@@ -129,8 +183,8 @@ next
   } note ** = this
   have ma: "map (\<lambda>t. t \<cdot> \<theta>) aft :\<^sub>l \<sigma>a in \<T>(F,V)" using length_map
     by (auto simp:** theta aft list_all2_conv_all_nth len_aft)
-  show "More f bef C aft \<cdot>\<^sub>c \<theta> :\<^sub>c \<sigma> \<rightarrow> \<tau> in \<T>(F,V)"
-    by (auto intro!: has_type_context.intros fssig simp:ctheta mb ma)
+  show "More f bef C aft \<cdot>\<^sub>c \<theta> : \<sigma> \<rightarrow> \<tau> in \<C>(F,V)"
+    by (auto intro!: More_hastypeI fssig simp:ctheta mb ma)
 qed
 
 end
