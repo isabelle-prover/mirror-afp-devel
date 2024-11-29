5ea2859f105f6a758c818da8f0228a0c65385d49
wenzelm <unknown>
2024-11-30 00:02:48 +0100

    clarified signature, following Isabelle/08574da77b4a;

--- thys/AOT/AOT_syntax.thy
+++ thys/AOT/AOT_syntax.thy
@@ -584,7 +584,7 @@ AOT_syntax_print_translations [
   fn [lhs as Abs (lhsName, lhsTy, lhsTrm), rhs as Abs (rhsName, rhsTy, rhsTrm)] =>
     let
       val (name,_) = Name.variant lhsName
-        (Term.declare_term_names rhsTrm (Term.declare_term_names lhsTrm Name.context));
+        (Term.declare_term_names rhsTrm (Name.build_context (Term.declare_term_names lhsTrm)));
       val lhs = Term.betapply (lhs, Const ("_bound", dummyT) $ Free (name, lhsTy))
       val rhs = Term.betapply (rhs, Const ("_bound", dummyT) $ Free (name, rhsTy))
     in
