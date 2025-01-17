35b37316aa571145ff6046bec854de92a776d8f6
nipkow <unknown>
2025-01-17 17:22:09 +0100

    New entry First_Order_Clause

---
+++ thys/First_Order_Clause/Ground_Clause.thy
@@ -0,0 +1,11 @@
+theory Ground_Clause
+  imports
+    Saturation_Framework_Extensions.Clausal_Calculus
+    Ground_Term_Extra
+    Ground_Context
+    Uprod_Extra
+begin
+
+type_synonym 'f gatom = "'f gterm uprod"
+
+end
