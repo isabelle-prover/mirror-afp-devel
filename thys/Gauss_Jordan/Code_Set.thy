9bc2e99e73b9095155f3c5a77a5739e0ac66a5c1
haftmann <unknown>
2025-05-06 07:12:08 +0200

    less invasive custom code setup

--- thys/Gauss_Jordan/Code_Set.thy
+++ thys/Gauss_Jordan/Code_Set.thy
@@ -11,32 +11,12 @@ imports
   "HOL-Library.Code_Cardinality"
 begin
 
-text\<open>The following setup could help to get code generation for List.coset, 
-  but it neither works correctly it complains that code
-  equations for remove are missed, even when List.coset should be rewritten
-  to an enum:\<close>
+lemma [code]:
+  \<open>UNIV = set Enum.enum\<close>
+  by (fact UNIV_enum)
 
-declare minus_coset_filter [code del]
-declare remove_code(2) [code del]
-declare insert_code(2) [code del]
-declare inter_coset_fold [code del]
-declare compl_coset[code del]
-declare Code_Cardinality.card'_code(2)[code del]
-
-code_datatype set
-
-text\<open>The following code equation could be useful to avoid the problem of
-  code generation for List.coset []:\<close>
-
-lemma [code]: "List.coset (l::'a::enum list) = set (enum_class.enum) - set l"
-  by (metis Compl_eq_Diff_UNIV coset_def enum_UNIV)
-
-text\<open>Now the following examples work:\<close>
-
-value "UNIV::bool set"
-value "List.coset ([]::bool list)"
-value "UNIV::Enum.finite_2 set"
-value "List.coset ([]::Enum.finite_2 list)"
-value "List.coset ([]::Enum.finite_5 list)"
+lemma [code_unfold]:
+  \<open>- (Collect P) = Set.filter (Not \<circ> P) UNIV\<close>
+  by auto
 
 end
