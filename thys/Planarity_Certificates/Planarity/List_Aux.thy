1ff8e71518fac9d2aea6f4054c45b5027cac39ef
nipkow <unknown>
2025-06-14 11:51:42 +0200

    used new minus_list functions and lemmas

--- thys/Planarity_Certificates/Planarity/List_Aux.thy
+++ thys/Planarity_Certificates/Planarity/List_Aux.thy
@@ -44,34 +44,4 @@ next
   qed
 qed
 
-lemma not_nil_if_in_set:
-  assumes "x \<in> set xs" shows "xs \<noteq> []"
-  using assms by auto
-
-lemma length_fold_remove1_le:
- "length (fold remove1 ys xs) \<le> length xs"
-proof (induct ys arbitrary: xs)
-  case (Cons y ys)
-  then have "length (fold remove1 ys (remove1 y xs)) \<le> length (remove1 y xs)" by auto
-  also have "\<dots> \<le> length xs" by (auto simp: length_remove1)
-  finally show ?case by simp
-qed simp
-
-lemma set_fold_remove1':
-  assumes "x \<in> set xs - set ys" shows "x \<in> set (fold remove1 ys xs)"
-  using assms by (induct ys arbitrary: xs) auto
-
-lemma set_fold_remove1:
-  "set (fold remove1 xs ys) \<subseteq> set ys"
-  by (induct xs arbitrary: ys) (auto, metis notin_set_remove1 subsetCE)
-
-lemma set_fold_remove1_distinct:
-  assumes "distinct xs" shows "set (fold remove1 ys xs) = set xs - set ys"
-  using assms by (induct ys arbitrary: xs) auto
-
-lemma distinct_fold_remove1:
-  assumes "distinct xs"
-  shows "distinct (fold remove1 ys xs)"
-  using assms by (induct ys arbitrary: xs) auto
-
 end
