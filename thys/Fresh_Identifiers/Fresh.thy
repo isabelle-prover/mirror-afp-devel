ffeea70f38ced8e5439ea01c92bc260fa67256ec
nipkow <unknown>
2025-08-27 13:36:08 +0200

    added function freshs

--- thys/Fresh_Identifiers/Fresh.thy
+++ thys/Fresh_Identifiers/Fresh.thy
@@ -4,12 +4,11 @@ theory Fresh
   imports Main
 begin
 
-text \<open>A type in this class comes with a mechanism to generate fresh
-items. The fresh operator takes a list of items to be avoided, \<open>xs\<close>, and
+text \<open>A type in this class comes with a mechanism to generate fresh elements.
+The \<open>fresh\<close> operator takes a set of items to be avoided, \<open>xs\<close>, and
 a preferred element to be generated, \<open>x\<close>.
 
-It is required that
-implementations of fresh for specific types produce \<open>x\<close> if possible
+It is required that implementations of fresh for specific types produce \<open>x\<close> if possible
 (i.e., if not in \<open>xs\<close>).
 
 While not required, it is also expected that, if \<open>x\<close> is not possible,
@@ -21,7 +20,15 @@ class fresh =
   fixes fresh :: "'a set \<Rightarrow> 'a \<Rightarrow> 'a"
   assumes fresh_notIn: "finite F \<Longrightarrow> fresh F x \<notin> F"
   and fresh_eq: "x \<notin> A \<Longrightarrow> fresh A x = x"
+begin
 
+text \<open>Generate a list of fresh names \<open>[a', b', c', \<dots>]\<close> given a list \<open>[a, b, c, \<dots>]\<close>:\<close>
+
+fun freshs :: "'a set \<Rightarrow> 'a list \<Rightarrow> 'a list" where
+"freshs X [] = []" |
+"freshs X (a#as) = (let a' = fresh X a in a' # freshs (insert a' X) as)"
+
+end
 
 text \<open>The type class \<^class>\<open>fresh\<close> is essentially the same as
 the type class \<open>infinite\<close> but with an emphasis on fresh item generation.\<close>
