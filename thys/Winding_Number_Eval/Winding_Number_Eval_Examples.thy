9f2b3652b17443836ba7c6136ff43d4e8d429cdd
paulson <lp15@cam.ac.uk>
2025-09-06 12:53:55 +0100

    tidying

--- thys/Winding_Number_Eval/Winding_Number_Eval_Examples.thy
+++ thys/Winding_Number_Eval/Winding_Number_Eval_Examples.thy
@@ -4,7 +4,9 @@
 
 section \<open>Some examples of applying the method winding\_eval\<close>
 
-theory Winding_Number_Eval_Examples imports Winding_Number_Eval
+theory Winding_Number_Eval_Examples 
+  imports Winding_Number_Eval
+
 begin
   
 lemma example1:
@@ -20,9 +22,8 @@ proof (eval_winding,simp_all)
   from cindex_pathE_linepath[OF this] have "cindex_pathE L \<i> = -1"
     unfolding L_def using \<open>R>1\<close> by auto
   moreover have "cindex_pathE CR \<i> = -1"
-    unfolding CR_def using \<open>R>1\<close> 
-    apply (subst cindex_pathE_part_circlepath)
-    by (simp_all add:jumpF_pathstart_part_circlepath jumpF_pathfinish_part_circlepath)
+    unfolding CR_def using \<open>R>1\<close>
+    by (simp add: cindex_pathE_circlepath_upper(1)) 
   ultimately show "- complex_of_real (cindex_pathE CR \<i>) - cindex_pathE L \<i> = 2"
     unfolding L_def CR_def by auto
 qed
@@ -40,9 +41,8 @@ proof (eval_winding,simp_all)
   from cindex_pathE_linepath[OF this] have "cindex_pathE L (-\<i>) = 1"
     unfolding L_def using \<open>R>1\<close> by auto
   moreover have "cindex_pathE CR (-\<i>) = -1"
-    unfolding CR_def using \<open>R>1\<close> 
-    apply (subst cindex_pathE_part_circlepath)
-    by (simp_all add:jumpF_pathstart_part_circlepath jumpF_pathfinish_part_circlepath)
+    unfolding CR_def using \<open>R>1\<close>
+    by (simp add: cindex_pathE_circlepath_upper(1)) 
   ultimately show "-cindex_pathE CR (-\<i>) = cindex_pathE L (-\<i>)"
     unfolding L_def CR_def by auto    
 qed
@@ -59,37 +59,29 @@ proof (eval_winding)
   and ?l2 = "linepath (Complex (Re ub) (Im lb)) ub"
   and ?l3 = "linepath ub (Complex (Re lb) (Im ub))"
   and ?l4 = "linepath (Complex (Re lb) (Im ub)) lb"
-  show l1: "z \<notin> path_image ?l1" 
-    apply (auto intro!: not_on_closed_segmentI_complex)
-    using order_asms by (simp add: algebra_simps crossproduct_eq)
-  show l2:"z \<notin> path_image ?l2" 
-    apply (auto intro!: not_on_closed_segmentI_complex)      
-    using order_asms by (simp add: algebra_simps crossproduct_eq)
-  show l3:"z \<notin> path_image ?l3" 
-    apply (auto intro!: not_on_closed_segmentI_complex)      
-    using order_asms by (simp add: algebra_simps crossproduct_eq)
-  show l4:"z \<notin> path_image ?l4"  
-    apply (auto intro!: not_on_closed_segmentI_complex)      
-    using order_asms by (simp add: algebra_simps crossproduct_eq)
+  show l1: "z \<notin> path_image ?l1"
+    using closed_segment_same_Im order_asms(3) by fastforce 
+  show l2:"z \<notin> path_image ?l2"
+    using closed_segment_same_Re order_asms(2) by auto 
+  show l3:"z \<notin> path_image ?l3"
+    using closed_segment_same_Im order_asms(4) by auto 
+  show l4:"z \<notin> path_image ?l4"
+    using closed_segment_same_Re order_asms(1) by auto
   show "- complex_of_real (cindex_pathE ?l1 z + (cindex_pathE ?l2 z + (cindex_pathE ?l3 z +
           cindex_pathE ?l4 z))) = 2 * 1"  
   proof -
     have "(Im z - Im ub) * (Re ub - Re lb) < 0" 
       using mult_less_0_iff order_asms(1) order_asms(2) order_asms(4) by fastforce
     then have "cindex_pathE ?l3 z = -1"
-      apply (subst cindex_pathE_linepath)
-      using l3 order_asms by (auto simp add:algebra_simps)
+      using l3 order_asms by (simp add: cindex_pathE_linepath algebra_simps)
     moreover have "(Im lb - Im z) * (Re ub - Re lb) <0" 
       using mult_less_0_iff order_asms(1) order_asms(2) order_asms(3) by fastforce
     then have "cindex_pathE ?l1 z = -1"    
-      apply (subst cindex_pathE_linepath)
-      using l1 order_asms by (auto simp add:algebra_simps)  
+      using l1 order_asms by (simp add: cindex_pathE_linepath algebra_simps)  
     moreover have "cindex_pathE ?l2 z = 0"    
-      apply (subst cindex_pathE_linepath)
-      using l2 order_asms by (auto simp add:algebra_simps)  
+      using l2 order_asms by (simp add: cindex_pathE_linepath algebra_simps)  
     moreover have "cindex_pathE ?l4 z = 0"
-      apply (subst cindex_pathE_linepath)
-      using l4 order_asms by (auto simp add:algebra_simps) 
+      using l4 order_asms by (simp add: cindex_pathE_linepath algebra_simps)
     ultimately show ?thesis by auto
   qed
 qed
