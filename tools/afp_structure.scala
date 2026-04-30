252fd5936005b07d7f86b195aed43b3496b2accf
Fabian Huch <huch@in.tum.de>
2026-04-30 18:40:36 +0200

    clarified: add operations;

--- tools/afp_structure.scala
+++ tools/afp_structure.scala
@@ -16,6 +16,24 @@ object AFP_Structure {
   def entry_sessions(name: String): List[Sessions.Session_Entry] =
     Sessions.parse_root_entries(entry_dir(name) + Sessions.ROOT)
 
+  class Entry private[AFP_Structure](
+    val name: String,
+    val metadata: Metadata.Entry,
+    val sessions: List[Sessions.Info],
+  ) {
+    override def toString: String = name
+
+    def entry_dir: Path = AFP_Structure.entry_dir(name)
+    def is_proper: Boolean = !metadata.statistics_ignore
+
+    override def hashCode(): Int = name.hashCode
+    override def equals(that: Any): Boolean =
+      that match {
+        case other: Entry => other.name == name
+        case _ => false
+      }
+  }
+
   def roots_entries: List[String] = Sessions.parse_roots(thys_dir + Sessions.ROOTS)
 
   def sessions(options: Options = Options.init()): Sessions.Structure =
@@ -51,7 +69,31 @@ object AFP_Structure {
       Metadata.Entries(entry_names.map(name =>
         Metadata.files.load_entry(name, authors, topics, licenses, releases)))
 
-    new AFP(authors, topics, licenses, releases, entries, sessions())
+    val sessions_structure = sessions()
+
+    def load_entry(name: String): Entry = {
+      val sessions = for (session <- entry_sessions(name)) yield sessions_structure(session.name)
+      Entry(name, entries(name), sessions)
+    }
+
+    val entry_list = entry_names.map(load_entry)
+
+    val session_entry = entry_list.flatMap(entry => entry.sessions.map(_.name -> entry)).toMap
+    def entry_deps(entry: Entry): List[Entry] =
+      (for {
+        session <- entry.sessions
+        dep <- session.deps
+        dep_entry <- session_entry.get(dep)
+        if dep_entry != entry
+      } yield dep_entry).distinct
+
+    val uses_graph =
+      Graph.make(
+        entry_list.map(entry => ((entry.name, entry), entry_deps(entry).map(_.name))),
+        converse = true)
+
+    new AFP(authors, topics, licenses, releases, entries, entry_list, uses_graph,
+      sessions_structure)
   }
 
   class AFP private[AFP_Structure](
@@ -60,5 +102,17 @@ object AFP_Structure {
     val licenses: Metadata.Licenses,
     val releases: Metadata.Releases,
     val entries: Metadata.Entries,
-    val sessions_structure: Sessions.Structure)
+    val entry_list: List[Entry],
+    val uses_graph: Graph[String, Entry],
+    val sessions_structure: Sessions.Structure
+  ) {
+    def root_topics: List[Metadata.Topic] = Metadata.Topics.root_topics(topics)
+
+    def the_entry(name: String): Entry =
+      entry_list.find(_.name == name) getOrElse error("No such entry: " + name)
+
+    def sessions: List[Sessions.Info] = entry_list.flatMap(_.sessions)
+    def perhaps_session_entry(session: String): Option[Entry] =
+      entry_list.find(_.sessions.exists(_.name == session))
+  }
 }
