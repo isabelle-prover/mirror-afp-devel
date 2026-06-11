3dc275e66fd08f8b38e2c1f877e41b098de6972e
Fabian Huch <fabian@huch.de>
2026-06-11 16:09:14 +0200

    clarified: more operations;

--- tools/afp_system.scala
+++ tools/afp_system.scala
@@ -12,4 +12,7 @@ object AFP_System {
   def hg_id: String =
     if (Mercurial.Hg_Sync.ok(AFP.BASE)) File.read(AFP.BASE + Mercurial.Hg_Sync.PATH_ID)
     else Mercurial.repository(AFP.BASE).id()
+
+  def afp_version: String = Isabelle_System.getenv_strict("AFP_VERSION")
+  def afp_name: String = "afp-" + afp_version
 }
