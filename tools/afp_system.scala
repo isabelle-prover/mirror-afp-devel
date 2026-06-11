875c9c505d423b15f88b1a570da9c98ee46aaaad
Fabian Huch <fabian@huch.de>
2026-06-11 16:40:27 +0200

    clarified: more operations;

--- tools/afp_system.scala
+++ tools/afp_system.scala
@@ -9,9 +9,11 @@ import isabelle.*
 
 
 object AFP_System {
+  def repository(): Mercurial.Repository = Mercurial.repository(AFP.BASE)
+
   def hg_id: String =
     if (Mercurial.Hg_Sync.ok(AFP.BASE)) File.read(AFP.BASE + Mercurial.Hg_Sync.PATH_ID)
-    else Mercurial.repository(AFP.BASE).id()
+    else repository().id()
 
   def afp_version: String = Isabelle_System.getenv_strict("AFP_VERSION")
   def afp_name: String = "afp-" + afp_version
