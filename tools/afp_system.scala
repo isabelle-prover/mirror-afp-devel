b3fbf1f223b634e925d671d53f4dd58cfb831184
Fabian Huch <fabian@huch.de>
2026-06-18 10:23:21 +0200

    clarified: more operations;

--- tools/afp_system.scala
+++ tools/afp_system.scala
@@ -17,4 +17,7 @@ object AFP_System {
 
   def afp_version: String = Isabelle_System.getenv_strict("AFP_VERSION")
   def afp_name: String = "afp-" + afp_version
+
+  def AFP_BUILD_OPTIONS: List[Options.Spec] =
+    Word.explode(Isabelle_System.getenv("AFP_BUILD_OPTIONS")).map(Options.Spec.make)
 }
