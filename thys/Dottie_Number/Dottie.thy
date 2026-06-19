8f143303e1cbddaa2fb9986beb8739befab1823b
paulson <lp15@cam.ac.uk>
2026-06-19 12:17:16 +0100

    Tidied the proofs

--- thys/Dottie_Number/Dottie.thy
+++ thys/Dottie_Number/Dottie.thy
@@ -125,12 +125,10 @@ definition lb::real where "lb \<equiv> 0.739085133215"
 definition ub::real where "ub \<equiv> 0.739085133216"
 
 lemma lb_gt: "cos lb > lb"
-  unfolding lb_def
-  by (approximation 50)
+  unfolding lb_def by (approximation 50)
 
 lemma ub_lt: "cos ub < ub"
-  unfolding ub_def
-  by (approximation 50)
+  unfolding ub_def by (approximation 50)
 
 lemma lb: "lb < dottie"
 proof (rule ccontr)
@@ -139,7 +137,7 @@ proof (rule ccontr)
     using facts lb_gt by (auto simp: g_def)
   show False
     using DERIV_neg_imp_decreasing [OF _ g_has_negative_deriv] facts neg
-    by (smt (verit, ccfv_SIG) cos_le_one cos_monotone_0_pi lb_gt pi_gt3)
+    by (smt (verit, best) cos_le_one g_def lb_gt)
 qed
 
 lemma ub: "ub > dottie"
@@ -149,8 +147,20 @@ proof (rule ccontr)
     using facts ub_lt by (auto simp: g_def)
   show False
     using DERIV_neg_imp_decreasing [OF _ g_has_negative_deriv] facts neg
-    by (smt (verit) cos_ge_minus_one ub_lt gd g_def)
+    by (smt (verit) abs_cos_le_one g_def ub_lt)
 qed
+
+end
+
+subsection \<open>A trigonometric identity\<close>
+
+text \<open>Now for something trivial. 
+Since $\cos(\mathit{dottie}) = \mathit{dottie}$ and $\mathit{dottie} \in (0,1)$, the
+  Pythagorean identity gives $\sin(\mathit{dottie}) = \sqrt{1 - \mathit{dottie}^2}$.
+Sledgehammer found this proof.\<close>
+
+lemma sin_dottie: "sin dottie = sqrt (1 - dottie\<^sup>2)"
+  by (smt (verit) Dottie.facts pi_gt3 sin_cos_sqrt sin_ge_zero)
 
 subsection \<open>The Dottie number is a universal attractor\<close>
 
@@ -160,15 +170,7 @@ text \<open>Iterating cosine from \emph{any} real starting point converges to th
   and that $\cos$ maps all of $\mathbb{R}$ into $[-1,1]$.\<close>
 
 lemma sin1_bounds: "0 < sin (1::real)" "sin (1::real) < 1"
-proof -
-  have lt: "(1::real) < pi" using pi_gt3 by simp
-  have "0 < (1::real)" by simp
-  from sin_gt_zero[OF this lt] show "0 < sin (1::real)" .
-next
-  have "sin 1 < sin (pi/2)"
-    using sin_monotone_2pi[of 1 "pi/2"] pi_gt3 by simp
-  then show "sin (1::real) < 1" by simp
-qed
+  using sin_monotone_2pi[of 1 "pi/2"] pi_gt3 by (auto simp: sin_gt_zero_02)
 
 lemma abs_sin_le_sin1:
   assumes "\<bar>t\<bar> \<le> 1" shows "\<bar>sin t\<bar> \<le> sin (1::real)"
@@ -188,14 +190,14 @@ proof -
   have cont: "continuous_on {x..y} cos" by (intro continuous_intros)
   have deriv: "((cos::real\<Rightarrow>real) has_derivative (*) (- sin u)) (at u)" for u :: real
     using DERIV_cos[of u] unfolding has_field_derivative_def by simp
-  have "\<exists>\<xi>\<in>{x<..<y}. norm (cos y - cos x) \<le> norm ((*) (- sin \<xi>) (y - x))"
+  have "\<exists>\<xi> \<in> {x<..<y}. norm (cos y - cos x) \<le> norm ((*) (- sin \<xi>) (y - x))"
     by (rule mvt_general[OF \<open>x < y\<close> cont]) (use deriv in blast)
   then obtain \<xi> where \<xi>: "\<xi> \<in> {x<..<y}" "norm (cos y - cos x) \<le> norm (- sin \<xi> * (y - x))"
     by auto
   have "\<bar>\<xi>\<bar> \<le> 1" using \<xi> assms by auto
   then have absxi: "\<bar>sin \<xi>\<bar> \<le> sin 1" by (rule abs_sin_le_sin1)
   have "\<bar>cos y - cos x\<bar> \<le> \<bar>sin \<xi>\<bar> * \<bar>y - x\<bar>"
-    using \<xi>(2) by (simp add: abs_mult)
+    using \<xi> by (simp add: abs_mult)
   also have "\<dots> \<le> sin 1 * \<bar>y - x\<bar>"
     using absxi by (simp add: mult_right_mono)
   finally show ?thesis
@@ -209,13 +211,10 @@ lemma cos_contraction:
   using cos_contraction_lt[of x y] cos_contraction_lt[of y x] assms
   by (cases x y rule: linorder_cases) (auto simp: abs_minus_commute)
 
-lemma dottie_in_pm1: "\<bar>dottie\<bar> \<le> 1"
-  using facts by simp
-
 lemma cos_step_to_dottie:
   assumes "\<bar>w\<bar> \<le> 1"
   shows "\<bar>cos w - dottie\<bar> \<le> sin 1 * \<bar>w - dottie\<bar>"
-  using facts by (metis assms cos_contraction dottie_in_pm1)
+  using Dottie.facts by (metis abs_cos_le_one assms cos_contraction)
 
 text \<open>After one step the iteration lands in $[-1,1]$ and stays there.\<close>
 
@@ -224,10 +223,10 @@ lemma cos_funpow_in_pm1:
   assumes "n \<ge> 1"
   shows "\<bar>(cos ^^ n) x0\<bar> \<le> 1"
 proof -
-  obtain m where "n = Suc m" using assms not0_implies_Suc by force
-  then have "(cos ^^ n) x0 = cos ((cos ^^ m) x0)"
-    by (simp add: funpow_swap1)
-  then show ?thesis by (simp add: abs_cos_le_one)
+  have "(cos ^^ n) x0 = cos ((cos ^^ (n-1)) x0)"
+    using assms funpow.simps
+    by (metis One_nat_def Suc_diff_le diff_Suc_Suc diff_zero o_apply)
+  then show ?thesis by simp
 qed
 
 text \<open>From a start in $[-1,1]$, the distance to the fixed point decays geometrically.\<close>
@@ -241,15 +240,12 @@ proof (induction n)
   show ?case by simp
 next
   case (Suc n)
-  have inpm1: "\<bar>(cos ^^ n) y0\<bar> \<le> 1"
-    using cos_funpow_in_pm1[of n y0] assms
-    by (metis funpow_0 less_one not_le)
-  have "\<bar>(cos ^^ Suc n) y0 - dottie\<bar> = \<bar>cos ((cos ^^ n) y0) - dottie\<bar>"
-    by (simp add: funpow_swap1)
-  also have "\<dots> \<le> sin 1 * \<bar>(cos ^^ n) y0 - dottie\<bar>"
-    using cos_step_to_dottie[OF inpm1] .
+  have "\<bar>(cos ^^ n) y0\<bar> \<le> 1"
+    by (metis funpow_0 less_one not_le cos_funpow_in_pm1[of n y0] assms)
+  then have "\<bar>(cos ^^ Suc n) y0 - dottie\<bar> \<le> sin 1 * \<bar>(cos ^^ n) y0 - dottie\<bar>"
+    using cos_step_to_dottie by simp
   also have "\<dots> \<le> sin 1 * ((sin 1)^n * \<bar>y0 - dottie\<bar>)"
-    using Suc.IH sin1_bounds(1) by (simp add: mult_left_mono)
+    using Suc.IH sin1_bounds by (simp add: mult_left_mono)
   also have "\<dots> = (sin 1)^(Suc n) * \<bar>y0 - dottie\<bar>"
     by (simp add: mult.assoc)
   finally show ?case .
@@ -266,17 +262,15 @@ proof -
     using tendsto_mult[OF pow0 tendsto_const, of "\<bar>y0 - dottie\<bar>"] by simp
   have "(\<lambda>n. \<bar>(cos ^^ n) y0 - dottie\<bar>) \<longlonglongrightarrow> 0"
     using tendsto_sandwich[OF _ _ tendsto_const null] cos_funpow_bound[OF assms] by auto
-  then have "(\<lambda>n. (cos ^^ n) y0 - dottie) \<longlonglongrightarrow> 0"
-    by (rule tendsto_rabs_zero_cancel)
   then show ?thesis
-    using Lim_null by blast
+    using Lim_null tendsto_rabs_zero_iff by blast
 qed
 
 theorem cos_iter_tendsto_dottie:
   fixes x0 :: real
   shows "(\<lambda>n. (cos ^^ n) x0) \<longlonglongrightarrow> dottie"
 proof -
-  have "\<bar>cos x0\<bar> \<le> 1" by (simp add: abs_cos_le_one)
+  have "\<bar>cos x0\<bar> \<le> 1" by simp
   from cos_iter_tendsto_unit[OF this]
   have "(\<lambda>n. (cos ^^ n) (cos x0)) \<longlonglongrightarrow> dottie" .
   moreover have "\<And>n. (cos ^^ n) (cos x0) = (cos ^^ Suc n) x0"
@@ -284,24 +278,6 @@ proof -
   ultimately have "(\<lambda>n. (cos ^^ Suc n) x0) \<longlonglongrightarrow> dottie" by simp
   then show ?thesis
     using filterlim_sequentially_Suc by blast
-qed
-
-subsection \<open>A trigonometric identity\<close>
-
-text \<open>Since $\cos(\mathit{dottie}) = \mathit{dottie}$ and $\mathit{dottie} \in (0,1)$, the
-  Pythagorean identity gives $\sin(\mathit{dottie}) = \sqrt{1 - \mathit{dottie}^2}$.\<close>
-
-lemma sin_dottie: "sin dottie = sqrt (1 - dottie\<^sup>2)"
-proof -
-  have "0 < dottie" "dottie < pi" using facts pi_gt3 by auto
-  then have pos: "0 < sin dottie" by (rule sin_gt_zero)
-  have "(sin dottie)\<^sup>2 = 1 - (cos dottie)\<^sup>2"
-    using sin_cos_squared_add[of dottie] by (simp add: algebra_simps)
-  also have "\<dots> = 1 - dottie\<^sup>2"
-    using facts(3) by simp
-  finally have "(sin dottie)\<^sup>2 = 1 - dottie\<^sup>2" .
-  then show ?thesis
-    using pos by (simp add: real_sqrt_unique)
 qed
 
 subsection \<open>Transcendence\<close>
@@ -314,20 +290,16 @@ theorem dottie_transcendental: "\<not> algebraic dottie"
 proof
   assume alg: "algebraic dottie"
   then have "\<not> algebraic (cos (complex_of_real dottie))"
-    using facts transcendental_cos by auto 
+    using Dottie.facts transcendental_cos by auto 
   moreover have "cos (complex_of_real dottie) = complex_of_real dottie"
-    using facts by (simp add: cos_of_real)
+    using Dottie.facts by (simp add: cos_of_real)
   ultimately show False using alg by simp
 qed
-
-end
 
 text \<open> We make key facts available outside the locale \<close>
 lemmas dottie_fp = Dottie.facts(3)
 lemmas dottie_bounds = Dottie.lb Dottie.ub
 lemmas dottie_attractor = Dottie.cos_iter_tendsto_dottie
-lemmas dottie_sin = Dottie.sin_dottie
-lemmas dottie_transcendental = Dottie.dottie_transcendental
 
 end
 
